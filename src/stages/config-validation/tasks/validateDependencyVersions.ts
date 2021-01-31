963bee5b7347739c6eb69adde9314b36715ba4fa
Craig Miller <craigmiller160@gmail.com>
2021-01-31 18:35:24 -0500

    Stubbed out some more logic

--- src/stages/config-validation/tasks/validateDependencyVersions.ts
+++ src/stages/config-validation/tasks/validateDependencyVersions.ts
@@ -2,6 +2,18 @@ import ProjectInfo from '../../../types/ProjectInfo';
 import { InputTask } from '../../../types/Build';
 import * as E from 'fp-ts/Either';
 
+const validateMavenVersions = (projectInfo: ProjectInfo) => {
+
+};
+
+const validateNpmVersions = (projectInfo: ProjectInfo) => {
+
+};
+
+const doVersionValidation = (projectInfo: ProjectInfo) => {
+
+};
+
 const validateDependencyVersions: InputTask<ProjectInfo,boolean> = (projectInfo: ProjectInfo) => {
     // TODO finish this
     return E.left(new Error());
