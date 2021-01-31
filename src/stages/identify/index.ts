633ea7f780eae4059fb9afd826902c36e3b20140
Craig <craigmiller160@gmail.com>
2021-01-31 08:30:35 -0500

    Got core of main stage working

--- src/stages/identify/index.ts
+++ src/stages/identify/index.ts
@@ -1,12 +1,12 @@
 import identifyProject from './tasks/identifyProject';
-import getProjectConfig from './tasks/getProjectConfig';
-import * as A from 'fp-ts/es6/Array';
+import getProjectConfig from './tasks/getProjectInfo';
 import * as E from 'fp-ts/es6/Either';
 import { pipe } from 'fp-ts/es6/pipeable';
 
-// pipe(
-//     [identifyProject, getProjectConfig],
-//     A.reduce()
-// );
+const identify = (): E.Either<Error, any> =>
+    pipe(
+        identifyProject(),
+        E.map((projectType) => getProjectConfig(projectType))
+    );
 
-
+export default identify;
