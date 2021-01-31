59fa3bcdac85e455d07319090ddf24ab7aa1f4bc
Craig <craigmiller160@gmail.com>
2021-01-31 09:38:08 -0500

    Fixed another eslint error

--- src/error/BuildError.ts
+++ src/error/BuildError.ts
@@ -1,9 +1,5 @@
 import { CustomError } from 'ts-custom-error';
 
-class BuildError extends CustomError {
-    constructor(message: string) {
-        super(message);
-    }
-}
+class BuildError extends CustomError {}
 
 export default BuildError;
