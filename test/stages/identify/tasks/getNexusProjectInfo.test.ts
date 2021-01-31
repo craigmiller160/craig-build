ad5657eed16de8d8f0ea4334fbc50ae0582168ad
Craig Miller <craigmiller160@gmail.com>
2021-01-31 18:13:57 -0500

    Test setup logic

--- test/stages/identify/tasks/getNexusProjectInfo.test.ts
+++ test/stages/identify/tasks/getNexusProjectInfo.test.ts
@@ -1,6 +1,28 @@
-import fs from 'fs';
+import {
+    searchForNpmBetas,
+    searchForNpmReleases,
+    searchForMavenReleases,
+    searchForMavenSnapshots
+} from '../../../../src/common/services/NexusRepoApi';
+import Mock = jest.Mock;
+
+jest.mock('../../../../src/common/services/NexusRepoApi', () => ({
+    searchForNpmBetas: jest.fn(),
+    searchForNpmReleases: jest.fn(),
+    searchForMavenReleases: jest.fn(),
+    searchForMavenSnapshots: jest.fn()
+}));
+
+const searchForNpmBetasMock = searchForNpmBetas as Mock;
+const searchForNpmReleasesMock = searchForNpmReleases as Mock;
+const searchForMavenSnapshotsMock = searchForMavenSnapshots as Mock;
+const searchForMavenReleasesMock = searchForMavenReleases as Mock;
 
 describe('getNexusProjectInfo task', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
     it('get Maven Nexus Project Info', () => {
         throw new Error();
     });
