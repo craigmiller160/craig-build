cceaa8f527b572fb627499422e669ad28c395b77
Craig <craigmiller160@gmail.com>
2021-01-31 08:50:42 -0500

    Some type tweaks

--- src/stages/identify/index.ts
+++ src/stages/identify/index.ts
@@ -2,11 +2,13 @@ import identifyProject from './tasks/identifyProject';
 import getProjectConfig from './tasks/getProjectInfo';
 import * as E from 'fp-ts/es6/Either';
 import { pipe } from 'fp-ts/es6/pipeable';
+import ProjectInfo from '../../types/ProjectInfo';
 
-const identify = (): E.Either<Error, any> =>
+const identify = (): E.Either<Error, ProjectInfo> =>
     pipe(
         identifyProject(),
-        E.map((projectType) => getProjectConfig(projectType))
+        E.map((projectType) => getProjectConfig(projectType)),
+        E.flatten
     );
 
 export default identify;
