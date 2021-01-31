871c17adf31955dd15804755799ecca6079cae99
Craig <craigmiller160@gmail.com>
2021-01-31 08:31:25 -0500

    Fixed compile errors

--- src/context/logger.ts
+++ src/context/logger.ts
@@ -1,12 +1,12 @@
 const LOG_PREFIX = 'CRAIG-BUILD'
 
-const taskLogger = (taskName: string, message: string) =>
+export const taskLogger = (taskName: string, message: string) =>
     console.log(`[${LOG_PREFIX}] [Task: ${taskName}] ${message}`);
 
-const stageLogger = (stageName: string, message: string) =>
+export const stageLogger = (stageName: string, message: string) =>
     console.log(`[${LOG_PREFIX}] [Stage: ${stageName}] ${message}`);
 
-const buildLogger = (message: string) =>
+export const buildLogger = (message: string) =>
     console.log(`[${LOG_PREFIX}] [Build] ${message}`);
 
 // TODO if unused, delete this
