4ecf59b99c1b93c937cebd22ddb4a2abd0cf34b9
Craig Miller <craigmiller160@gmail.com>
2021-01-31 18:21:09 -0500

    Implemented more tests

--- test/stages/identify/tasks/getNexusProjectInfo.test.ts
+++ test/stages/identify/tasks/getNexusProjectInfo.test.ts
@@ -1,10 +1,16 @@
 import {
-    searchForNpmBetas,
-    searchForNpmReleases,
     searchForMavenReleases,
-    searchForMavenSnapshots
+    searchForMavenSnapshots,
+    searchForNpmBetas,
+    searchForNpmReleases
 } from '../../../../src/common/services/NexusRepoApi';
+import ProjectType from '../../../../src/types/ProjectType';
+import ProjectInfo from '../../../../src/types/ProjectInfo';
 import Mock = jest.Mock;
+import getNexusProjectInfo from '../../../../src/stages/identify/tasks/getNexusProjectInfo';
+import NexusSearchResult from '../../../../src/types/NexusSearchResult';
+import * as TE from 'fp-ts/TaskEither';
+import '@relmify/jest-fp-ts';
 
 jest.mock('../../../../src/common/services/NexusRepoApi', () => ({
     searchForNpmBetas: jest.fn(),
@@ -18,16 +24,58 @@ const searchForNpmReleasesMock = searchForNpmReleases as Mock;
 const searchForMavenSnapshotsMock = searchForMavenSnapshots as Mock;
 const searchForMavenReleasesMock = searchForMavenReleases as Mock;
 
+const createProjectInfo = (projectType: ProjectType): ProjectInfo => ({
+    projectType,
+    name: 'The Project',
+    version: '1.1.0',
+    isPreRelease: false,
+    dependencies: []
+});
+
+const createNexusResult = (version: string): NexusSearchResult => ({
+    items: [
+        {
+            id: '',
+            repository: '',
+            format: '',
+            group: '',
+            name: '',
+            version,
+            assets: []
+        }
+    ]
+});
+
 describe('getNexusProjectInfo task', () => {
     beforeEach(() => {
         jest.resetAllMocks();
+        searchForMavenReleasesMock.mockImplementation(() => TE.right(createNexusResult('Maven-Release')));
+        searchForMavenSnapshotsMock.mockImplementation(() => TE.right(createNexusResult('Maven-Pre-Release')));
+        searchForNpmReleasesMock.mockImplementation(() => TE.right(createNexusResult('Npm-Release')));
+        searchForNpmBetasMock.mockImplementation(() => TE.right(createNexusResult('Npm-Pre-Release')));
     });
 
-    it('get Maven Nexus Project Info', () => {
-        throw new Error();
+    it('get Maven Nexus Project Info', async () => {
+        const projectInfo = createProjectInfo(ProjectType.MavenApplication);
+        const result = await getNexusProjectInfo(projectInfo)();
+        expect(result).toEqualRight({
+            ...projectInfo,
+            latestNexusVersions: {
+                latestPreReleaseVersion: 'Maven-Pre-Release',
+                latestReleaseVersion: 'Maven-Release'
+            }
+        });
     });
 
-    it('get NPM Nexus Project Info', () => {
-        throw new Error();
+    it('get NPM Nexus Project Info', async () => {
+        const projectInfo = createProjectInfo(ProjectType.NpmApplication);
+        const result = await getNexusProjectInfo(projectInfo)();
+        expect(result).toEqualRight({
+            ...projectInfo,
+            latestNexusVersions: {
+                latestPreReleaseVersion: 'Npm-Pre-Release',
+                latestReleaseVersion: 'Npm-Release'
+            }
+        });
     });
 });
