e9447dc702a345cf0762adfc5521470fdb13ea36
Craig Miller <craigmiller160@gmail.com>
2021-01-31 12:47:23 -0500

    Implemented some tests

--- test/utils/projectTypeUtils.test.ts
+++ test/utils/projectTypeUtils.test.ts
@@ -1,11 +1,26 @@
-import fs from 'fs';
+import { isApplication, isLibrary } from '../../src/utils/projectTypeUtils';
+import ProjectType from '../../src/types/ProjectType';
 
 describe('projectTypeUtils', () => {
     it('isApplication', () => {
-        throw new Error();
+        expect(isApplication(ProjectType.MavenApplication))
+            .toEqual(true);
+        expect(isApplication(ProjectType.NpmApplication))
+            .toEqual(true);
+        expect(isApplication(ProjectType.MavenLibrary))
+            .toEqual(false);
+        expect(isApplication(ProjectType.NpmLibrary))
+            .toEqual(false);
     });
 
     it('isLibrary', () => {
-        throw new Error();
+        expect(isLibrary(ProjectType.MavenApplication))
+            .toEqual(false);
+        expect(isLibrary(ProjectType.NpmApplication))
+            .toEqual(false);
+        expect(isLibrary(ProjectType.MavenLibrary))
+            .toEqual(true);
+        expect(isLibrary(ProjectType.NpmLibrary))
+            .toEqual(true);
     });
 });
