201b71c2f333fba60386b7e4be4d999357aa9d62
Craig Miller <craigmiller160@gmail.com>
2021-01-31 10:29:46 -0500

    Figured out dependency structure for PomXml

--- src/types/PomXml.ts
+++ src/types/PomXml.ts
@@ -1,7 +1,18 @@
+interface Dependency {
+    groupId: string[];
+    artifactId: string[];
+    version: string[];
+}
+
+interface Dependencies {
+    dependency: Dependency[];
+}
+
 export default interface PomXml {
     project: {
         groupId: string[];
         artifactId: string[];
         version: string[];
+        dependencies: Dependencies[];
     };
 }
