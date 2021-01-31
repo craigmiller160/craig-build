075980cdbe57575485740506d4299bf59a519b02
Craig <craigmiller160@gmail.com>
2021-01-31 09:34:09 -0500

    Added TODO

--- src/execution.ts
+++ src/execution.ts
@@ -5,6 +5,7 @@ import { pipe } from 'fp-ts/pipeable';
 import * as E from 'fp-ts/Either';
 
 // TODO need to log error here
+// TODO error should know which stage/task it failed on
 
 const execute = (): E.Either<Error, any> => { // TODO improve type here
     buildLogger(`Starting build for: ${getCwd()}`);
