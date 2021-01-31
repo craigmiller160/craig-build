7879473db1059f2c29aca57c5b707791cc4a106d
Craig <craigmiller160@gmail.com>
2021-01-31 09:21:07 -0500

    More logging tweaks

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import identify from './stages/identify';
 import { buildLogger } from './context/logger';
 import getCwd from './utils/getCwd';
 import { pipe } from 'fp-ts/pipeable';
+import * as E from 'fp-ts/Either';
 
 buildLogger(`Starting build for: ${getCwd()}`);
 
 pipe(
-    identify()
+    identify(),
+    E.map(() => {
+        // TODO include more details on output
+        buildLogger('Build finished successfully');
+    })
 );
