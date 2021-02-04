94a90fc9f1df04db60a759fdcb837fcb5d4412d3
Craig Miller <craigmiller160@gmail.com>
2021-02-04 16:47:53 -0500

    Stubbed out runCommand and tests

---
+++ src/utils/runCommand.ts
@@ -0,0 +1,9 @@
+
+/*
+ * const spawn = require('cross-spawn');
+
+const result = spawn.sync('ls', ['-l']);
+console.log(result.stdout.toString());
+ */
+
+export default {};
---
+++ test/utils/runCommand.test.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+describe('runCommand', () => {
+    it('test', () => {
+        throw new Error();
+    });
+});
