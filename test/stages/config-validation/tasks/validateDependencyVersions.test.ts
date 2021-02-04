c7cc76611e1e4e13faf9790f4a1f97af884a3643
Craig Miller <craigmiller160@gmail.com>
2021-02-03 21:50:44 -0500

    Stubbed out tests

--- test/stages/config-validation/tasks/validateDependencyVersions.test.ts
+++ test/stages/config-validation/tasks/validateDependencyVersions.test.ts
@@ -1,7 +1,27 @@
 import fs from 'fs';
 
 describe('validateDependencyVersions task', () => {
-    it('test', () => {
+    it('validates maven for release, with snapshot dependency', () => {
+        throw new Error();
+    });
+
+    it('validates maven for release successfully', () => {
+        throw new Error();
+    });
+
+    it('validates maven for snapshot, with snapshot dependency', () => {
+        throw new Error();
+    });
+
+    it('validates npm for release, with beta dependency', () => {
+        throw new Error();
+    });
+
+    it('validates npm for release successfully', () => {
+        throw new Error();
+    });
+
+    it('validates npm for beta, with beta dependency', () => {
         throw new Error();
     });
 });
