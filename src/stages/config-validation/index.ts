81010f4d7b14f3ab7d04596f48b35f0796ff5f43
Craig Miller <craigmiller160@gmail.com>
2021-02-04 16:16:08 -0500

    Integrated new task into stage

--- src/stages/config-validation/index.ts
+++ src/stages/config-validation/index.ts
@@ -4,13 +4,21 @@ import ProjectInfo from '../../types/ProjectInfo';
 import { stageLogger } from '../../common/logger';
 import { pipe } from 'fp-ts/pipeable';
 import validateDependencyVersions from './tasks/validateDependencyVersions';
+import { isApplication } from '../../utils/projectTypeUtils';
+import validateKubeVersion from './tasks/validateKubeVersion';
 
 export const STAGE_NAME = 'Config Validation';
 
 const configValidation: InputStage<ProjectInfo, ProjectInfo> = (projectInfo: ProjectInfo) => {
     stageLogger(STAGE_NAME, 'Starting...');
     return pipe(
-        validateDependencyVersions(projectInfo)
+        validateDependencyVersions(projectInfo),
+        E.chain((projectInfo) => {
+            if (isApplication(projectInfo.projectType)) {
+                return validateKubeVersion(projectInfo);
+            }
+            return E.right(projectInfo);
+        })
     );
 };
 
