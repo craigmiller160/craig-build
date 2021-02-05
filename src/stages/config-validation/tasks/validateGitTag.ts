b71824330c1b03fe8d0595c4aa89626751adf8b0
Craig <craigmiller160@gmail.com>
2021-02-05 11:25:36 -0500

    Finished validateGitTag

--- src/stages/config-validation/tasks/validateGitTag.ts
+++ src/stages/config-validation/tasks/validateGitTag.ts
@@ -1,9 +1,44 @@
 import * as E from 'fp-ts/Either';
-import { Task } from '../../../types/Build';
+import * as A from 'fp-ts/Array';
+import * as O from 'fp-ts/Option';
+import { InputTask, Task } from '../../../types/Build';
+import ProjectInfo from '../../../types/ProjectInfo';
+import { SUCCESS_STATUS, taskLogger } from '../../../common/logger';
+import { STAGE_NAME } from '../index';
+import runCommand from '../../../utils/runCommand';
+import { pipe } from 'fp-ts/pipeable';
+import { isLeft } from 'fp-ts/Either';
 
-const validateGitTask: Task<boolean> = () => {
-    // TODO finish this
-    return E.left(new Error());
+export const TASK_NAME = 'Validate Git Tags';
+
+// TODO make sure all tasks/stages have proper start/finish logging
+
+const validateGitTag: InputTask<ProjectInfo,ProjectInfo> = (projectInfo: ProjectInfo) => {
+    taskLogger(STAGE_NAME, TASK_NAME, 'Starting...');
+    if (projectInfo.isPreRelease) {
+        taskLogger(STAGE_NAME, TASK_NAME, 'Finished. Skipping git tag check for pre-release version.', SUCCESS_STATUS);
+        return E.right(projectInfo);
+    }
+
+    return pipe(
+        runCommand('git tag'),
+        E.chain((output: string) =>
+            pipe(
+                A.array.filter(output.split('\n'), (tag) => tag === `v${projectInfo.version}`),
+                A.reduce(E.right('Git tags validated'), (acc, value: string) => {
+                    if (E.isLeft(acc)) {
+                        return acc;
+                    }
+
+                    return E.left(new Error('Project version git tag already exists'));
+                })
+            )
+        ),
+        E.map((message: string) => {
+            taskLogger(STAGE_NAME, TASK_NAME, `Finished. ${message}`, SUCCESS_STATUS);
+            return projectInfo;
+        })
+    );
 };
 
-export default validateGitTask;
+export default validateGitTag;
--- test/stages/config-validation/tasks/validateGitTag.test.ts
+++ test/stages/config-validation/tasks/validateGitTag.test.ts
@@ -1,7 +1,15 @@
 import fs from 'fs';
 
 describe('validateGitTag task', () => {
-    it('test', () => {
+    it('git tag already exists', () => {
+        throw new Error();
+    });
+
+    it('git tag does not exist', () => {
+        throw new Error();
+    });
+
+    it('no tag check because pre-release', () => {
         throw new Error();
     });
 });
