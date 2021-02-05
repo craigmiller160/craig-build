31598c94d45339b0716298e71af13da8b601972f
Craig Miller <craigmiller160@gmail.com>
2021-02-05 16:20:25 -0500

    Making progress with the stage execution refactor

--- src/common/execution/task.ts
+++ src/common/execution/task.ts
@@ -7,39 +7,45 @@ import { pipe } from 'fp-ts/pipeable';
 
 // TODO when input and output are the same, don't need to put it in twice
 export type TaskFunction<Input,ResultValue> = (context: TaskContext<Input>) => TE.TaskEither<Error, Result<ResultValue>>;
-export type TaskShouldExecuteFunction<Input> = (context: TaskContext<Input>) => string | undefined;
-export interface Task<Input,ResultValue> {
+export type TaskShouldExecuteFunction<Input> = (input: Input) => string | undefined;
+export interface BuildTask<Input,ResultValue> {
+    stageName: string;
+    taskName: string;
     operation: (input: Input) => TE.TaskEither<Error, ResultValue>;
-    shouldExecute: (context: TaskContext<Input>) => string | undefined;
+    shouldExecute: (input: Input) => string | undefined;
 }
 
-const defaultShouldExecute: TaskShouldExecuteFunction<any> = (context: TaskContext<any>) => undefined;
+const defaultShouldExecute: TaskShouldExecuteFunction<any> = (context: any) => undefined;
 
 // TODO when tasks are added to a stage, ensure they are of the Task type
 
-const createTask = <Input, ResultValue>(stageName: string, taskName: string, taskFn: TaskFunction<Input, ResultValue>, shouldExecuteFn?: TaskShouldExecuteFunction<Input>): Task<Input,ResultValue> => ({
-    shouldExecute: (context: TaskContext<Input>) => {
-        return shouldExecuteFn ? shouldExecuteFn(context) : defaultShouldExecute(context);
-    },
-    operation: (input: Input): TE.TaskEither<Error, ResultValue> => {
-        const taskContext: TaskContext<Input> = {
-            stageName,
-            taskName,
-            createBuildError: createBuildError(stageName, taskName),
-            input,
-            logger: createTaskLogger(stageName, taskName)
-        };
+const createTask = <Input, ResultValue>(stageName: string, taskName: string, taskFn: TaskFunction<Input, ResultValue>, shouldExecuteFn?: TaskShouldExecuteFunction<Input>): BuildTask<Input,ResultValue> => {
+    return {
+        stageName,
+        taskName,
+        shouldExecute: (input: Input) => {
+            return shouldExecuteFn ? shouldExecuteFn(input) : defaultShouldExecute(input);
+        },
+        operation: (input: Input): TE.TaskEither<Error, ResultValue> => {
+            const taskContext: TaskContext<Input> = {
+                stageName,
+                taskName,
+                createBuildError: createBuildError(stageName, taskName),
+                input,
+                logger: createTaskLogger(stageName, taskName)
+            };
 
-        taskContext.logger('Starting...');
+            taskContext.logger('Starting...');
 
-        return pipe(
-            taskFn(taskContext),
-            TE.map((result) => {
-                taskContext.logger(`Finished. ${result.message}`, SUCCESS_STATUS);
-                return result.value;
-            })
-        );
-    }
-})
+            return pipe(
+                taskFn(taskContext),
+                TE.map((result) => {
+                    taskContext.logger(`Finished. ${result.message}`, SUCCESS_STATUS);
+                    return result.value;
+                })
+            );
+        }
+    };
+};
 export type TaskCreator = typeof createTask;
 export default createTask;
--- src/stages/identify/index.ts
+++ src/stages/identify/index.ts
@@ -3,12 +3,14 @@ import getBaseProjectInfo from './tasks/getBaseProjectInfo';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/pipeable';
 import ProjectInfo from '../../types/ProjectInfo';
-import { stageLogger, SUCCESS_STATUS } from '../../common/logger';
+import { SUCCESS_STATUS } from '../../common/logger';
 import { isApplication } from '../../utils/projectTypeUtils';
 import getKubeProjectInfo from './tasks/getKubeProjectInfo';
 import getNexusProjectInfo from './tasks/getNexusProjectInfo';
 import createStage, { StageFunction } from '../../common/execution/stage';
-import { Task } from '../../common/execution/task';
+import { BuildTask } from '../../common/execution/task';
+import { StageContext } from '../../common/execution/context';
+import ProjectType from '../../types/ProjectType';
 
 export const STAGE_NAME = 'Identify';
 
@@ -19,6 +21,27 @@ export const STAGE_NAME = 'Identify';
  * 2. Create array of task wrappers.
  * 3. Iterate over this array, probably with a reduce to get a single output
  */
+
+const conditionallyExecuteTask = <Input,ResultValue>(context: StageContext<unknown>, input: Input, task: BuildTask<Input, ResultValue>): TE.TaskEither<Error, ResultValue> => {
+    const shouldExecuteResult = task.shouldExecute(input);
+    if (shouldExecuteResult) {
+        context.logger(`Skipping task ${task.taskName}: ${shouldExecuteResult}`);
+        // return TE.right(input); // TODO how to handle output value.
+        return TE.left(new Error());
+    } else {
+        return task.operation(input);
+    }
+}
+
+const newIdentify: StageFunction<undefined, ProjectType> = (context: StageContext<undefined>) =>
+    pipe(
+        TE.of<Error,BuildTask<undefined, ProjectType>>(identifyProject),
+        TE.chain((identifyProjectTask) => conditionallyExecuteTask(context, undefined, identifyProjectTask)),
+        TE.map((value) => ({
+            message: '',
+            value
+        }))
+    );
 
 const identify: StageFunction<undefined,ProjectInfo> = () =>
     pipe(
