3f58b71a1d285684b5f315e2c7f43ed768f55c60
Craig Miller <craigmiller160@gmail.com>
2021-02-06 17:22:20 -0500

    Stubbed out more tests

--- test/stages/config-validation/tasks/validateNexusVersion.test.ts
+++ test/stages/config-validation/tasks/validateNexusVersion.test.ts
@@ -1,7 +1,19 @@
 import fs from 'fs';
 
 describe('validateNexusVersion task', () => {
-    it('test', () => {
+    it('is release, higher than all releases & pre-releases', () => {
+        throw new Error();
+    });
+
+    it('is release, higher than all releases, not pre-releases', () => {
+        throw new Error();
+    });
+
+    it('is pre-release, higher than all releases & pre-releases', () => {
+        throw new Error();
+    });
+
+    it('is pre-release, higher than all releases, not pre-releases', () => {
         throw new Error();
     });
 });
