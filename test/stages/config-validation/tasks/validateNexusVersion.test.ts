982660a5fffd1977a40f017cad96abced62edd2e
Craig Miller <craigmiller160@gmail.com>
2021-02-06 17:24:40 -0500

    Stubbed out more tests and began working on test logic

--- test/stages/config-validation/tasks/validateNexusVersion.test.ts
+++ test/stages/config-validation/tasks/validateNexusVersion.test.ts
@@ -1,7 +1,15 @@
-import fs from 'fs';
+import ProjectInfo from '../../../../src/types/ProjectInfo';
+import ProjectType from '../../../../src/types/ProjectType';
 
 describe('validateNexusVersion task', () => {
     it('is release, higher than all releases & pre-releases', () => {
+        const projectInfo: ProjectInfo = {
+            projectType: ProjectType.NpmLibrary,
+            isPreRelease: false,
+            name: 'my-project',
+            version: '1.1.0',
+            dependencies: []
+        }
         throw new Error();
     });
 
@@ -14,6 +22,18 @@ describe('validateNexusVersion task', () => {
     });
 
     it('is pre-release, higher than all releases, not pre-releases', () => {
+        throw new Error();
+    });
+
+    it('no nexus versions', () => {
+        throw new Error();
+    });
+
+    it('is release, no release version, higher than pre-release', () => {
+        throw new Error();
+    });
+
+    it('is pre-release, has release version, no pre-release', () => {
         throw new Error();
     });
 });
