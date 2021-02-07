33199f5c28c139eab713737d8c711efe33da62fc
Craig Miller <craigmiller160@gmail.com>
2021-02-07 12:29:11 -0500

    Added TODO

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { pipe } from 'fp-ts/pipeable';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 
+// TODO accept commands of "build" for the full build/deploy and redeploy to redeploy a production artifact
+
 pipe(
     execute(),
     TE.fold(
