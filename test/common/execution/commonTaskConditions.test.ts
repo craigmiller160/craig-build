7db0350e813765a988cf72f2df0b2055ddca9aae
Craig Miller <craigmiller160@gmail.com>
2021-02-06 19:57:51 -0500

    Stubbed out more tests

---
+++ test/common/execution/commonTaskConditions.test.ts
@@ -0,0 +1,22 @@
+
+describe('commonTaskConditions', () => {
+    describe('executeIfNotPreRelease', () => {
+        it('is pre-release', () => {
+            throw new Error();
+        });
+
+        it('is not pre-release', () => {
+            throw new Error();
+        });
+    });
+
+    describe('executeIfApplication', () => {
+        it('is application', () => {
+            throw new Error();
+        });
+
+        it('is not application', () => {
+            throw new Error();
+        });
+    });
+});
