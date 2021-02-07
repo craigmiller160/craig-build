3fe5713440ce69e102b18851eae149d07f21db73
Craig Miller <craigmiller160@gmail.com>
2021-02-07 11:26:30 -0500

    Updated runCommand to include log output

--- src/utils/runCommand.ts
+++ src/utils/runCommand.ts
@@ -3,7 +3,7 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/pipeable';
 
-const runCommand = (command: string): E.Either<Error, string> => {
+const runCommand = (command: string, logOutput: boolean = false): E.Either<Error, string> => {
   const commandParts = command.split(' ');
   const result = spawn.sync(commandParts[0], commandParts.slice(1));
   return pipe(
@@ -12,14 +12,22 @@ const runCommand = (command: string): E.Either<Error, string> => {
     E.of,
     E.chain<Error, number, string>((status) => {
       if (status === 0) {
-        return E.right(result.stdout.toString());
+          const output = result.stdout.toString();
+          if (logOutput) {
+              console.log(output);
+          }
+          return E.right(output);
       }
 
       if (status === -1) {
-        return E.left(new Error('No status code returned from command'));
+          return E.left(new Error('No status code returned from command'));
       }
 
-      return E.left(new Error(result.stderr.toString()));
+      const error = result.stderr.toString();
+      if (logOutput) {
+          console.error(error);
+      }
+      return E.left(new Error(error));
     })
   );
 };
