9c079ac853ebd81ba6c3c81e028d57db45510fbb
Craig Miller <craigmiller160@gmail.com>
2021-02-06 19:56:05 -0500

    Implemented more tests

--- test/stages/config-validation/configValidation.test.ts
+++ test/stages/config-validation/configValidation.test.ts
@@ -1,11 +1,65 @@
-import fs from 'fs';
+import validateDependencyVersions from '../../../src/stages/config-validation/tasks/validateDependencyVersions';
+import validateKubeVersion from '../../../src/stages/config-validation/tasks/validateKubeVersion';
+import validateGitTag from '../../../src/stages/config-validation/tasks/validateGitTag';
+import validateNexusVersion from '../../../src/stages/config-validation/tasks/validateNexusVersion';
+import ProjectInfo from '../../../src/types/ProjectInfo';
+import ProjectType from '../../../src/types/ProjectType';
+import configValidation from '../../../src/stages/config-validation';
+import * as TE from 'fp-ts/TaskEither';
+import '@relmify/jest-fp-ts';
+import BuildError from '../../../src/error/BuildError';
+import { STAGE_NAME } from '../../../src/stages/identify';
+
+jest.mock('../../../src/stages/config-validation/tasks/validateDependencyVersions', () => jest.fn());
+jest.mock('../../../src/stages/config-validation/tasks/validateKubeVersion', () => jest.fn());
+jest.mock('../../../src/stages/config-validation/tasks/validateGitTag', () => jest.fn());
+jest.mock('../../../src/stages/config-validation/tasks/validateNexusVersion', () => jest.fn());
+
+const validateDependencyVersionsMock = validateDependencyVersions as jest.Mock;
+const validateKubeVersionMock = validateKubeVersion as jest.Mock;
+const validateGitTagMock = validateGitTag as jest.Mock;
+const validateNexusVersionMock = validateNexusVersion as jest.Mock;
+
+const projectInfo: ProjectInfo = {
+    projectType: ProjectType.NpmLibrary,
+    name: 'my-project',
+    version: '1.0.0',
+    dependencies: [],
+    isPreRelease: false
+};
 
 describe('configValidation stage', () => {
-    it('completes successfully', () => {
-        throw new Error();
+    beforeEach(() => {
+        jest.resetAllMocks();
     });
 
-    it('completes with error', () => {
-        throw new Error();
+    it('completes successfully', async () => {
+        validateDependencyVersionsMock.mockImplementation(() => TE.right(projectInfo));
+        validateKubeVersionMock.mockImplementation(() => TE.right(projectInfo));
+        validateGitTagMock.mockImplementation(() => TE.right(projectInfo));
+        validateNexusVersionMock.mockImplementation(() => TE.right(projectInfo));
+
+        const result = await configValidation(projectInfo)();
+        expect(result).toEqualRight(projectInfo);
+
+        expect(validateDependencyVersionsMock).toHaveBeenCalledWith(projectInfo);
+        expect(validateKubeVersionMock).toHaveBeenCalledWith(projectInfo);
+        expect(validateGitTagMock).toHaveBeenCalledWith(projectInfo);
+        expect(validateNexusVersionMock).toHaveBeenCalledWith(projectInfo);
+    });
+
+    it('completes with error', async () => {
+        validateDependencyVersionsMock.mockImplementation(() => TE.right(projectInfo));
+        validateKubeVersionMock.mockImplementation(() => TE.left(new BuildError('Failing', STAGE_NAME)));
+        validateGitTagMock.mockImplementation(() => TE.right(projectInfo));
+        validateNexusVersionMock.mockImplementation(() => TE.right(projectInfo));
+
+        const result = await configValidation(projectInfo)();
+        expect(result).toEqualLeft(new BuildError('Failing', STAGE_NAME));
+
+        expect(validateDependencyVersionsMock).toHaveBeenCalledWith(projectInfo);
+        expect(validateKubeVersionMock).toHaveBeenCalledWith(projectInfo);
+        expect(validateGitTagMock).not.toHaveBeenCalled();
+        expect(validateNexusVersionMock).not.toHaveBeenCalled();
     });
 });
