54ebe16369eb1edcc9fa8db3334c2afa275b788c
Craig Miller <craigmiller160@gmail.com>
2021-02-07 12:20:32 -0500

    Integrated new task into stage

--- src/stages/createArtifact/index.ts
+++ src/stages/createArtifact/index.ts
@@ -4,12 +4,14 @@ import { StageContext } from '../../common/execution/context';
 import { pipe } from 'fp-ts/pipeable';
 import buildAndTest from './tasks/buildAndTest';
 import * as TE from 'fp-ts/TaskEither';
+import bumpNpmBeta from './tasks/bumpNpmBeta';
 
 export const STAGE_NAME = 'Create Artifact';
 
 const createArtifact: StageFunction<ProjectInfo> = (context: StageContext<ProjectInfo>) =>
     pipe(
         buildAndTest(context.input),
+        TE.chain(bumpNpmBeta),
         TE.map((projectInfo) => ({
             message: 'Artifact created successfully',
             value: projectInfo
