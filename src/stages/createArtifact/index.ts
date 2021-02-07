81624a107a1d12c0ed74bd3eef38266a2d56a1dc
Craig Miller <craigmiller160@gmail.com>
2021-02-07 11:37:13 -0500

    Built initial logic of createArtifact stage

--- src/stages/createArtifact/index.ts
+++ src/stages/createArtifact/index.ts
@@ -1,4 +1,17 @@
+import { StageFunction } from '../../common/execution/stage';
+import ProjectInfo from '../../types/ProjectInfo';
+import { StageContext } from '../../common/execution/context';
+import { pipe } from 'fp-ts/pipeable';
+import buildAndTest from './tasks/buildAndTest';
+import * as TE from 'fp-ts/TaskEither';
 
 export const STAGE_NAME = 'Create Artifact';
 
-export default {};
+const createArtifact: StageFunction<ProjectInfo> = (context: StageContext<ProjectInfo>) =>
+    pipe(
+        buildAndTest(context.input),
+        TE.map((projectInfo) => ({
+            message: 'Artifact created successfully',
+            value: projectInfo
+        }))
+    );
