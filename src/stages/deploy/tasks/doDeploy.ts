93a957e1b1356d2d137f5528f651be57c26ac6e7
Craig Miller <craigmiller160@gmail.com>
2021-02-07 16:42:22 -0500

    Wrote out the docker/k8s commands

--- src/stages/deploy/tasks/doDeploy.ts
+++ src/stages/deploy/tasks/doDeploy.ts
@@ -1,2 +1,17 @@
 
+export const TASK_NAME = 'Deploy';
+
+const DOCKER_REPO = 'craigmiller160.ddns.net:30004';
+export const APPLY_CONFIGMAP = 'kubectl apply -f configmap.yml';
+export const APPLY_DEPLOYMENT = 'kubectl apply -f deployment.yml';
+
+const createDockerLogin = (userName: string, password: string) =>
+    `sudo docker login ${DOCKER_REPO} -u ${userName} -p ${password}`;
+const createDockerBuild = (tag: string) =>
+    `sudo docker build --network=host -t ${tag} .`;
+const createDockerPush = (tag: string) =>
+    `sudo docker push ${tag}`;
+
+// TODO need to be able to change CWD in runCommand
+
 export default {};
