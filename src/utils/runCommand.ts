3c55c94a9f261a8c340ad34acdeb6794e9aa2bb6
Craig Miller <craigmiller160@gmail.com>
2021-02-07 16:46:29 -0500

    Overhauled runCommand to include options

--- src/utils/runCommand.ts
+++ src/utils/runCommand.ts
@@ -4,35 +4,43 @@ import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/pipeable';
 import getCwd from './getCwd';
 
-const runCommand = (command: string, logOutput: boolean = false): E.Either<Error, string> => {
-  const commandParts = command.split(' ');
-  const result = spawn.sync(commandParts[0], commandParts.slice(1), {
-    cwd: getCwd()
-  });
-  return pipe(
-    O.fromNullable(result.status),
-    O.getOrElse(() => -1),
-    E.of,
-    E.chain<Error, number, string>((status) => {
-      if (status === 0) {
-        const output = result.stdout.toString();
-        if (logOutput) {
-          console.log(output); // eslint-disable-line no-console
-        }
-        return E.right(output);
-      }
+export interface Options {
+    logOutput?: boolean;
+    cwd?: string;
+}
 
-      if (status === -1) {
-        return E.left(new Error('No status code returned from command'));
-      }
+const runCommand = (command: string, options?: Options): E.Either<Error, string> => {
+    const logOutput = options?.logOutput ?? false;
+    const cwd = options?.cwd ?? getCwd();
 
-      const error = result.stderr.toString();
-      if (logOutput) {
-        console.error(error);
-      }
-      return E.left(new Error(error));
-    })
-  );
+    const commandParts = command.split(' ');
+    const result = spawn.sync(commandParts[0], commandParts.slice(1), {
+        cwd
+    });
+    return pipe(
+        O.fromNullable(result.status),
+        O.getOrElse(() => -1),
+        E.of,
+        E.chain<Error, number, string>((status) => {
+            if (status === 0) {
+                const output = result.stdout.toString();
+                if (logOutput) {
+                    console.log(output); // eslint-disable-line no-console
+                }
+                return E.right(output);
+            }
+
+            if (status === -1) {
+                return E.left(new Error('No status code returned from command'));
+            }
+
+            const error = result.stderr.toString();
+            if (logOutput) {
+                console.error(error);
+            }
+            return E.left(new Error(error));
+        })
+    );
 };
 
 export default runCommand;
