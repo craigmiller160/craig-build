2925a625bb6fd266df7eeaebf258381f39b3a83c
Craig Miller <craigmiller160@gmail.com>
2021-02-07 12:11:33 -0500

    Wrote bumpNpmBeta

--- src/stages/createArtifact/tasks/bumpNpmBeta.ts
+++ src/stages/createArtifact/tasks/bumpNpmBeta.ts
@@ -1,2 +1,36 @@
 
+/*
+ * 1) Increment beta version
+ * 2) Update ProjectInfo with new version
+ * 3) Update package.json with new version
+ */
+
+import ProjectInfo from '../../../types/ProjectInfo';
+import { TaskFunction } from '../../../common/execution/task';
+import { TaskContext } from '../../../common/execution/context';
+import * as TE from 'fp-ts/TaskEither';
+
+const separateBetaNumber = (version: string): [string, number] => {
+    const [versionNumber, betaPart] = version.split('-');
+    const [beta, betaNumber] = betaPart.split('.'); // TODO what if there is no beta number yet?
+    return [`${versionNumber}-${beta}`, parseInt(betaNumber)];
+};
+
+const bumpNpmBeta: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo>) => {
+    const [versionWithoutBetaNum, betaNumber] = separateBetaNumber(context.input.version);
+    const newBetaNumber = betaNumber + 1;
+    const newVersion = `${versionWithoutBetaNum}.${newBetaNumber}`;
+    const newProjectInfo: ProjectInfo = {
+        ...context.input,
+        version: newVersion
+    };
+
+    // TODO write it to package.json
+
+    return TE.right({
+        message: `Bumped Npm project beta version: ${newVersion}`,
+        value: newProjectInfo
+    });
+};
+
 export default {};
