be65f64a65b4b96b29ee341c342692ec4c32e51d
Craig Miller <craigmiller160@gmail.com>
2021-02-13 18:37:16 -0500

    Fixed tests

--- test/stages/config-validation/tasks/validateKubeVersion.test.ts
+++ test/stages/config-validation/tasks/validateKubeVersion.test.ts
@@ -8,7 +8,7 @@ import { STAGE_NAME } from '../../../../src/stages/config-validation';
 describe('validateKubeVersion task', () => {
     it('valid pre-release kubernetes version', async () => {
         const projectInfo: ProjectInfo = {
-            projectType: ProjectType.NpmLibrary,
+            projectType: ProjectType.NpmApplication,
             name: 'my-project',
             version: '1.0.0-beta',
             isPreRelease: true,
@@ -21,7 +21,7 @@ describe('validateKubeVersion task', () => {
 
     it('invalid pre-release kubernetes version', async () => {
         const projectInfo: ProjectInfo = {
-            projectType: ProjectType.NpmLibrary,
+            projectType: ProjectType.NpmApplication,
             name: 'my-project',
             version: '1.0.0-beta',
             isPreRelease: true,
@@ -29,7 +29,6 @@ describe('validateKubeVersion task', () => {
             kubernetesDockerImage: 'localhost:30000/my-project:1.0.0-beta'
         };
         const result = await validateKubeVersion(projectInfo)();
-        console.log(result); // TODO delete this
         const message = 'Invalid Kubernetes Version. Project Version: 1.0.0-beta Kubernetes Image: localhost:30000/my-project:1.0.0-beta';
         expect(result).toEqualLeft(new BuildError(message,
             TASK_NAME,
@@ -39,7 +38,7 @@ describe('validateKubeVersion task', () => {
 
     it('valid release kubernetes version', async () => {
         const projectInfo: ProjectInfo = {
-            projectType: ProjectType.NpmLibrary,
+            projectType: ProjectType.NpmApplication,
             name: 'my-project',
             version: '1.0.0',
             isPreRelease: false,
@@ -52,7 +51,7 @@ describe('validateKubeVersion task', () => {
 
     it('invalid release kubernetes version', async () => {
         const projectInfo: ProjectInfo = {
-            projectType: ProjectType.NpmLibrary,
+            projectType: ProjectType.NpmApplication,
             name: 'my-project',
             version: '1.0.0',
             isPreRelease: false,
