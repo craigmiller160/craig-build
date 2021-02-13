76acc3b06650be9d0ef7aa98af6d0cebc7bd5c7b
Craig <craigmiller160@gmail.com>
2021-02-13 10:35:35 -0500

    Got first test working

--- test/stages/deploy/tasks/dockerBuild.test.ts
+++ test/stages/deploy/tasks/dockerBuild.test.ts
@@ -1,5 +1,11 @@
 import ProjectInfo from '../../../../src/types/ProjectInfo';
 import ProjectType from '../../../../src/types/ProjectType';
+import '@relmify/jest-fp-ts';
+import runCommand from '../../../../src/utils/runCommand';
+import dockerBuild, { TASK_NAME } from '../../../../src/stages/deploy/tasks/dockerBuild';
+import BuildError from '../../../../src/error/BuildError';
+import { STAGE_NAME } from '../../../../src/stages/deploy';
+import getCwd from '../../../../src/utils/getCwd';
 
 const baseProjectInfo: ProjectInfo = {
     projectType: ProjectType.MavenApplication,
@@ -10,10 +16,18 @@ const baseProjectInfo: ProjectInfo = {
     dependencies: []
 };
 
+const runCommandMock = runCommand as jest.Mock;
+const getCwdMock = getCwd as jest.Mock;
+
 describe('dockerBuild task', () => {
-    describe('validations', () => {
-        it('has no kubernetesDockerImage', () => {
-            throw new Error();
+    describe('validations',  () => {
+        it('has no kubernetesDockerImage', async () => {
+            getCwdMock.mockImplementation(() => '');
+            const result = await dockerBuild({
+                ...baseProjectInfo,
+                kubernetesDockerImage: undefined
+            })();
+            expect(result).toEqualLeft(new BuildError('Missing Kubernetes Docker Image', STAGE_NAME, TASK_NAME));
         });
 
         it('has no docker username', () => {
