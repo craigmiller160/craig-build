23145cc68feef41a95ce9fce948e401abf9f399f
Craig <craigmiller160@gmail.com>
2021-02-13 08:57:35 -0500

    Wrote deploy stage

--- src/stages/deploy/index.ts
+++ src/stages/deploy/index.ts
@@ -1,4 +1,21 @@
+import createStage, { StageFunction } from '../../common/execution/stage';
+import ProjectInfo from '../../types/ProjectInfo';
+import { StageContext } from '../../common/execution/context';
+import { pipe } from 'fp-ts/pipeable';
+import dockerBuild from './tasks/dockerBuild';
+import * as TE from 'fp-ts/TaskEither';
+import kubeDeploy from './tasks/kubeDeploy';
 
 export const STAGE_NAME = 'Deploy';
 
-export default {};
+const deploy: StageFunction<ProjectInfo> = (context: StageContext<ProjectInfo>) =>
+    pipe(
+        dockerBuild(context.input),
+        TE.chain(kubeDeploy),
+        TE.map((projectInfo) => ({
+            message: 'Deployment complete',
+            value: projectInfo
+        }))
+    );
+
+export default createStage(STAGE_NAME, deploy);
