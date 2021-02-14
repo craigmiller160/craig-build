64d58d84a5b7a241ab80732522f5faf8199f05e5
Craig Miller <craigmiller160@gmail.com>
2021-02-14 08:05:24 -0500

    Test setup logic

--- test/stages/deploy/tasks/kubeDeploy.test.ts
+++ test/stages/deploy/tasks/kubeDeploy.test.ts
@@ -1,4 +1,8 @@
 import fs from 'fs';
+import path from 'path';
+
+const configmapPath = path.resolve(process.cwd(), 'test', '__working-dirs__', 'npmReleaseApplication');
+const noConfigmapPath = path.resolve(process.cwd(), 'test', '__working-dirs__', 'mavenReleaseApplication');
 
 describe('kubeDeploy task', () => {
     it('deploys with configmap', () => {
