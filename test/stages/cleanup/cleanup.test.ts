06ad50031bd47335604e2c5a184f31973b5ea68e
Craig Miller <craigmiller160@gmail.com>
2021-02-16 06:44:55 -0500

    Implemented another test

--- test/stages/cleanup/cleanup.test.ts
+++ test/stages/cleanup/cleanup.test.ts
@@ -1,7 +1,33 @@
-import fs from 'fs';
+import ProjectInfo from '../../../src/types/ProjectInfo';
+import ProjectType from '../../../src/types/ProjectType';
+import gitTag from '../../../src/stages/cleanup/tasks/gitTag';
+import * as TE from 'fp-ts/TaskEither';
+import '@relmify/jest-fp-ts';
+import cleanup from '../../../src/stages/cleanup';
+
+jest.mock('../../../src/stages/cleanup/tasks/gitTag', () => jest.fn());
+
+const projectInfo: ProjectInfo = {
+    projectType: ProjectType.NpmApplication,
+    name: 'my-project',
+    version: '1.0.0',
+    dependencies: [],
+    isPreRelease: false
+};
+
+const gitTagMock = gitTag as jest.Mock;
 
 describe('cleanup stage', () => {
-    it('test', () => {
-        throw new Error();
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('completes successfully', async () => {
+        gitTagMock.mockImplementation(() => TE.right(projectInfo));
+
+        const result = await cleanup(projectInfo)();
+        expect(result).toEqualRight(projectInfo);
+
+        expect(gitTagMock).toHaveBeenCalledWith(projectInfo);
     });
 });
