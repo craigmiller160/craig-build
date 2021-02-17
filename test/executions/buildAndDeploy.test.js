3e434712e1edc5def9659de3b26e7d19f1a46f6d
Craig Miller <craigmiller160@gmail.com>
2021-02-17 14:57:32 -0500

    Stubbed out new files and tests

---
+++ test/executions/buildAndDeploy.test.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+describe('buildAndDeploy', () => {
+    it('test', () => {
+        throw new Error();
+    });
+});
---
+++ test/executions/deployOnly.test.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+describe('deployOnly', () => {
+    it('test', () => {
+        throw new Error();
+    });
+});
