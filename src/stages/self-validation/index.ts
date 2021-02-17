3a7021668dd17f0a34e064dc6e1ec97c5d246376
Craig Miller <craigmiller160@gmail.com>
2021-02-17 17:48:13 -0500

    Stubbed out final task and stage

--- src/stages/self-validation/index.ts
+++ src/stages/self-validation/index.ts
@@ -1,2 +1,12 @@
+import * as TE from 'fp-ts/TaskEither';
+import createStage, { StageFunction } from '../../common/execution/stage';
+import { StageContext } from '../../common/execution/context';
 
-export default {};
+export const STAGE_NAME = 'Self-Validation';
+
+// TODO figure out better types
+const selfValidation: StageFunction<any> = (context: StageContext<any>) => {
+    return TE.left(new Error('Finish this'));
+};
+
+export default createStage(STAGE_NAME, selfValidation);
--- src/stages/self-validation/tasks/confirmLatestVersion.ts
+++ src/stages/self-validation/tasks/confirmLatestVersion.ts
@@ -1,2 +1,14 @@
+import createTask, { TaskFunction } from '../../../common/execution/task';
+import ProjectInfo from '../../../types/ProjectInfo';
+import { TaskContext } from '../../../common/execution/context';
+import * as TE from 'fp-ts/TaskEither';
+import { STAGE_NAME } from '../index';
 
-export default {};
+const TASK_NAME = 'Confirm Latest Version';
+
+// TODO figure out types
+const confirmLatestVersion: TaskFunction<any> = (context: TaskContext<ProjectInfo>) => {
+    return TE.left(new Error('Finish this'));
+};
+
+export default createTask(STAGE_NAME, TASK_NAME, confirmLatestVersion);
