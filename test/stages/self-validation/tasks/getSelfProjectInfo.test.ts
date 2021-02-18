6dbf0918912459421fd919b0b3acfdd3922072cc
Craig Miller <craigmiller160@gmail.com>
2021-02-17 19:11:46 -0500

    Finished test

--- test/stages/self-validation/tasks/getSelfProjectInfo.test.ts
+++ test/stages/self-validation/tasks/getSelfProjectInfo.test.ts
@@ -1,7 +1,21 @@
+import '@relmify/jest-fp-ts';
+import getSelfProjectInfo from '../../../../src/stages/self-validation/tasks/getSelfProjectInfo';
+import ProjectType from '../../../../src/types/ProjectType';
+import PackageJson from '../../../../src/types/PackageJson';
 import fs from 'fs';
+import path from 'path';
+
+const packageJson: PackageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', '..', '..', 'package.json'), 'utf8'));
 
 describe('getSelfProjectInfo task', () => {
-    it('test', () => {
-        throw new Error();
+    it('gets info successfully', async () => {
+        const result = await getSelfProjectInfo(undefined)();
+        expect(result).toEqualRight({
+            projectType: ProjectType.NpmApplication,
+            name: '@craigmiller160/craig-build',
+            version: packageJson.version,
+            dependencies: [],
+            isPreRelease: packageJson.version.includes('beta')
+        })
     });
 });
