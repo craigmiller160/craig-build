3c93e9a76d3627abe04145024fd6359e6ff27144
Craig Miller <craigmiller160@gmail.com>
2021-02-17 19:17:04 -0500

    Fixed eslint errors

--- src/execution/buildAndDeploy.ts
+++ src/execution/buildAndDeploy.ts
@@ -1,43 +1,44 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/pipeable';
 import ProjectInfo from '../types/ProjectInfo';
 import { buildLogger, ERROR_STATUS, SUCCESS_STATUS } from '../common/logger';
 import getCwd from '../utils/getCwd';
-import { pipe } from 'fp-ts/pipeable';
 import identify from '../stages/identify';
 import configValidation from '../stages/config-validation';
 import createArtifact from '../stages/createArtifact';
 import deploy from '../stages/deploy';
 import cleanup from '../stages/cleanup';
 import { isBuildError } from '../error/BuildError';
-import confirmLatestVersion from '../stages/self-validation/tasks/confirmLatestVersion';
+import selfValidation from '../stages/self-validation';
 
 const buildAndDeploy = (): TE.TaskEither<Error, ProjectInfo> => {
-    buildLogger(`Building and deploying ${getCwd()}`);
+  buildLogger(`Building and deploying ${getCwd()}`);
 
-    return pipe(
-        confirmLatestVersion(null), // TODO figure out input here
-        TE.chain(() => identify(undefined)), // TODO figure out input here
-        TE.chain(configValidation),
-        TE.chain(createArtifact),
-        TE.chain(deploy),
-        TE.chain(cleanup),
-        TE.map((projectInfo) => {
-            buildLogger('Build and deploy finished successfully', SUCCESS_STATUS);
-            return projectInfo;
-        }),
-        TE.mapLeft((error) => {
-            if (isBuildError(error)) {
-                const message = `Build and deploy failed on Stage ${error.stageName} and Task ${error.taskName}: ${error.message}`;
-                buildLogger(message, ERROR_STATUS);
-                console.error(error);
-            } else {
-                const message = `Build and Deploy Error: ${error.message}`;
-                buildLogger(message, ERROR_STATUS);
-                console.error(error);
-            }
-            return error;
-        })
-    );
+  return pipe(
+    selfValidation(undefined),
+    TE.chain(identify),
+    TE.chain(configValidation),
+    TE.chain(createArtifact),
+    TE.chain(deploy),
+    TE.chain(cleanup),
+    TE.map((projectInfo) => {
+      buildLogger('Build and deploy finished successfully', SUCCESS_STATUS);
+      return projectInfo;
+    }),
+    TE.mapLeft((error) => {
+      if (isBuildError(error)) {
+        const message = `Build and deploy failed on Stage ${error.stageName} and Task ${error.taskName}: ` +
+            `${error.message}`;
+        buildLogger(message, ERROR_STATUS);
+        console.error(error);
+      } else {
+        const message = `Build and Deploy Error: ${error.message}`;
+        buildLogger(message, ERROR_STATUS);
+        console.error(error);
+      }
+      return error;
+    })
+  );
 };
 
 export default buildAndDeploy;
--- src/execution/deployOnly.ts
+++ src/execution/deployOnly.ts
@@ -1,8 +1,8 @@
 import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/pipeable';
 import ProjectInfo from '../types/ProjectInfo';
 import { buildLogger, ERROR_STATUS, SUCCESS_STATUS } from '../common/logger';
 import getCwd from '../utils/getCwd';
-import { pipe } from 'fp-ts/pipeable';
 import selfValidation from '../stages/self-validation';
 import identify from '../stages/identify';
 import configValidation from '../stages/config-validation';
@@ -10,30 +10,30 @@ import deploy from '../stages/deploy';
 import { isBuildError } from '../error/BuildError';
 
 const deployOnly = (): TE.TaskEither<Error, ProjectInfo> => {
-    buildLogger(`Deploying only ${getCwd()}`);
+  buildLogger(`Deploying only ${getCwd()}`);
 
-    return pipe(
-        selfValidation(null), // TODO review this arg
-        TE.chain(() => identify(undefined)), // TODO review this arg
-        TE.chain(configValidation),
-        TE.chain(deploy),
-        TE.map((projectInfo) => {
-            buildLogger('Deploy only finished successfully', SUCCESS_STATUS);
-            return projectInfo;
-        }),
-        TE.mapLeft((error) => {
-            if (isBuildError(error)) {
-                const message = `Deploy only failed on Stage ${error.stageName} and Task ${error.taskName}: ${error.message}`;
-                buildLogger(message, ERROR_STATUS);
-                console.error(error);
-            } else {
-                const message = `Deploy Only Error: ${error.message}`;
-                buildLogger(message, ERROR_STATUS);
-                console.error(error);
-            }
-            return error;
-        })
-    );
+  return pipe(
+    selfValidation(null), // TODO review this arg
+    TE.chain(() => identify(undefined)), // TODO review this arg
+    TE.chain(configValidation),
+    TE.chain(deploy),
+    TE.map((projectInfo) => {
+      buildLogger('Deploy only finished successfully', SUCCESS_STATUS);
+      return projectInfo;
+    }),
+    TE.mapLeft((error) => {
+      if (isBuildError(error)) {
+        const message = `Deploy only failed on Stage ${error.stageName} and Task ${error.taskName}: ${error.message}`;
+        buildLogger(message, ERROR_STATUS);
+        console.error(error);
+      } else {
+        const message = `Deploy Only Error: ${error.message}`;
+        buildLogger(message, ERROR_STATUS);
+        console.error(error);
+      }
+      return error;
+    })
+  );
 };
 
 export default deployOnly;
