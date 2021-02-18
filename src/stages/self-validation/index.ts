823d02e7d9c1e7564988f5958db2de30b1b639b9
Craig Miller <craigmiller160@gmail.com>
2021-02-17 19:08:35 -0500

    Implemented self-validation stage

--- src/stages/self-validation/index.ts
+++ src/stages/self-validation/index.ts
@@ -1,12 +1,22 @@
 import * as TE from 'fp-ts/TaskEither';
 import createStage, { StageFunction } from '../../common/execution/stage';
 import { StageContext } from '../../common/execution/context';
+import getSelfProjectInfo from './tasks/getSelfProjectInfo';
+import { pipe } from 'fp-ts/pipeable';
+import getNexusProjectInfo from '../../common/tasks/getNexusProjectInfo';
+import validateNexusVersion from '../../common/tasks/validateNexusVersion';
 
 export const STAGE_NAME = 'Self-Validation';
 
-// TODO figure out better types
-const selfValidation: StageFunction<any> = (context: StageContext<any>) => {
-    return TE.left(new Error('Finish this'));
-};
+const selfValidation: StageFunction<undefined> = (context: StageContext<undefined>) =>
+    pipe(
+        getSelfProjectInfo(undefined),
+        TE.chain(getNexusProjectInfo),
+        TE.chain(validateNexusVersion),
+        TE.map(() => ({
+            message: 'Successfully validated build application',
+            value: undefined
+        }))
+    );
 
 export default createStage(STAGE_NAME, selfValidation);
