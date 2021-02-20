3797bf6d87580f183f1362d75c59d6b67169d956
Craig Miller <craigmiller160@gmail.com>
2021-02-20 14:19:43 -0500

    Integrated simple git

--- src/stages/createArtifact/tasks/publish.ts
+++ src/stages/createArtifact/tasks/publish.ts
@@ -7,21 +7,32 @@ import { pipe } from 'fp-ts/pipeable';
 import * as TE from 'fp-ts/TaskEither';
 import * as E from 'fp-ts/Either';
 import stageName from '../stageName';
+import simpleGit from 'simple-git';
+import handleUnknownError from '../../../utils/handleUnknownError';
+import getCwd from '../../../utils/getCwd';
 
 export const TASK_NAME = 'Publish';
 
 export const NPM_PUBLISH_COMMAND = 'yarn publish --no-git-tag-version --new-version';
-export const CLEAR_GIT_COMMAND = 'git reset HEAD && git checkout .';
 
-const publish: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo>) =>
-    pipe(
+// TODO update tests to mock the command
+const publish: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo>) => {
+    const git = simpleGit({
+        baseDir: getCwd()
+    });
+
+    return pipe(
         runCommand(`${NPM_PUBLISH_COMMAND} ${context.input.version}`, { logOutput: true }),
-        E.chain(() => runCommand(CLEAR_GIT_COMMAND)),
         TE.fromEither,
+        TE.chain(() => TE.tryCatch(
+            () => git.checkout('.'),
+            handleUnknownError
+        )),
         TE.map(() => ({
             message: 'Published artifact',
             value: context.input
         }))
     );
+};
 
 export default createTask(stageName, TASK_NAME, publish, executeIfNpmProject);
