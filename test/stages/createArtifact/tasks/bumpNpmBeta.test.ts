51c347195883deb3d64d008bb0044d294e6d43b8
Craig Miller <craigmiller160@gmail.com>
2021-02-20 14:05:33 -0500

    Updated tests

--- test/stages/createArtifact/tasks/bumpNpmBeta.test.ts
+++ test/stages/createArtifact/tasks/bumpNpmBeta.test.ts
@@ -7,16 +7,30 @@ const baseProjectInfo: ProjectInfo = {
     projectType: ProjectType.NpmApplication,
     isPreRelease: true,
     name: 'my-project',
-    version: '1.0.0-beta.1',
+    version: '1.0.0-beta',
     dependencies: []
 };
 
 describe('bumpNpmBeta task', () => {
-    it('bumps npm beta with nexus pre-release version', () => {
-        throw new Error();
+    it('bumps npm beta with nexus pre-release version', async () => {
+        const projectInfo: ProjectInfo = {
+            ...baseProjectInfo,
+            latestNexusVersions: {
+                latestPreReleaseVersion: '1.0.0-beta.2'
+            }
+        };
+        const result = await bumpNpmBeta(projectInfo)();
+        expect(result).toEqualRight({
+            ...projectInfo,
+            version: '1.0.0-beta.3'
+        });
     });
 
-    it('bumps npm beta with no nexus pre-release version', () => {
-        throw new Error();
+    it('bumps npm beta with no nexus pre-release version', async () => {
+        const result = await bumpNpmBeta(baseProjectInfo)();
+        expect(result).toEqualRight({
+            ...baseProjectInfo,
+            version: '1.0.0-beta.1'
+        });
     });
 });
