1d1701be6abb064a4850a0a8b7589cce4a4bf83e
Craig Miller <craigmiller160@gmail.com>
2021-02-22 09:23:29 -0500

    More test setup logic

--- test/stages/deploy/tasks/downloadArtifact.test.ts
+++ test/stages/deploy/tasks/downloadArtifact.test.ts
@@ -1,16 +1,38 @@
 import '@relmify/jest-fp-ts';
 import * as TE from 'fp-ts/TaskEither';
 import { downloadArtifact as downloadArtifactService } from '../../../../src/common/services/NexusRepoApi';
+import path from 'path';
+import fs from 'fs';
 
 jest.mock('../../../../src/common/services/NexusRepoApi', () => ({
     downloadArtifact: jest.fn()
 }));
 
 const downloadArtifactServiceMock = downloadArtifactService as jest.Mock;
+const workingDirs = path.resolve(process.cwd(), 'test', '__working-dirs__');
+const mavenReleaseWorkDir = path.resolve(workingDirs, 'mavenReleaseApplication');
+const mavenReleaseBuildDir = path.resolve(mavenReleaseWorkDir, 'deploy', 'build');
+const mavenPreReleaseWorkDir = path.resolve(workingDirs, 'mavenSnapshotApplication');
+const mavenPreReleaseBuildDir = path.resolve(mavenPreReleaseWorkDir, 'deploy', 'build');
+
+const cleanDirs = () => {
+    if (fs.existsSync(mavenReleaseBuildDir)) {
+        fs.rmdirSync(mavenReleaseBuildDir, { recursive: true });
+    }
+
+    if (fs.existsSync(mavenPreReleaseBuildDir)) {
+        fs.rmdirSync(mavenPreReleaseBuildDir, { recursive: true });
+    }
+};
 
 describe('downloadArtifact task', () => {
     beforeEach(() => {
         jest.resetAllMocks();
+        cleanDirs();
+    });
+
+    afterEach(() => {
+        cleanDirs();
     });
 
     it('downloads maven pre-release', () => {
