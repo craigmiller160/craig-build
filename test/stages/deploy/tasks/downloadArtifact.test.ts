48670e899e04ad32143b43a14cc69274b2c81763
Craig Miller <craigmiller160@gmail.com>
2021-02-22 09:18:42 -0500

    Did a lot of test setup logic

--- test/stages/deploy/tasks/downloadArtifact.test.ts
+++ test/stages/deploy/tasks/downloadArtifact.test.ts
@@ -1,7 +1,23 @@
 import '@relmify/jest-fp-ts';
+import * as TE from 'fp-ts/TaskEither';
+import { downloadArtifact as downloadArtifactService } from '../../../../src/common/services/NexusRepoApi';
+
+jest.mock('../../../../src/common/services/NexusRepoApi', () => ({
+    downloadArtifact: jest.fn()
+}));
+
+const downloadArtifactServiceMock = downloadArtifactService as jest.Mock;
 
 describe('downloadArtifact task', () => {
-    it('test', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('downloads maven pre-release', () => {
+        throw new Error();
+    });
+
+    it('downloads maven release', () => {
         throw new Error();
     });
 });
