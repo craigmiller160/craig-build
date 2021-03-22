b4ec67ac61ff69be76ea80350cd6da4a23d1ecba
Craig Miller <craigmiller160@gmail.com>
2021-03-22 14:36:24 -0400

    Implemented more tests

--- test/stages/cleanup/tasks/gitTag.test.ts
+++ test/stages/cleanup/tasks/gitTag.test.ts
@@ -4,6 +4,7 @@ import '@relmify/jest-fp-ts';
 import gitTag from '../../../../src/stages/cleanup/tasks/gitTag';
 import runCommand from '../../../../src/utils/runCommand';
 import * as E from 'fp-ts/Either';
+import {DEPLOY_ONLY_BUILD} from "../../../../src/execution/executionConstants";
 
 const projectInfo: ProjectInfo = {
     projectType: ProjectType.NpmApplication,
@@ -27,12 +28,22 @@ describe('gitTag task', () => {
     });
 
     describe('skip execution', () => {
-        it('is not release', () => {
-            throw new Error();
+        it('is not release', async () => {
+            const newProjectInfo: ProjectInfo = {
+                ...projectInfo,
+                isPreRelease: true
+            };
+
+            const result = await gitTag(newProjectInfo)();
+            expect(result).toEqualRight(newProjectInfo);
+            expect(runCommandMock).not.toHaveBeenCalled();
         });
 
-        it('is deploy-only build', () => {
-            throw new Error();
+        it('is deploy-only build', async () => {
+            process.env.BUILD_NAME = DEPLOY_ONLY_BUILD;
+            const result = await gitTag(projectInfo)();
+            expect(result).toEqualRight(projectInfo);
+            expect(runCommandMock).not.toHaveBeenCalled();
         });
     });
 });
