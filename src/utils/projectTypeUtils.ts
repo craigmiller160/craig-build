c8afab6365838555e8278f157796bf8367a3d2a2
Craig Miller <craigmiller160@gmail.com>
2021-03-22 15:10:53 -0400

    Expanded projectTypeUtils and updated tests

--- src/utils/projectTypeUtils.ts
+++ src/utils/projectTypeUtils.ts
@@ -1,17 +1,22 @@
 import ProjectType from '../types/ProjectType';
 
 export const isApplication = (projectType: ProjectType) =>
-  ProjectType.MavenApplication === projectType ||
-    ProjectType.NpmApplication === projectType;
+    ProjectType.MavenApplication === projectType ||
+    ProjectType.NpmApplication === projectType ||
+    ProjectType.DockerApplication === projectType;
 
 export const isLibrary = (projectType: ProjectType) =>
-  ProjectType.MavenLibrary === projectType ||
+    ProjectType.MavenLibrary === projectType ||
     ProjectType.NpmLibrary === projectType;
 
 export const isMaven = (projectType: ProjectType) =>
-  ProjectType.MavenLibrary === projectType ||
+    ProjectType.MavenLibrary === projectType ||
     ProjectType.MavenApplication === projectType;
 
 export const isNpm = (projectType: ProjectType) =>
-  ProjectType.NpmLibrary === projectType ||
+    ProjectType.NpmLibrary === projectType ||
     ProjectType.NpmApplication === projectType;
+
+export const isDocker = (projectType: ProjectType) =>
+    ProjectType.DockerApplication === projectType ||
+    ProjectType.DockerImage === projectType;
--- test/utils/projectTypeUtils.test.ts
+++ test/utils/projectTypeUtils.test.ts
@@ -1,4 +1,4 @@
-import { isApplication, isLibrary, isMaven, isNpm } from '../../src/utils/projectTypeUtils';
+import { isApplication, isDocker, isLibrary, isMaven, isNpm } from '../../src/utils/projectTypeUtils';
 import ProjectType from '../../src/types/ProjectType';
 
 describe('projectTypeUtils', () => {
@@ -11,6 +11,10 @@ describe('projectTypeUtils', () => {
             .toEqual(false);
         expect(isApplication(ProjectType.NpmLibrary))
             .toEqual(false);
+        expect(isApplication(ProjectType.DockerApplication))
+            .toEqual(true);
+        expect(isApplication(ProjectType.DockerImage))
+            .toEqual(false);
     });
 
     it('isLibrary', () => {
@@ -22,6 +26,10 @@ describe('projectTypeUtils', () => {
             .toEqual(true);
         expect(isLibrary(ProjectType.NpmLibrary))
             .toEqual(true);
+        expect(isLibrary(ProjectType.DockerApplication))
+            .toEqual(false);
+        expect(isLibrary(ProjectType.DockerImage))
+            .toEqual(false);
     });
 
     it('isMaven', () => {
@@ -33,6 +41,10 @@ describe('projectTypeUtils', () => {
             .toEqual(true);
         expect(isMaven(ProjectType.NpmLibrary))
             .toEqual(false);
+        expect(isMaven(ProjectType.DockerApplication))
+            .toEqual(false);
+        expect(isMaven(ProjectType.DockerImage))
+            .toEqual(false);
     });
 
     it('isNpm', () => {
@@ -43,6 +55,25 @@ describe('projectTypeUtils', () => {
         expect(isNpm(ProjectType.MavenLibrary))
             .toEqual(false);
         expect(isNpm(ProjectType.NpmLibrary))
+            .toEqual(true);
+        expect(isNpm(ProjectType.DockerApplication))
+            .toEqual(false);
+        expect(isNpm(ProjectType.DockerImage))
+            .toEqual(false);
+    });
+
+    it('isDocker', () => {
+        expect(isDocker(ProjectType.MavenApplication))
+            .toEqual(false);
+        expect(isDocker(ProjectType.NpmApplication))
+            .toEqual(false);
+        expect(isDocker(ProjectType.MavenLibrary))
+            .toEqual(false);
+        expect(isDocker(ProjectType.NpmLibrary))
+            .toEqual(false);
+        expect(isDocker(ProjectType.DockerApplication))
+            .toEqual(true);
+        expect(isDocker(ProjectType.DockerImage))
             .toEqual(true);
     });
 });
