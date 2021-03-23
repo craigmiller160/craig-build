8297214f8ce76e515104cd0cd0634d33390b71b4
Craig Miller <craigmiller160@gmail.com>
2021-03-22 20:14:30 -0400

    Modified dockerBuild task to better handle docker projects

--- src/stages/deploy/tasks/dockerBuild.ts
+++ src/stages/deploy/tasks/dockerBuild.ts
@@ -9,6 +9,7 @@ import shellEnv from 'shell-env';
 import EnvironmentVariables from '../../../types/EnvironmentVariables';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
+import * as O from 'fp-ts/Option';
 import stageName from '../stageName';
 import {isApplication, isDocker} from "../../../utils/projectTypeUtils";
 
@@ -27,11 +28,10 @@ const createDockerRemoveMatch = (name: string, version: string) =>
 const createDockerFindMatch = (name: string, version: string) =>
     `sudo docker image ls | grep ${name} | grep ${version} | cat`;
 
-const removeExistingDockerImages = (context: TaskContext<ProjectInfo>): E.Either<Error, string> => {
-    const tag = context.input.kubernetesDockerImage!!;
-    const endIndex = tag.lastIndexOf(':');
-    const name = tag.substring(0, endIndex);
-    const version = tag.substring(endIndex + 1);
+const removeExistingDockerImages = (context: TaskContext<ProjectInfo>, dockerImage: string): E.Either<Error, string> => {
+    const endIndex = dockerImage.lastIndexOf(':');
+    const name = dockerImage.substring(0, endIndex);
+    const version = dockerImage.substring(endIndex + 1);
 
     return pipe(
         runCommand(createDockerFindMatch(name, version)),
@@ -46,6 +46,21 @@ const removeExistingDockerImages = (context: TaskContext<ProjectInfo>): E.Either
     );
 };
 
+const getDockerImage = (projectInfo: ProjectInfo): O.Option<string> =>
+    pipe(
+        projectInfo.kubernetesDockerImage,
+        O.fromNullable,
+        O.fold(
+            () => {
+                if (isDocker(projectInfo.projectType)) {
+                    return O.some(`${projectInfo.name}:${projectInfo.version}`);
+                }
+                return O.none;
+            },
+            (dockerImage) => O.some(dockerImage)
+        )
+    );
+
 const dockerBuild: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo>) => {
     const deployDir = path.resolve(getCwd(), 'deploy');
     const {
@@ -53,19 +68,37 @@ const dockerBuild: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo
         NEXUS_DOCKER_PASSWORD
     } = shellEnv.sync<EnvironmentVariables>();
 
-    if (!context.input.kubernetesDockerImage) {
-        return TE.left(context.createBuildError('Missing Kubernetes Docker Image'));
-    }
-
     if (!NEXUS_DOCKER_USER || !NEXUS_DOCKER_PASSWORD) {
         return TE.left(context.createBuildError('Missing Docker credential environment variables'));
     }
 
     return pipe(
-        runCommand(createDockerLogin(NEXUS_DOCKER_USER, NEXUS_DOCKER_PASSWORD), { logOutput: true }),
-        E.chain(() => removeExistingDockerImages(context)),
-        E.chain(() => runCommand(createDockerBuild(context.input.kubernetesDockerImage!!), { cwd: deployDir, logOutput: true })),
-        E.chain(() => runCommand(createDockerPush(context.input.kubernetesDockerImage!!), { cwd: deployDir, logOutput: true })),
+        getDockerImage(context.input),
+        E.fromOption(() => context.createBuildError('Missing Kubernetes Docker Image')),
+        E.chain((dockerImage) =>
+            pipe(
+                runCommand(createDockerLogin(NEXUS_DOCKER_USER, NEXUS_DOCKER_PASSWORD), { logOutput: true }),
+                E.map(() => dockerImage)
+            )
+        ),
+        E.chain((dockerImage: string) =>
+            pipe(
+                removeExistingDockerImages(context, dockerImage),
+                E.map(() => dockerImage)
+            )
+        ),
+        E.chain((dockerImage: string) =>
+            pipe(
+                runCommand(createDockerBuild(dockerImage), { cwd: deployDir, logOutput: true }),
+                E.map(() => dockerImage)
+            )
+        ),
+        E.chain((dockerImage: string) =>
+            pipe(
+                runCommand(createDockerPush(dockerImage), { cwd: deployDir, logOutput: true }),
+                E.map(() => dockerImage)
+            )
+        ),
         TE.fromEither,
         TE.map(() => ({
             message: 'Docker build complete',
--- test/stages/deploy/tasks/dockerBuild.test.ts
+++ test/stages/deploy/tasks/dockerBuild.test.ts
@@ -2,7 +2,7 @@ import ProjectInfo from '../../../../src/types/ProjectInfo';
 import ProjectType from '../../../../src/types/ProjectType';
 import '@relmify/jest-fp-ts';
 import runCommand from '../../../../src/utils/runCommand';
-import dockerBuild, { TASK_NAME } from '../../../../src/stages/deploy/tasks/dockerBuild';
+import dockerBuild, {TASK_NAME} from '../../../../src/stages/deploy/tasks/dockerBuild';
 import BuildError from '../../../../src/error/BuildError';
 import getCwd from '../../../../src/utils/getCwd';
 import shellEnv from 'shell-env';
@@ -28,7 +28,7 @@ const syncMock = shellEnv.sync as jest.Mock;
 
 describe('dockerBuild task', () => {
     describe('validations',  () => {
-        it('has no kubernetesDockerImage', async () => {
+        it('has no docker image', async () => {
             getCwdMock.mockImplementation(() => '');
             syncMock.mockImplementationOnce(() => ({}));
             const result = await dockerBuild({
@@ -55,6 +55,52 @@ describe('dockerBuild task', () => {
             const result = await dockerBuild(baseProjectInfo)();
             expect(result).toEqualLeft(new BuildError('Missing Docker credential environment variables', stageName, TASK_NAME));
         });
+    });
+
+    it('builds and pushes docker image from Docker project, no existing matches', async () => {
+        getCwdMock.mockImplementation(() => '/');
+        syncMock.mockImplementationOnce(() => ({
+            NEXUS_DOCKER_USER: 'abc',
+            NEXUS_DOCKER_PASSWORD: 'def'
+        }));
+        runCommandMock.mockImplementation(() => E.right(''));
+
+        const projectInfo: ProjectInfo = {
+            ...baseProjectInfo,
+            projectType: ProjectType.DockerImage,
+            kubernetesDockerImage: undefined
+        };
+        const result = await dockerBuild(projectInfo)();
+        expect(result).toEqualRight(projectInfo);
+
+        expect(runCommandMock).toHaveBeenCalledTimes(4);
+        expect(runCommandMock).toHaveBeenNthCalledWith(
+            1,
+            'sudo docker login craigmiller160.ddns.net:30004 -u abc -p def',
+            {
+                logOutput: true
+            }
+        );
+        expect(runCommandMock).toHaveBeenNthCalledWith(
+            2,
+            'sudo docker image ls | grep my-project | grep 1.0.0 | cat'
+        );
+        expect(runCommandMock).toHaveBeenNthCalledWith(
+            3,
+            'sudo docker build --network=host -t my-project:1.0.0 .',
+            {
+                cwd: '/deploy',
+                logOutput: true
+            }
+        );
+        expect(runCommandMock).toHaveBeenNthCalledWith(
+            4,
+            'sudo docker push my-project:1.0.0',
+            {
+                cwd: '/deploy',
+                logOutput: true
+            }
+        );
     });
 
     it('builds and pushes docker image, no existing matches', async () => {
