391ccb0bda84908e3190be7e9d69f1142195adfc
Craig Miller <craigmiller160@gmail.com>
2021-12-13 16:46:56 -0500

    Integrated new task into stage

--- src/stages/deploy/index.ts
+++ src/stages/deploy/index.ts
@@ -8,13 +8,15 @@ import kubeDeploy from './tasks/kubeDeploy';
 import stageName from './stageName';
 import downloadArtifact from './tasks/downloadArtifact';
 import wait from '../../utils/wait';
+import bumpDockerBeta from './tasks/bumpDockerBeta';
 
 const deploy: StageFunction<ProjectInfo> = (context: StageContext<ProjectInfo>) => {
     context.logger('Waiting for Neuxs to update before deployment.');
     return pipe(
         wait(3000),
         TE.fromTask,
-        TE.chain(() => downloadArtifact(context.input)),
+        TE.chain(() => bumpDockerBeta(context.input)),
+        TE.chain(downloadArtifact),
         TE.chain(dockerBuild),
         TE.chain(kubeDeploy),
         TE.map((projectInfo) => ({
