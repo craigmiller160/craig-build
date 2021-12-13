acae64bbdeaf66718ae0da4d84d0c83ad73dfa4e
Craig Miller <craigmiller160@gmail.com>
2021-12-13 16:37:12 -0500

    Stubbed out more tests

--- src/stages/deploy/tasks/bumpDockerBeta.ts
+++ src/stages/deploy/tasks/bumpDockerBeta.ts
@@ -1,15 +1,29 @@
-import createTask, {TaskFunction} from '../../../common/execution/task';
+import createTask, {TaskFunction, TaskShouldExecuteFunction} from '../../../common/execution/task';
 import ProjectInfo from '../../../types/ProjectInfo';
 import {TaskContext} from '../../../common/execution/context';
 import stageName from '../stageName';
 import {executeIfPreRelease} from '../../../common/execution/commonTaskConditions';
+import {isApplication, isDocker} from '../../../utils/projectTypeUtils';
 
 export const TASK_NAME = 'Bump Docker Beta';
 
 const bumpDockerBeta: TaskFunction<ProjectInfo> = (context: TaskContext<ProjectInfo>) => {
+    // 1) Handle Maven & NPM, they're easy
+    // 2) Figure out how to handle Docker
+};
 
+const shouldExecute: TaskShouldExecuteFunction<ProjectInfo> = (input: ProjectInfo) => {
+    if (isDocker(input.projectType) || isApplication(input.projectType)) {
+        return undefined;
+    }
+
+    return {
+        message: 'Is not application or a Docker project',
+        defaultResult: input
+    };
 };
 
 export default createTask(stageName, TASK_NAME, bumpDockerBeta, [
+    shouldExecute,
     executeIfPreRelease
 ]);
--- test/stages/deploy/tasks/bumpDockerBeta.test.ts
+++ test/stages/deploy/tasks/bumpDockerBeta.test.ts
@@ -1,7 +1,37 @@
 export {};
 
 describe('bumpDockerBeta', () => {
-    it('test', () => {
+    it('bumps docker beta for NPM', () => {
         throw new Error();
+    });
+
+    it('bumps docker beta for Maven', () => {
+        throw new Error();
+    });
+
+    it('bumps docker beta for Docker', () => {
+        throw new Error();
+    });
+
+    describe('skip execution', () => {
+        it('is not docker, application, or pre-release', () => {
+            throw new Error();
+        });
+
+        it('is pre-release docker', () => {
+            throw new Error();
+        });
+
+        it('is release docker', () => {
+            throw new Error();
+        });
+
+        it('is pre-release non-docker application', () => {
+            throw new Error();
+        });
+
+        it('is release non-docker application', () => {
+            throw new Error();
+        });
     });
 });
