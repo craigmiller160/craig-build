fb32ac49d39b8dcd4cf58f7db62031c9e29ca225
Craig Miller <craigmiller160@gmail.com>
2021-12-19 17:56:47 -0500

    Added lint config files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    rules: {
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'no-console': ['error']
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    vueIndentScriptAndStyle: true
+};
