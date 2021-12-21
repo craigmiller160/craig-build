b4cd3ee8b5846480014007824603ed0dac2278ef
Craig Miller <craigmiller160@gmail.com>
2021-12-21 14:39:11 -0500

    Working on execution flow

--- src/execute.ts
+++ src/execute.ts
@@ -1,9 +1,31 @@
 import { BuildContext } from './context/BuildContext';
 import * as TE from 'fp-ts/TaskEither';
+import { Stage } from './stages/Stage';
+import { getCommandInfo } from './stages/getCommandInfo';
+import * as A from 'fp-ts/Array';
+import { pipe } from 'fp-ts/function';
+
+const stages: Stage[] = [getCommandInfo];
+
+const executeStage = (
+	contextTE: TE.TaskEither<Error, BuildContext>,
+	stage: Stage
+): TE.TaskEither<Error, BuildContext> =>
+	pipe(
+		contextTE,
+		// TODO pre-logging
+		TE.chain((context) => stage(context))
+		// TODO post-logging
+		// TODO log error
+	);
 
 export const execute = (
 	context: BuildContext
-): TE.TaskEither<Error, BuildContext> => {
-	// TODO finish this
-	throw new Error();
-};
+): TE.TaskEither<Error, BuildContext> =>
+	pipe(
+		stages,
+		A.reduce(
+			TE.right<Error, BuildContext>(context),
+			(ctxTE, stage: Stage) => executeStage(ctxTE, stage)
+		)
+	);
