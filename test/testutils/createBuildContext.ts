7ce36bb6a8ba0f8253a6c961af2cbed76c659a13
Craig Miller <craigmiller160@gmail.com>
2021-12-21 15:36:47 -0500

    Plugging holes from build context changes

--- test/testutils/createBuildContext.ts
+++ test/testutils/createBuildContext.ts
@@ -3,6 +3,7 @@ import { BuildContext } from '../../src/context/BuildContext';
 import { CommandInfo } from '../../src/context/CommandInfo';
 import { CommandType } from '../../src/context/CommandType';
 import * as O from 'fp-ts/Option';
+import { BuildToolInfo } from '../../src/context/BuildToolInfo';
 
 const defaultOptions: OptionValues = {
 	fullBuild: true
@@ -10,16 +11,24 @@ const defaultOptions: OptionValues = {
 const defaultCommandInfo: CommandInfo = {
 	type: CommandType.FULL_BUILD
 };
+const defaultBuildToolInfo: BuildToolInfo = {
+	name: 'craig-build',
+	version: '2.0.0',
+	isPreRelease: false
+};
 
 const defaultBuildContext: BuildContext = {
 	options: defaultOptions,
-	commandInfo: O.some(defaultCommandInfo)
+	commandInfo: O.some(defaultCommandInfo),
+	buildToolInfo: O.some(defaultBuildToolInfo)
 };
 
 export const createBuildContext = ({
 	options = defaultOptions,
-	commandInfo = O.some(defaultCommandInfo)
+	commandInfo = O.some(defaultCommandInfo),
+	buildToolInfo = O.some(defaultBuildToolInfo)
 }: BuildContext = defaultBuildContext): BuildContext => ({
 	options,
-	commandInfo
+	commandInfo,
+	buildToolInfo
 });
