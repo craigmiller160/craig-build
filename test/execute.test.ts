7dcd19d2ad3b8ea0d8e7848f5a134f89c80c6646
Craig Miller <craigmiller160@gmail.com>
2021-12-21 15:11:09 -0500

    Stubbed out tests

---
+++ test/execute.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('execute', () => {
+	it('executes all stages successfully', () => {
+		throw new Error();
+	});
+
+	it('aborts execution after stage error', () => {
+		throw new Error();
+	});
+});
