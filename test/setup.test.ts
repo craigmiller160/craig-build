1d0fe1962b6cfeb184a265a1068aa802407de0c4
Craig Miller <craigmiller160@gmail.com>
2021-12-21 13:52:24 -0500

    Stubbed out tests

---
+++ test/setup.test.ts
@@ -0,0 +1,17 @@
+import { setupBuildContext } from '../src/setup';
+
+describe('setup', () => {
+	describe('setupBuildContext', () => {
+		it('setup for FULL_BUILD', () => {
+			throw new Error();
+		});
+
+		it('setup for DOCKER_ONLY', () => {
+			throw new Error();
+		});
+
+		it('setup for KUBERNETES_ONLY', () => {
+			throw new Error();
+		});
+	});
+});
