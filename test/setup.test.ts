684498748b3bf8ad431cffb337d3cc92b3984dd0
Craig Miller <craigmiller160@gmail.com>
2021-12-21 13:54:58 -0500

    Implemented tests

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -1,17 +1,43 @@
 import { setupBuildContext } from '../src/setup';
+import { OptionValues } from 'commander';
+import { CommandType } from '../src/context/CommandType';
 
 describe('setup', () => {
 	describe('setupBuildContext', () => {
 		it('setup for FULL_BUILD', () => {
-			throw new Error();
+			const options: OptionValues = {
+				fullBuild: true
+			};
+			const buildContext = setupBuildContext(options);
+			expect(buildContext).toEqual({
+				commandInfo: {
+					type: CommandType.FULL_BUILD
+				}
+			});
 		});
 
 		it('setup for DOCKER_ONLY', () => {
-			throw new Error();
+			const options: OptionValues = {
+				dockerOnly: true
+			};
+			const buildContext = setupBuildContext(options);
+			expect(buildContext).toEqual({
+				commandInfo: {
+					type: CommandType.DOCKER_ONLY
+				}
+			});
 		});
 
 		it('setup for KUBERNETES_ONLY', () => {
-			throw new Error();
+			const options: OptionValues = {
+				kubernetesOnly: true
+			};
+			const buildContext = setupBuildContext(options);
+			expect(buildContext).toEqual({
+				commandInfo: {
+					type: CommandType.KUBERNETES_ONLY
+				}
+			});
 		});
 	});
 });
