92f0388ad8ac8238d1bf87d0b6e6746f69813ce1
Craig Miller <craigmiller160@gmail.com>
2021-12-21 14:22:36 -0500

    Implemented new tests

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -1,43 +1,16 @@
 import { setupBuildContext } from '../src/setup';
 import { OptionValues } from 'commander';
-import { CommandType } from '../src/context/CommandType';
+import * as O from 'fp-ts/Option';
 
 describe('setup', () => {
-	describe('setupBuildContext', () => {
-		it('setup for FULL_BUILD', () => {
-			const options: OptionValues = {
-				fullBuild: true
-			};
-			const buildContext = setupBuildContext(options);
-			expect(buildContext).toEqual({
-				commandInfo: {
-					type: CommandType.FULL_BUILD
-				}
-			});
-		});
-
-		it('setup for DOCKER_ONLY', () => {
-			const options: OptionValues = {
-				dockerOnly: true
-			};
-			const buildContext = setupBuildContext(options);
-			expect(buildContext).toEqual({
-				commandInfo: {
-					type: CommandType.DOCKER_ONLY
-				}
-			});
-		});
-
-		it('setup for KUBERNETES_ONLY', () => {
-			const options: OptionValues = {
-				kubernetesOnly: true
-			};
-			const buildContext = setupBuildContext(options);
-			expect(buildContext).toEqual({
-				commandInfo: {
-					type: CommandType.KUBERNETES_ONLY
-				}
-			});
+	it('setupBuildContext', () => {
+		const options: OptionValues = {
+			kubernetesOnly: true
+		};
+		const buildContext = setupBuildContext(options);
+		expect(buildContext).toEqual({
+			options,
+			commandInfo: O.none
 		});
 	});
 });
