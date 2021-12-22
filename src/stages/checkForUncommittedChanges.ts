41252136489ff0cde4bb8805de09bb4fe4089a7f
Craig Miller <craigmiller160@gmail.com>
2021-12-22 13:06:47 -0500

    Implemented logic for checkForUncommittedChanges

--- src/stages/checkForUncommittedChanges.ts
+++ src/stages/checkForUncommittedChanges.ts
@@ -1,9 +1,27 @@
 import { Stage, StageFunction } from './Stage';
+import { runCommand } from '../command/runCommand';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { match, when } from 'ts-pattern';
 
-const execute: StageFunction = (context) => {
-	// TODO finish this
-	throw new Error();
-};
+const GIT_COMMAND = 'git status --porcelain';
+
+const handleCommandResult = (message: string): E.Either<Error, string> =>
+	match(message)
+		.with(
+			when<string>((_) => _.length === 0),
+			() => E.right('')
+		)
+		.otherwise(() => E.left(new Error('Cannot run with uncommitted changes')));
+
+const execute: StageFunction = (context) =>
+	pipe(
+		runCommand(GIT_COMMAND),
+		E.chain(handleCommandResult),
+		TE.fromEither,
+		TE.map(() => context)
+	);
 
 export const checkForUncommittedChanges: Stage = {
 	name: 'Check For Uncommitted Changes',
