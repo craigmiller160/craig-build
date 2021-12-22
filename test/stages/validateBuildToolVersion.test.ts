a736ca6d15bf52ed4bb57a36b1bd834f20464ad2
Craig <craigmiller160@gmail.com>
2021-12-21 20:40:07 -0500

    Working on implementing tests

--- test/stages/validateBuildToolVersion.test.ts
+++ test/stages/validateBuildToolVersion.test.ts
@@ -1,19 +1,31 @@
-export {};
+import { createBuildContext } from '../testutils/createBuildContext';
+import * as O from 'fp-ts/Option';
+import { validateBuildToolVersion } from '../../src/stages/validateBuildToolVersion';
 
 describe('validateBuildToolVersion', () => {
-	it('tool version is highest release version', () => {
+	it('tool version is highest release version', async () => {
+		const buildContext = createBuildContext({
+			buildToolInfo: O.some({
+				group: 'craigmiller160',
+				name: 'craig-build',
+				version: '1.0.0',
+				isPreRelease: false
+			})
+		});
+
+		const result = await validateBuildToolVersion.execute(buildContext)();
+		expect(result).toEqual(buildContext);
+	});
+
+	it('tool version is not highest release version', async () => {
 		throw new Error();
 	});
 
-	it('tool version is not highest release version', () => {
+	it('user allows tool with pre-release version to run', async () => {
 		throw new Error();
 	});
 
-	it('user allows tool with pre-release version to run', () => {
-		throw new Error();
-	});
-
-	it('user does not allow tool with pre-release version to run', () => {
+	it('user does not allow tool with pre-release version to run', async () => {
 		throw new Error();
 	});
 });
