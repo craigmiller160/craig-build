23460a33836dddb3d904963adf2e2207b48d59ad
Craig Miller <craigmiller160@gmail.com>
2021-12-22 14:12:29 -0500

    Added ProjectType and ProjectInfo to BuildContext

--- src/context/ProjectInfo.ts
+++ src/context/ProjectInfo.ts
@@ -1,15 +1,6 @@
-import { ProjectType } from './ProjectType';
-
-export interface Dependency {
-	name: string;
-	version: string;
-}
-
 export interface ProjectInfo {
-	projectType: ProjectType;
 	group: string;
 	name: string;
 	version: string;
 	isPreRelease: boolean;
-	dependencies: Dependency[];
 }
