0e26b51d2a600b55986a66459d59cdfce4ab3d8f
Craig Miller <craigmiller160@gmail.com>
2021-12-22 14:36:50 -0500

    Working on implementing stage

--- src/stages/getProjectType.ts
+++ src/stages/getProjectType.ts
@@ -1,9 +1,47 @@
 import { Stage, StageFunction } from './Stage';
+import path from 'path';
+import { getCwd } from '../command/getCwd';
+import fs from 'fs';
+import { match, when } from 'ts-pattern';
+import { ProjectType } from '../context/ProjectType';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
 
-const execute: StageFunction = (context) => {
-	// TODO finish this
-	throw new Error();
-};
+const NPM_PROJECT_FILE = 'package.json';
+const MVN_PROJECT_FILE = 'pom.xml';
+const DOCKER_PROJECT_FILE = 'docker.json';
+const DEPLOY_PATH = path.join('deploy', 'deployment.yml');
+
+const fileExists = (cwd: string, file: string): boolean =>
+	fs.existsSync(path.resolve(cwd, file));
+
+const checkProjectFilesForType = (): E.Either<Error, ProjectType> =>
+	match(getCwd())
+		.with(
+			when<string>(
+				(_) =>
+					fileExists(_, NPM_PROJECT_FILE) &&
+					fileExists(_, DEPLOY_PATH)
+			),
+			() => E.right(ProjectType.NpmApplication)
+		)
+		.with(
+			when<string>((_) => fileExists(_, NPM_PROJECT_FILE)),
+			() => E.right(ProjectType.NpmLibrary)
+		)
+		.otherwise(() => E.left(new Error('Unable to identify ProjectType')));
+
+const execute: StageFunction = (context) =>
+	pipe(
+		checkProjectFilesForType(),
+		E.map((projectType) => ({
+			...context,
+			projectType: O.some(projectType)
+		})),
+		TE.fromEither
+	);
 
 export const getProjectType: Stage = {
 	name: 'Get Project Type',
