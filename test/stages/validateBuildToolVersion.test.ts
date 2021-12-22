6b97a93e87e3a8a80448017a27007463e604de5d
Craig <craigmiller160@gmail.com>
2021-12-21 21:01:41 -0500

    Implemented more test logic

--- test/stages/validateBuildToolVersion.test.ts
+++ test/stages/validateBuildToolVersion.test.ts
@@ -1,20 +1,57 @@
 import { createBuildContext } from '../testutils/createBuildContext';
 import * as O from 'fp-ts/Option';
 import { validateBuildToolVersion } from '../../src/stages/validateBuildToolVersion';
+import { searchForNpmReleases } from '../../src/services/NexusRepoApi';
+import NexusSearchResult, {
+	NexusSearchResultItem
+} from '../../old-src/types/NexusSearchResult';
+import * as TE from 'fp-ts/TaskEither';
+import '@relmify/jest-fp-ts';
+
+jest.mock('../../src/services/NexusRepoApi', () => ({
+	searchForNpmReleases: jest.fn()
+}));
+
+const searchForNpmReleasesMock = searchForNpmReleases as jest.Mock;
+
+const createNexusItem = (version: string): NexusSearchResultItem => ({
+	name: '',
+	version,
+	repository: '',
+	group: '',
+	format: '',
+	id: '',
+	assets: []
+});
 
 describe('validateBuildToolVersion', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
 	it('tool version is highest release version', async () => {
 		const buildContext = createBuildContext({
 			buildToolInfo: O.some({
 				group: 'craigmiller160',
 				name: 'craig-build',
-				version: '1.0.0',
+				version: '1.1.0',
 				isPreRelease: false
 			})
 		});
+		const nexusResult: NexusSearchResult = {
+			items: [createNexusItem('1.0.0')]
+		};
+		searchForNpmReleasesMock.mockImplementation(() =>
+			TE.right(nexusResult)
+		);
 
 		const result = await validateBuildToolVersion.execute(buildContext)();
-		expect(result).toEqual(buildContext);
+		expect(result).toEqualRight(buildContext);
+
+		expect(searchForNpmReleasesMock).toHaveBeenCalledWith(
+			'craigmiller160',
+			'craig-build'
+		);
 	});
 
 	it('tool version is not highest release version', async () => {
