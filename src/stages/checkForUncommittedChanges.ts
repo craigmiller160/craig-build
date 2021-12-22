dff38b385b1bcc55cbee98b36a55dbc5afa88611
Craig Miller <craigmiller160@gmail.com>
2021-12-22 13:12:33 -0500

    Feature/cb 17 (#30)
    * Stubbed out new stage and test
    
    * Working on command running utility code
    
    * Working more on runCommand
    
    * Fully implemented runCommand
    
    * More work on runCommand
    
    * Transferred over runCommand tests
    
    * Implemented logic for checkForUncommittedChanges
    
    * Fixed lint errors
    
    * Setup re-usable mock
    
    * Implemented tests
    
    * Added validate script
    
    * Fixed lint errors

---
+++ src/stages/checkForUncommittedChanges.ts
@@ -0,0 +1,31 @@
+import { Stage, StageFunction } from './Stage';
+import { runCommand } from '../command/runCommand';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { match, when } from 'ts-pattern';
+
+export const GIT_COMMAND = 'git status --porcelain';
+
+const handleCommandResult = (message: string): E.Either<Error, string> =>
+	match(message)
+		.with(
+			when<string>((_) => _.length === 0),
+			() => E.right('')
+		)
+		.otherwise(() =>
+			E.left(new Error('Cannot run with uncommitted changes'))
+		);
+
+const execute: StageFunction = (context) =>
+	pipe(
+		runCommand(GIT_COMMAND),
+		E.chain(handleCommandResult),
+		TE.fromEither,
+		TE.map(() => context)
+	);
+
+export const checkForUncommittedChanges: Stage = {
+	name: 'Check For Uncommitted Changes',
+	execute
+};
--- src/stages/index.ts
+++ src/stages/index.ts
@@ -2,9 +2,11 @@ import { Stage } from './Stage';
 import { getCommandInfo } from './getCommandInfo';
 import { getBuildToolInfo } from './getBuildToolInfo';
 import { validateBuildToolVersion } from './validateBuildToolVersion';
+import { checkForUncommittedChanges } from './checkForUncommittedChanges';
 
 export const STAGES: Stage[] = [
 	getCommandInfo,
 	getBuildToolInfo,
-	validateBuildToolVersion
+	validateBuildToolVersion,
+	checkForUncommittedChanges
 ];
