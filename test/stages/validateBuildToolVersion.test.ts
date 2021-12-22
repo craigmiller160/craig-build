d3648ddf13b9d953b7121d32ac70729fa14c1de5
Craig <craigmiller160@gmail.com>
2021-12-21 20:36:09 -0500

    Stubbed out more tests

--- test/stages/validateBuildToolVersion.test.ts
+++ test/stages/validateBuildToolVersion.test.ts
@@ -1,7 +1,19 @@
 export {};
 
 describe('validateBuildToolVersion', () => {
-	it('test', () => {
+	it('tool version is highest release version', () => {
+		throw new Error();
+	});
+
+	it('tool version is not highest release version', () => {
+		throw new Error();
+	});
+
+	it('user allows tool with pre-release version to run', () => {
+		throw new Error();
+	});
+
+	it('user does not allow tool with pre-release version to run', () => {
 		throw new Error();
 	});
 });
