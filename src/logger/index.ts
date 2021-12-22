6df575ac7e7af8723fa052119334cc20b5863216
Craig Miller <craigmiller160@gmail.com>
2021-12-22 12:17:20 -0500

    Making more progress with logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,30 @@
-import pino from 'pino';
+// import pino from 'pino';
+//
+// export const logger = pino({
+// 	level: 'debug',
+// 	transport: {
+// 		target: 'pino-pretty',
+// 		options: {
+// 			translateTime: 'yyyy-mm-dd HH:MM:ss.l',
+// 			colorize: true
+// 		}
+// 	}
+// });
 
-export const logger = pino({
+import { createLogger, transports, format } from 'winston';
+
+const myFormat = format.printf(
+	({ level, message, timestamp }) => `${timestamp} [${level}] - ${message}`
+);
+
+export const logger = createLogger({
 	level: 'debug',
-	transport: {
-		target: 'pino-pretty',
-		options: {
-			translateTime: 'yyyy-mm-dd HH:MM:ss.l',
-			colorize: true
-		}
-	}
+	format: format.combine(
+		format.timestamp(),
+		format.colorize(),
+		format.prettyPrint(),
+		format.simple(),
+		myFormat
+	),
+	transports: [new transports.Console()]
 });
