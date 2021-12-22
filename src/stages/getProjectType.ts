ca6f9022a4c895e2ccacf1c95ded938d02c2cac0
Craig Miller <craigmiller160@gmail.com>
2021-12-22 14:18:59 -0500

    Stubbed out stage and tests

---
+++ src/stages/getProjectType.ts
@@ -0,0 +1,11 @@
+import { Stage, StageFunction } from './Stage';
+
+const execute: StageFunction = (context) => {
+	// TODO finish this
+	throw new Error();
+};
+
+export const getProjectType: Stage = {
+	name: 'Get Project Type',
+	execute
+};
---
+++ test/stages/getProjectType.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+describe('getProjectType', () => {
+	it('is NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('is MavenLibrary', () => {
+		throw new Error();
+	});
+
+	it('is NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('is MavenApplication', () => {
+		throw new Error();
+	});
+
+	it('is DockerApplication', () => {
+		throw new Error();
+	});
+
+	it('is DockerImage', () => {
+		throw new Error();
+	});
+});
