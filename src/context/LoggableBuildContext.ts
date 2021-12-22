0222ed5a9048a41226e340766504c8a47a4a128f
Craig Miller <craigmiller160@gmail.com>
2021-12-22 14:16:32 -0500

    Fixed context logging

--- src/context/LoggableBuildContext.ts
+++ src/context/LoggableBuildContext.ts
@@ -3,15 +3,21 @@ import { CommandInfo } from './CommandInfo';
 import { BuildContext } from './BuildContext';
 import { getOrNull } from '../functions/OptionUtils';
 import { BuildToolInfo } from './BuildToolInfo';
+import { ProjectType } from './ProjectType';
+import { ProjectInfo } from './ProjectInfo';
 
 export interface LoggableBuildContext {
 	options: OptionValues;
 	commandInfo: CommandInfo | null;
 	buildToolInfo: BuildToolInfo | null;
+	projectType: ProjectType | null;
+	projectInfo: ProjectInfo | null;
 }
 
 export const toLoggable = (context: BuildContext): LoggableBuildContext => ({
 	options: context.options,
 	commandInfo: getOrNull(context.commandInfo),
-	buildToolInfo: getOrNull(context.buildToolInfo)
+	buildToolInfo: getOrNull(context.buildToolInfo),
+	projectType: getOrNull(context.projectType),
+	projectInfo: getOrNull(context.projectInfo)
 });
