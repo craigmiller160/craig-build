ccd2c76e512b4f12c1ccfab435942bbccce0e213
Craig Miller <craigmiller160@gmail.com>
2021-12-22 12:59:42 -0500

    Fully implemented runCommand

--- src/command/runCommand.ts
+++ src/command/runCommand.ts
@@ -3,6 +3,9 @@ import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { getCwd } from './getCwd';
+import { logger } from '../logger';
+import { match, when } from 'ts-pattern';
+import { SpawnSyncReturns } from 'child_process';
 
 export interface CommandOptions {
 	printOutput: boolean;
@@ -31,14 +34,46 @@ const getCommandOptions = (options?: Partial<CommandOptions>): CommandOptions =>
 		)
 	);
 
+const handleSuccess = (result: SpawnSyncReturns<Buffer>) => {
+	const output = result.stdout.toString();
+	console.log(output); // eslint-disable-line no-console
+	return E.right(output);
+};
+
+const handleFailure = (result: SpawnSyncReturns<Buffer>) => {
+	const output = result.stderr.toString();
+	console.log(output); // eslint-disable-line no-console
+	return E.left(new Error(output));
+};
+
+const handleNoStatus = (): E.Either<Error, string> =>
+	E.left(new Error('No status code returned from command'));
+
 export const runCommand = (
 	command: string,
 	options?: Partial<CommandOptions>
 ): E.Either<Error, string> => {
+	logger.debug(`Running Command: ${command}`);
 	const { printOutput, cwd } = getCommandOptions(options);
 
 	const result = spawn.sync('bash', ['-c', command], {
 		stdio: 'inherit',
 		cwd
 	});
+
+	const status = pipe(
+		O.fromNullable(result.status),
+		O.getOrElse(() => -1)
+	);
+
+	return match(status)
+		.with(
+			when<number>((_) => _ === 0),
+			() => handleSuccess(result)
+		)
+		.with(
+			when<number>((_) => _ === -1),
+			handleNoStatus
+		)
+		.otherwise(() => handleFailure(result));
 };
