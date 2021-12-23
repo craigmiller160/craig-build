88893b7ad8ecca2184d17cf9d49e98c3605acf9d
Craig Miller <craigmiller160@gmail.com>
2021-12-23 16:00:55 -0500

    Stubbed out tests

---
+++ test/context/BuildContext.test.ts
@@ -0,0 +1,25 @@
+import { fromIncompleteContext } from '../../src/context/BuildContext';
+
+describe('BuildContext', () => {
+	describe('fromIncompleteContext', () => {
+		it('context is complete', () => {
+			throw new Error();
+		});
+
+		it('context is missing commandInfo', () => {
+			throw new Error();
+		});
+
+		it('context is missing buildToolInfo', () => {
+			throw new Error();
+		});
+
+		it('context is missing projectType', () => {
+			throw new Error();
+		});
+
+		it('context is missing ProjectInfo', () => {
+			throw new Error();
+		});
+	});
+});
