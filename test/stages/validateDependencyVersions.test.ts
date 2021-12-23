2127a6a9ea014681a8934e39b4006f9a64132732
Craig Miller <craigmiller160@gmail.com>
2021-12-23 10:34:44 -0500

    Stubbed out tests

---
+++ test/stages/validateDependencyVersions.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+describe('validateDependencyVersions', () => {
+	it('skips validation for pre-release maven project', () => {
+		throw new Error();
+	});
+
+	it('skips validation for pre-release npm project', () => {
+		throw new Error();
+	});
+
+	it('validates release dependencies for maven project', () => {
+		throw new Error();
+	});
+
+	it('validates release dependencies for npm project', () => {
+		throw new Error();
+	});
+});
