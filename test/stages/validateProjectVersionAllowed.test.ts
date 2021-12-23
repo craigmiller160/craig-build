e6dd3192a9328f1498471b44f1a3af2971d6d406
Craig Miller <craigmiller160@gmail.com>
2021-12-23 13:46:59 -0500

    Implemented first test

--- test/stages/validateProjectVersionAllowed.test.ts
+++ test/stages/validateProjectVersionAllowed.test.ts
@@ -5,6 +5,12 @@ import {
 } from '../../src/services/NexusRepoApi';
 import '@relmify/jest-fp-ts';
 import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import ProjectType from '../../old-src/types/ProjectType';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { validateProjectVersionAllowed } from '../../src/stages/validateProjectVersionAllowed';
 
 jest.mock('../../src/services/NexusRepoApi', () => ({
 	searchForDockerReleases: jest.fn(),
@@ -16,46 +22,62 @@ const searchForDockerReleasesMock = searchForDockerReleases as jest.Mock;
 const searchForMavenReleasesMock = searchForMavenReleases as jest.Mock;
 const searchForNpmReleasesMock = searchForNpmReleases as jest.Mock;
 
-const buildContext = createBuildContext();
+const baseBuildContext = createBuildContext();
 
 describe('validateProjectVersionAllowed', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
 	});
 
-	it('allows npm pre-release version', () => {
+	it('allows npm pre-release version', async () => {
+		searchForNpmReleasesMock.mockImplementation(() => E.left(new Error()));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: O.some(ProjectType.NpmApplication),
+			projectInfo: pipe(
+				baseBuildContext.projectInfo,
+				O.map((_) => ({
+					..._,
+					isPreRelease: true
+				}))
+			)
+		};
+
+		const result = await validateProjectVersionAllowed.execute(
+			buildContext
+		)();
+		expect(result).toEqualRight(buildContext);
+	});
+
+	it('allows maven pre-release version', async () => {
 		throw new Error();
 	});
 
-	it('allows maven pre-release version', () => {
+	it('allows docker pre-release version', async () => {
 		throw new Error();
 	});
 
-	it('allows docker pre-release version', () => {
+	it('allows npm release version with no conflicts', async () => {
 		throw new Error();
 	});
 
-	it('allows npm release version with no conflicts', () => {
+	it('allows maven release version with no conflicts', async () => {
 		throw new Error();
 	});
 
-	it('allows maven release version with no conflicts', () => {
+	it('allows docker release version with no conflicts', async () => {
 		throw new Error();
 	});
 
-	it('allows docker release version with no conflicts', () => {
+	it('rejects npm release version with conflict', async () => {
 		throw new Error();
 	});
 
-	it('rejects npm release version with conflict', () => {
+	it('rejects maven release version with conflicts', async () => {
 		throw new Error();
 	});
 
-	it('rejects maven release version with conflicts', () => {
-		throw new Error();
-	});
-
-	it('rejects docker release version with conflicts', () => {
+	it('rejects docker release version with conflicts', async () => {
 		throw new Error();
 	});
 });
