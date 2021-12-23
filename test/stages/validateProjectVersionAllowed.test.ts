65c6e24cb79c9a1471ed9f30d9d604afafbfa64d
Craig Miller <craigmiller160@gmail.com>
2021-12-23 13:14:21 -0500

    Stubbed out lots of tests

--- test/stages/validateProjectVersionAllowed.test.ts
+++ test/stages/validateProjectVersionAllowed.test.ts
@@ -1,7 +1,39 @@
 export {};
 
 describe('validateProjectVersionAllowed', () => {
-	it('test', () => {
+	it('allows npm pre-release version', () => {
+		throw new Error();
+	});
+
+	it('allows maven pre-release version', () => {
+		throw new Error();
+	});
+
+	it('allows docker pre-release version', () => {
+		throw new Error();
+	});
+
+	it('allows npm release version with no conflicts', () => {
+		throw new Error();
+	});
+
+	it('allows maven release version with no conflicts', () => {
+		throw new Error();
+	});
+
+	it('allows docker release version with no conflicts', () => {
+		throw new Error();
+	});
+
+	it('rejects npm release version with conflict', () => {
+		throw new Error();
+	});
+
+	it('rejects maven release version with conflicts', () => {
+		throw new Error();
+	});
+
+	it('rejects docker release version with conflicts', () => {
 		throw new Error();
 	});
 });
