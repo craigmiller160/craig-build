19a87a82945ca7c799016296c3d4ba5c7401f4e4
Craig Miller <craigmiller160@gmail.com>
2021-12-23 16:05:42 -0500

    Implemented a bunch of tests

--- src/context/BuildContext.ts
+++ src/context/BuildContext.ts
@@ -2,14 +2,7 @@ import { CommandInfo } from './CommandInfo';
 import { BuildToolInfo } from './BuildToolInfo';
 import { ProjectType } from './ProjectType';
 import { ProjectInfo } from './ProjectInfo';
-import { IncompleteBuildContext } from './IncompleteBuildContext';
-import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
-import { pipe } from 'fp-ts/function';
 import { Context } from './Context';
-
-type Container2 = [CommandInfo, BuildToolInfo];
-type Container3 = [CommandInfo, BuildToolInfo, ProjectType];
 
 export interface BuildContext extends Context {
 	commandInfo: CommandInfo;
@@ -18,46 +11,4 @@ export interface BuildContext extends Context {
 	projectInfo: ProjectInfo;
 }
 
-// TODO write tests for this
-export const fromIncompleteContext = (
-	context: IncompleteBuildContext
-): E.Either<Error, BuildContext> =>
-	pipe(
-		context.commandInfo,
-		O.chain((commandInfo) =>
-			pipe(
-				context.buildToolInfo,
-				O.map(
-					(buildToolInfo): Container2 => [commandInfo, buildToolInfo]
-				)
-			)
-		),
-		O.chain(([commandInfo, buildToolInfo]) =>
-			pipe(
-				context.projectType,
-				O.map(
-					(projectType): Container3 => [
-						commandInfo,
-						buildToolInfo,
-						projectType
-					]
-				)
-			)
-		),
-		O.chain(([commandInfo, buildToolInfo, projectType]) =>
-			pipe(
-				context.projectInfo,
-				O.map(
-					(projectInfo): BuildContext => ({
-						commandInfo,
-						buildToolInfo,
-						projectType,
-						projectInfo
-					})
-				)
-			)
-		),
-		E.fromOption(
-			() => new Error('IncompleteBuildContext has not yet been completed')
-		)
-	);
+
