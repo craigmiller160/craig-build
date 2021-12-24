23dfd447c77abe037b25b12e24b3a61cf96d90b4
Craig Miller <craigmiller160@gmail.com>
2021-12-24 15:18:42 -0500

    Stubbed out tests

---
+++ test/stages/buildArtifact.test.ts
@@ -0,0 +1,15 @@
+export {};
+
+describe('buildArtifact', () => {
+	it('skips docker projects', () => {
+		throw new Error();
+	});
+
+	it('builds maven artifact', () => {
+		throw new Error();
+	});
+
+	it('builds npm artifact', () => {
+		throw new Error();
+	});
+});
