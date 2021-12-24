b5f043f5c0cd3fe1e7ed8cdf40170e6bb468c0ca
Craig Miller <craigmiller160@gmail.com>
2021-12-24 15:27:10 -0500

    Implemented tests and updated command logging

--- test/stages/buildArtifact.test.ts
+++ test/stages/buildArtifact.test.ts
@@ -1,15 +1,50 @@
-export {};
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { ProjectType } from '../../src/context/ProjectType';
+import { buildArtifact } from '../../src/stages/buildArtifact';
+import '@relmify/jest-fp-ts';
+import { runCommandMock } from '../testutils/runCommandMock';
+
+const baseBuildContext = createBuildContext();
 
 describe('buildArtifact', () => {
-	it('skips docker projects', () => {
-		throw new Error();
+	beforeEach(() => {
+		jest.resetAllMocks();
 	});
 
-	it('builds maven artifact', () => {
-		throw new Error();
+	it('skips docker projects', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.DockerApplication
+		};
+
+		const result = await buildArtifact.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
 	});
 
-	it('builds npm artifact', () => {
-		throw new Error();
+	it('builds maven artifact', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenApplication
+		};
+
+		const result = await buildArtifact.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith('mvn clean deploy');
+	});
+
+	it('builds npm artifact', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmApplication
+		};
+
+		const result = await buildArtifact.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith('yarn build');
 	});
 });
