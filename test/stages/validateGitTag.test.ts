07075578016994ade4cce29f21a9da80d60c3f74
Craig Miller <craigmiller160@gmail.com>
2021-12-24 15:09:08 -0500

    Implemented remaining tests

--- test/stages/validateGitTag.test.ts
+++ test/stages/validateGitTag.test.ts
@@ -2,8 +2,11 @@ import { runCommandMock } from '../testutils/runCommandMock';
 import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import { validateGitTag } from '../../src/stages/validateGitTag';
+import * as E from 'fp-ts/Either';
+import '@relmify/jest-fp-ts';
 
 const baseBuildContext = createBuildContext();
+const versions: string[] = ['v0.0.1', 'v0.1.1'];
 
 describe('validateGitTag', () => {
 	beforeEach(() => {
@@ -20,14 +23,42 @@ describe('validateGitTag', () => {
 		};
 
 		const result = await validateGitTag.execute(buildContext)();
-		expect(result).toEqual(buildContext);
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
 	});
 
 	it('confirms there is no existing tag for release version', async () => {
-		throw new Error();
+		runCommandMock.mockImplementation(() => E.right(versions));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: false,
+				version: '1.0.0'
+			}
+		};
+
+		const result = await validateGitTag.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith('git tag');
 	});
 
 	it('finds an existing tag for release version', async () => {
-		throw new Error();
+		runCommandMock.mockImplementation(() => E.right(versions));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: false,
+				version: '0.1.1'
+			}
+		};
+
+		const result = await validateGitTag.execute(buildContext)();
+		expect(result).toEqualLeft(new Error());
+
+		expect(runCommandMock).toHaveBeenCalledWith('git tag');
 	});
 });
