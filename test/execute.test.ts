8af8de966ffbfd0c5edc47d2f5d8a4d6a397a681
Craig Miller <craigmiller160@gmail.com>
2021-12-23 20:21:39 -0500

    Implemented more tests

--- test/execute.test.ts
+++ test/execute.test.ts
@@ -1,13 +1,17 @@
 import { execute } from '../src/execute';
 import { BuildContext } from '../src/context/BuildContext';
-import { createBuildContext } from './testutils/createBuildContext';
-import { STAGES } from '../src/stages';
+import {
+	createBuildContext,
+	createIncompleteBuildContext
+} from './testutils/createBuildContext';
+import { STAGES, EARLY_STAGES } from '../src/stages';
 import * as TE from 'fp-ts/TaskEither';
-import { StageFunction } from '../src/stages/Stage';
+import { BaseStageFunction } from '../src/stages/Stage';
 import '@relmify/jest-fp-ts';
+import { Context } from '../src/context/Context';
 
 jest.mock('../src/stages', () => ({
-	STAGES: [
+	EARLY_STAGES: [
 		{
 			name: 'Stage1',
 			execute: jest.fn()
@@ -20,14 +24,29 @@ jest.mock('../src/stages', () => ({
 			name: 'Stage3',
 			execute: jest.fn()
 		}
+	],
+	STAGES: [
+		{
+			name: 'Stage4',
+			execute: jest.fn()
+		},
+		{
+			name: 'Stage5',
+			execute: jest.fn()
+		},
+		{
+			name: 'Stage6',
+			execute: jest.fn()
+		}
 	]
 }));
 
+const incompleteBuildContext = createIncompleteBuildContext();
 const buildContext: BuildContext = createBuildContext();
 
-const mockStageFn = (
-	fn: StageFunction,
-	result: TE.TaskEither<Error, BuildContext>
+const mockStageFn = <Ctx extends Context>(
+	fn: BaseStageFunction<Ctx>,
+	result: TE.TaskEither<Error, Context>
 ) => (fn as jest.Mock).mockImplementation(() => result);
 
 describe('execute', () => {
@@ -35,28 +54,63 @@ describe('execute', () => {
 		jest.resetAllMocks();
 	});
 
-	it('executes all stages successfully', async () => {
+	it('executes all early and main stages successfully', async () => {
+		mockStageFn(EARLY_STAGES[0].execute, TE.right(incompleteBuildContext));
+		mockStageFn(EARLY_STAGES[1].execute, TE.right(incompleteBuildContext));
+		mockStageFn(EARLY_STAGES[2].execute, TE.right(incompleteBuildContext));
+
 		mockStageFn(STAGES[0].execute, TE.right(buildContext));
 		mockStageFn(STAGES[1].execute, TE.right(buildContext));
 		mockStageFn(STAGES[2].execute, TE.right(buildContext));
 
-		const result = await execute(buildContext)();
+		const result = await execute(incompleteBuildContext)();
 		expect(result).toEqualRight(buildContext);
 
+		expect(EARLY_STAGES[0].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[1].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[2].execute).toHaveBeenCalled();
 		expect(STAGES[0].execute).toHaveBeenCalled();
 		expect(STAGES[1].execute).toHaveBeenCalled();
 		expect(STAGES[2].execute).toHaveBeenCalled();
 	});
 
-	it('aborts execution after stage error', async () => {
+	it('aborts execution after early stage error', async () => {
 		const error = new Error('Dying');
+		mockStageFn(EARLY_STAGES[0].execute, TE.right(incompleteBuildContext));
+		mockStageFn(EARLY_STAGES[1].execute, TE.left(error));
+		mockStageFn(EARLY_STAGES[2].execute, TE.right(incompleteBuildContext));
+
+		mockStageFn(STAGES[0].execute, TE.right(buildContext));
+		mockStageFn(STAGES[1].execute, TE.right(buildContext));
+		mockStageFn(STAGES[2].execute, TE.right(buildContext));
+
+		const result = await execute(incompleteBuildContext)();
+		expect(result).toEqualLeft(error);
+
+		expect(EARLY_STAGES[0].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[1].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[2].execute).not.toHaveBeenCalled();
+		expect(STAGES[0].execute).not.toHaveBeenCalled();
+		expect(STAGES[1].execute).not.toHaveBeenCalled();
+		expect(STAGES[2].execute).not.toHaveBeenCalled();
+	});
+
+	it('aborts execution after main stage error', async () => {
+		const error = new Error('Dying');
+		mockStageFn(EARLY_STAGES[0].execute, TE.right(incompleteBuildContext));
+		mockStageFn(EARLY_STAGES[1].execute, TE.right(incompleteBuildContext));
+		mockStageFn(EARLY_STAGES[2].execute, TE.right(incompleteBuildContext));
+
 		mockStageFn(STAGES[0].execute, TE.right(buildContext));
 		mockStageFn(STAGES[1].execute, TE.left(error));
 		mockStageFn(STAGES[2].execute, TE.right(buildContext));
 
-		const result = await execute(buildContext)();
+		const result = await execute(incompleteBuildContext)();
 		expect(result).toEqualLeft(error);
 
+		expect(EARLY_STAGES[0].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[1].execute).toHaveBeenCalled();
+		expect(EARLY_STAGES[2].execute).toHaveBeenCalled();
 		expect(STAGES[0].execute).toHaveBeenCalled();
 		expect(STAGES[1].execute).toHaveBeenCalled();
 		expect(STAGES[2].execute).not.toHaveBeenCalled();
