4dc18971af2ee307ab475909ab663ec08e5e5194
Craig <craigmiller160@gmail.com>
2021-12-24 10:33:24 -0500

    Stubbed out tests

---
+++ test/stages/validateKubernetesConfig.ts
@@ -0,0 +1,31 @@
+export {};
+
+describe('validateKubernetesConfig', () => {
+	it('skips for NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('skips for MavenLibrary', () => {
+		throw new Error();
+	});
+
+	it('skips for DockerImage', () => {
+		throw new Error();
+	});
+
+	it('kubernetes config is valid', () => {
+		throw new Error();
+	});
+
+	it('kubernetes config does not have version placeholder', () => {
+		throw new Error();
+	});
+
+	it('kubernetes config has wrong project name', () => {
+		throw new Error();
+	});
+
+	it('kubernetes config has wrong repo prefix', () => {
+		throw new Error();
+	});
+});
