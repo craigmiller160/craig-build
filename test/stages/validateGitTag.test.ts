47d1dd6e7413bfce01f8985ecf0f44b26037a03f
Craig Miller <craigmiller160@gmail.com>
2021-12-24 14:50:54 -0500

    Stubbed out tests

---
+++ test/stages/validateGitTag.test.ts
@@ -0,0 +1,15 @@
+export {};
+
+describe('validateGitTag', () => {
+	it('skips pre-release project', () => {
+		throw new Error();
+	});
+
+	it('confirms there is no existing tag for release version', () => {
+		throw new Error();
+	});
+
+	it('finds an existing tag for release version', () => {
+		throw new Error();
+	});
+});
