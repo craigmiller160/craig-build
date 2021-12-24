e984f688702ab51af0d0497504c5f93e760ece5d
Craig Miller <craigmiller160@gmail.com>
2021-12-24 18:40:18 -0500

    Stubbed out tests

---
+++ test/stages/preparePreReleaseVersion.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+describe('preparePreReleaseVersion', () => {
+	it('skips for release project', async () => {
+		throw new Error();
+	});
+
+	it('prepares pre-release version for NPM project', async () => {
+		throw new Error();
+	});
+
+	it('prepares pre-release version for Maven project', async () => {
+		throw new Error();
+	});
+
+	it('prepares pre-release version for Docker project', async () => {
+		throw new Error();
+	});
+});
