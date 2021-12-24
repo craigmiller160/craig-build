1e3caf7a020f519498e3c9a8a21d2acc2d46abaa
Craig <craigmiller160@gmail.com>
2021-12-24 10:54:11 -0500

    Implemented some initial logic

--- src/stages/validateKubernetesConfig.ts
+++ src/stages/validateKubernetesConfig.ts
@@ -1,14 +1,26 @@
 import { Stage, StageFunction } from './Stage';
 import { BuildContext } from '../context/BuildContext';
-import { match } from 'ts-pattern';
+import { match, when } from 'ts-pattern';
+import { isApplication } from '../context/projectTypeUtils';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
 
-const validateConfigByProject = (context: BuildContext) =>
-	match(context)
-		.with({  })
-
-const execute: StageFunction = (context) => {
+const validateConfig = (
+	context: BuildContext
+): E.Either<Error, BuildContext> => {
 	throw new Error();
 };
+
+const validateConfigByProject = (
+	context: BuildContext
+): E.Either<Error, BuildContext> =>
+	match(context)
+		.with({ projectType: when(isApplication) }, validateConfig)
+		.otherwise(E.right);
+
+const execute: StageFunction = (context) =>
+	pipe(validateConfig(context), TE.fromEither);
 
 export const validateKubernetesConfig: Stage = {
 	name: 'Validate Kubernetes Config',
