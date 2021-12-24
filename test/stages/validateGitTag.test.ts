d818e80f1fa88428ed813049e85c100c572e76ec
Craig Miller <craigmiller160@gmail.com>
2021-12-24 15:02:54 -0500

    Implemented first test

--- test/stages/validateGitTag.test.ts
+++ test/stages/validateGitTag.test.ts
@@ -1,15 +1,33 @@
-export {};
+import { runCommandMock } from '../testutils/runCommandMock';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { validateGitTag } from '../../src/stages/validateGitTag';
+
+const baseBuildContext = createBuildContext();
 
 describe('validateGitTag', () => {
-	it('skips pre-release project', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('skips pre-release project', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: true
+			}
+		};
+
+		const result = await validateGitTag.execute(buildContext)();
+		expect(result).toEqual(buildContext);
+	});
+
+	it('confirms there is no existing tag for release version', async () => {
 		throw new Error();
 	});
 
-	it('confirms there is no existing tag for release version', () => {
-		throw new Error();
-	});
-
-	it('finds an existing tag for release version', () => {
+	it('finds an existing tag for release version', async () => {
 		throw new Error();
 	});
 });
