a78681447e58433fe1ea873734c105d309757d7d
Craig Miller <craigmiller160@gmail.com>
2021-12-24 18:48:29 -0500

    Implemented even more test logic

--- test/stages/preparePreReleaseVersion.test.ts
+++ test/stages/preparePreReleaseVersion.test.ts
@@ -2,15 +2,22 @@ import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import '@relmify/jest-fp-ts';
 import { preparePreReleaseVersion } from '../../src/stages/preparePreReleaseVersion';
-import { searchForNpmBetas } from '../../src/services/NexusRepoApi';
+import {
+	searchForNpmBetas,
+	searchForDockerReleases,
+	searchForMavenSnapshots
+} from '../../src/services/NexusRepoApi';
 
 jest.mock('../../src/services/NexusRepoApi', () => ({
-	searchForNpmBetas: jest.fn()
+	searchForNpmBetas: jest.fn(),
+	searchForDockerReleases: jest.fn()
 }));
 
 const baseBuildContext = createBuildContext();
 
 const searchForNpmBetasMock = searchForNpmBetas as jest.Mock;
+const searchForDockerReleasesMock = searchForDockerReleases as jest.Mock;
+const searchForMavenSnapshotsMock = searchForMavenSnapshots as jest.Mock;
 
 describe('preparePreReleaseVersion', () => {
 	beforeEach(() => {
@@ -30,6 +37,8 @@ describe('preparePreReleaseVersion', () => {
 		expect(result).toEqualRight(buildContext);
 
 		expect(searchForNpmBetasMock).not.toHaveBeenCalled();
+		expect(searchForDockerReleasesMock).not.toHaveBeenCalled();
+		expect(searchForMavenSnapshotsMock).not.toHaveBeenCalled();
 	});
 
 	it('prepares pre-release version for NPM project', async () => {
