940da68045cc20d93320264dc4cc5c47971d7da7
Craig Miller <craigmiller160@gmail.com>
2021-12-24 18:45:12 -0500

    Implementing some test logic

--- test/stages/preparePreReleaseVersion.test.ts
+++ test/stages/preparePreReleaseVersion.test.ts
@@ -1,8 +1,35 @@
-export {};
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import '@relmify/jest-fp-ts';
+import { preparePreReleaseVersion } from '../../src/stages/preparePreReleaseVersion';
+import { searchForNpmBetas } from '../../src/services/NexusRepoApi';
+
+jest.mock('../../src/services/NexusRepoApi', () => ({
+	searchForNpmBetas: jest.fn()
+}));
+
+const baseBuildContext = createBuildContext();
+
+const searchForNpmBetasMock = searchForNpmBetas as jest.Mock;
 
 describe('preparePreReleaseVersion', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
 	it('skips for release project', async () => {
-		throw new Error();
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: false
+			}
+		};
+
+		const result = await preparePreReleaseVersion.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(searchForNpmBetasMock).not.toHaveBeenCalled();
 	});
 
 	it('prepares pre-release version for NPM project', async () => {
