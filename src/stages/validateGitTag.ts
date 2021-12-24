916f663faa719045a466e1b8c62497f4d8d3d921
Craig Miller <craigmiller160@gmail.com>
2021-12-24 15:00:41 -0500

    Implementing a small amount of logic

--- src/stages/validateGitTag.ts
+++ src/stages/validateGitTag.ts
@@ -1,8 +1,17 @@
 import { Stage, StageFunction } from './Stage';
+import { BuildContext } from '../context/BuildContext';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 
-const execute: StageFunction = (context) => {
+const handleValidationByProject = (
+	context: BuildContext
+): E.Either<Error, BuildContext> => {
 	throw new Error();
 };
+
+const execute: StageFunction = (context) =>
+	pipe(handleValidationByProject(context), TE.fromEither);
 
 export const validateGitTag: Stage = {
 	name: 'Validate Git Tag',
