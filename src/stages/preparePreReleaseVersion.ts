bd5367818208d48ef4424ce3a775e9d642d5f75d
Craig Miller <craigmiller160@gmail.com>
2021-12-24 19:19:31 -0500

    Began implementing logic

--- src/stages/preparePreReleaseVersion.ts
+++ src/stages/preparePreReleaseVersion.ts
@@ -1,8 +1,52 @@
 import { Stage, StageFunction } from './Stage';
+import * as TE from 'fp-ts/TaskEither';
+import { BuildContext } from '../context/BuildContext';
+import { match, when } from 'ts-pattern';
+import { logger } from '../logger';
+import { isDocker, isMaven, isNpm } from '../context/projectTypeUtils';
+import { isPreRelease } from '../context/projectInfoUtils';
 
-const execute: StageFunction = (context) => {
+const handleMavenPreReleaseVersion = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> => {
 	throw new Error();
 };
+
+const handleNpmPreReleaseVersion = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> => {
+	throw new Error();
+};
+
+const handleDockerPreReleaseVersion = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> => {
+	throw new Error();
+};
+
+const handlePreparingPreReleaseVersionByProject = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> =>
+	match(context)
+		.with(
+			{ projectType: when(isMaven), projectInfo: when(isPreRelease) },
+			handleMavenPreReleaseVersion
+		)
+		.with(
+			{ projectType: when(isNpm), projectInfo: when(isPreRelease) },
+			handleNpmPreReleaseVersion
+		)
+		.with(
+			{ projectType: when(isDocker), projectInfo: when(isPreRelease) },
+			handleDockerPreReleaseVersion
+		)
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
+
+const execute: StageFunction = (context) =>
+	handlePreparingPreReleaseVersionByProject(context);
 
 export const preparePreReleaseVersion: Stage = {
 	name: 'Prepare Pre-Release Version',
