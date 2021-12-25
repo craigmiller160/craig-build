739fa6103ddc018ce2a8f8d7cd468ce64890625a
Craig <craigmiller160@gmail.com>
2021-12-25 14:01:22 -0500

    Setting up test logic

--- src/stages/manuallyPublishArtifact.ts
+++ src/stages/manuallyPublishArtifact.ts
@@ -1,5 +1,8 @@
 import { Stage, StageFunction } from './Stage';
 
+export const NPM_PUBLISH_COMMAND =
+	'yarn publish --no-git-tag-version --new-version';
+
 const execute: StageFunction = (context) => {
 	throw new Error();
 };
--- test/stages/manuallyPublishArtifact.test.ts
+++ test/stages/manuallyPublishArtifact.test.ts
@@ -1,4 +1,9 @@
-export {};
+import { runCommandMock } from '../testutils/runCommandMock';
+import * as TE from 'fp-ts/TaskEither';
+import '@relmify/jest-fp-ts';
+import { createBuildContext } from '../testutils/createBuildContext';
+
+const baseBuildContext = createBuildContext();
 
 describe('manuallyPublishArtifact', () => {
 	it('skips for Maven project', () => {
@@ -6,6 +11,10 @@ describe('manuallyPublishArtifact', () => {
 	});
 
 	it('skips for Docker project', () => {
+		throw new Error();
+	});
+
+	it('publishes NPM project', () => {
 		throw new Error();
 	});
 });
