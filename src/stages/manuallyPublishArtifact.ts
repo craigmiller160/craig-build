95365b6c07e797a53fecd277823a5dc62c26796e
Craig <craigmiller160@gmail.com>
2021-12-25 14:09:39 -0500

    Fully implemented stage

--- src/stages/manuallyPublishArtifact.ts
+++ src/stages/manuallyPublishArtifact.ts
@@ -4,6 +4,8 @@ import * as TE from 'fp-ts/TaskEither';
 import { match, when } from 'ts-pattern';
 import { logger } from '../logger';
 import { isNpm } from '../context/projectTypeUtils';
+import { pipe } from 'fp-ts/function';
+import { runCommand } from '../command/runCommand';
 
 export const NPM_PUBLISH_COMMAND =
 	'yarn publish --no-git-tag-version --new-version';
@@ -12,9 +14,14 @@ export const CLEAR_FILES_COMMAND = 'git checkout .';
 
 const publishNpmArtifact = (
 	context: BuildContext
-): TE.TaskEither<Error, BuildContext> => {
-	throw new Error();
-};
+): TE.TaskEither<Error, BuildContext> =>
+	pipe(
+		runCommand(`${NPM_PUBLISH_COMMAND} ${context.projectInfo.version}`, {
+			printOutput: true
+		}),
+		TE.chain(() => runCommand(CLEAR_FILES_COMMAND)),
+		TE.map(() => context)
+	);
 
 const handlePublishByProject = (
 	context: BuildContext
@@ -26,9 +33,7 @@ const handlePublishByProject = (
 			return TE.right(context);
 		});
 
-const execute: StageFunction = (context) => {
-	throw new Error();
-};
+const execute: StageFunction = (context) => handlePublishByProject(context);
 
 export const manuallyPublishArtifact: Stage = {
 	name: 'Manually Publish Artifact',
