87233e1425411a6659d0911efa536f4d5893bcc8
Craig Miller <craigmiller160@gmail.com>
2021-12-24 19:01:03 -0500

    Implemented more tests

--- test/stages/preparePreReleaseVersion.test.ts
+++ test/stages/preparePreReleaseVersion.test.ts
@@ -3,10 +3,15 @@ import { BuildContext } from '../../src/context/BuildContext';
 import '@relmify/jest-fp-ts';
 import { preparePreReleaseVersion } from '../../src/stages/preparePreReleaseVersion';
 import {
-	searchForNpmBetas,
 	searchForDockerBetas,
-	searchForMavenSnapshots
+	searchForMavenSnapshots,
+	searchForNpmBetas
 } from '../../src/services/NexusRepoApi';
+import NexusSearchResult, {
+	NexusSearchResultItem
+} from '../../old-src/types/NexusSearchResult';
+import { ProjectType } from '../../src/context/ProjectType';
+import * as TE from 'fp-ts/TaskEither';
 
 jest.mock('../../src/services/NexusRepoApi', () => ({
 	searchForNpmBetas: jest.fn(),
@@ -18,6 +23,16 @@ const baseBuildContext = createBuildContext();
 const searchForNpmBetasMock = searchForNpmBetas as jest.Mock;
 const searchForDockerBetasMock = searchForDockerBetas as jest.Mock;
 const searchForMavenSnapshotsMock = searchForMavenSnapshots as jest.Mock;
+
+const createItem = (version: string): NexusSearchResultItem => ({
+	name: '',
+	group: '',
+	format: '',
+	repository: '',
+	version: '',
+	id: '',
+	assets: []
+});
 
 describe('preparePreReleaseVersion', () => {
 	beforeEach(() => {
@@ -41,15 +56,85 @@ describe('preparePreReleaseVersion', () => {
 		expect(searchForMavenSnapshotsMock).not.toHaveBeenCalled();
 	});
 
-	it('prepares pre-release version for NPM project', async () => {
-		throw new Error();
+	it('prepares pre-release version for NPM project based on existing version', async () => {
+		const nexusResult: NexusSearchResult = {
+			items: [createItem('1.0.0-beta.2')]
+		};
+		searchForNpmBetasMock.mockImplementation(() => TE.right(nexusResult));
+
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmApplication,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				group: 'craigmiller160',
+				name: 'my-project',
+				isPreRelease: true,
+				version: '1.0.0-beta'
+			}
+		};
+
+		const result = await preparePreReleaseVersion.execute(buildContext)();
+		expect(result).toEqualRight({
+			...buildContext,
+			projectInfo: {
+				...buildContext.projectInfo,
+				version: '1.0.0-beta.3'
+			}
+		});
+
+		expect(searchForNpmBetasMock).toHaveBeenCalledWith(
+			'craigmiller160',
+			'my-project'
+		);
+		expect(searchForDockerBetasMock).not.toHaveBeenCalled();
+		expect(searchForMavenSnapshotsMock).not.toHaveBeenCalled();
 	});
 
-	it('prepares pre-release version for Maven project', async () => {
+	it('prepares pre-release version for NPM project with no existing version', async () => {
+		const nexusResult: NexusSearchResult = {
+			items: [createItem('1.1.0-beta.2')]
+		};
+		searchForNpmBetasMock.mockImplementation(() => TE.right(nexusResult));
+
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmApplication,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				group: 'craigmiller160',
+				name: 'my-project',
+				isPreRelease: true,
+				version: '1.0.0-beta'
+			}
+		};
+
+		const result = await preparePreReleaseVersion.execute(buildContext)();
+		expect(result).toEqualRight({
+			...buildContext,
+			projectInfo: {
+				...buildContext.projectInfo,
+				version: '1.0.0-beta.1'
+			}
+		});
+
+		expect(searchForNpmBetasMock).toHaveBeenCalledWith(
+			'craigmiller160',
+			'my-project'
+		);
+		expect(searchForDockerBetasMock).not.toHaveBeenCalled();
+		expect(searchForMavenSnapshotsMock).not.toHaveBeenCalled();
+	});
+
+	it('grabs recently created Maven pre-release artifact version', async () => {
 		throw new Error();
 	});
 
 	it('prepares pre-release version for Docker project', async () => {
+		throw new Error();
+	});
+
+	it('prepares pre-release version for Docker project with no existing version', async () => {
 		throw new Error();
 	});
 });
