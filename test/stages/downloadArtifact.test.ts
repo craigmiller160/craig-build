12935abaa7b30e7d2aaa279753dd8304c9fe6247
Craig Miller <craigmiller160@gmail.com>
2021-12-25 17:05:17 -0500

    Stubbed out tests

---
+++ test/stages/downloadArtifact.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+describe('downloadArtifact', () => {
+	it('skips for Docker project', () => {
+		throw new Error();
+	});
+
+	it('downloads maven release artifact', () => {
+		throw new Error();
+	});
+
+	it('downloads maven pre-release artifact', () => {
+		throw new Error();
+	});
+
+	it('downloads npm release artifact', () => {
+		throw new Error();
+	});
+
+	it('downloads npm pre-release artifact', () => {
+		throw new Error();
+	});
+});
