3b1c09a29755797ed16bec562d346cca9531cc84
Craig <craigmiller160@gmail.com>
2021-12-25 14:02:44 -0500

    Implemented first two tests

--- test/stages/manuallyPublishArtifact.test.ts
+++ test/stages/manuallyPublishArtifact.test.ts
@@ -1,20 +1,42 @@
-import { runCommandMock } from '../testutils/runCommandMock';
-import * as TE from 'fp-ts/TaskEither';
 import '@relmify/jest-fp-ts';
 import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { ProjectType } from '../../src/context/ProjectType';
+import { manuallyPublishArtifact } from '../../src/stages/manuallyPublishArtifact';
+import { runCommandMock } from '../testutils/runCommandMock';
 
 const baseBuildContext = createBuildContext();
 
 describe('manuallyPublishArtifact', () => {
-	it('skips for Maven project', () => {
-		throw new Error();
+	beforeEach(() => {
+		jest.resetAllMocks();
 	});
 
-	it('skips for Docker project', () => {
-		throw new Error();
+	it('skips for Maven project', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenApplication
+		};
+
+		const result = await manuallyPublishArtifact.execute(buildContext)();
+		expect(result).toEqual(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
 	});
 
-	it('publishes NPM project', () => {
+	it('skips for Docker project', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.DockerApplication
+		};
+
+		const result = await manuallyPublishArtifact.execute(buildContext)();
+		expect(result).toEqual(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
+	});
+
+	it('publishes NPM project', async () => {
 		throw new Error();
 	});
 });
