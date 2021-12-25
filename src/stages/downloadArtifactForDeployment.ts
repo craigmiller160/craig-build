45c4cb57fdc69a238c3c23a47a69984aad48362e
Craig <craigmiller160@gmail.com>
2021-12-25 17:36:44 -0500

    Began implementing stage logic

--- src/stages/downloadArtifactForDeployment.ts
+++ src/stages/downloadArtifactForDeployment.ts
@@ -1,8 +1,50 @@
 import { Stage, StageFunction } from './Stage';
+import { BuildContext } from '../context/BuildContext';
+import * as TE from 'fp-ts/TaskEither';
+import { match, when } from 'ts-pattern';
+import { logger } from '../logger';
+import {
+	NexusRepoGroupSearchFn,
+	searchForMavenReleases,
+	searchForMavenSnapshots,
+	searchForNpmBetas,
+	searchForNpmReleases
+} from '../services/NexusRepoApi';
+import { isMaven, isNpm } from '../context/projectTypeUtils';
+import { isPreRelease, isRelease } from '../context/projectInfoUtils';
 
-const execute: StageFunction = (context) => {
+const doDownloadArtifact = (
+	context: BuildContext,
+	searchFn: NexusRepoGroupSearchFn
+): TE.TaskEither<Error, BuildContext> => {
 	throw new Error();
 };
+
+const downloadArtifactByProject = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> =>
+	match(context)
+		.with(
+			{ projectType: when(isMaven), projectInfo: when(isPreRelease) },
+			(_) => doDownloadArtifact(_, searchForMavenSnapshots)
+		)
+		.with(
+			{ projectType: when(isMaven), projectInfo: when(isRelease) },
+			(_) => doDownloadArtifact(_, searchForMavenReleases)
+		)
+		.with(
+			{ projectType: when(isNpm), projectInfo: when(isPreRelease) },
+			(_) => doDownloadArtifact(_, searchForNpmBetas)
+		)
+		.with({ projectType: when(isNpm), projectInfo: when(isRelease) }, (_) =>
+			doDownloadArtifact(_, searchForNpmReleases)
+		)
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
+
+const execute: StageFunction = (context) => downloadArtifactByProject(context);
 
 export const downloadArtifactForDeployment: Stage = {
 	name: 'Download Artifact For Deployment',
