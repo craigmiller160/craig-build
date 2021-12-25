366ba4ef44e0f566ceded9576d3e7a9e0a840afa
Craig Miller <craigmiller160@gmail.com>
2021-12-25 17:25:11 -0500

    Implemented another test

--- test/stages/downloadArtifactForDeployment.test.ts
+++ test/stages/downloadArtifactForDeployment.test.ts
@@ -3,22 +3,57 @@ import { BuildContext } from '../../src/context/BuildContext';
 import { ProjectType } from '../../src/context/ProjectType';
 import { downloadArtifactForDeployment } from '../../src/stages/downloadArtifactForDeployment';
 import '@relmify/jest-fp-ts';
-import { downloadArtifact, searchForMavenSnapshots, searchForMavenReleases, searchForNpmBetas, searchForNpmReleases } from '../../src/services/NexusRepoApi';
+import {
+	downloadArtifact,
+	searchForMavenSnapshots,
+	searchForMavenReleases,
+	searchForNpmBetas,
+	searchForNpmReleases
+} from '../../src/services/NexusRepoApi';
+import * as TE from 'fp-ts/TaskEither';
+import NexusSearchResult, {
+	NexusSearchResultItem
+} from '../../old-src/types/NexusSearchResult';
 
 jest.mock('../../src/services/NexusRepoApi', () => ({
 	downloadArtifact: jest.fn()
 }));
 
-const baseBuildContext = createBuildContext();
 const downloadArtifactMock = downloadArtifact as jest.Mock;
 const searchForMavenSnapshotsMock = searchForMavenSnapshots as jest.Mock;
 const searchForMavenReleasesMock = searchForMavenReleases as jest.Mock;
 const searchForNpmBetasMock = searchForNpmBetas as jest.Mock;
 const searchForNpmReleasesMock = searchForNpmReleases as jest.Mock;
 
+const createItem = (version: string): NexusSearchResultItem => ({
+	name: '',
+	version,
+	group: '',
+	format: '',
+	repository: '',
+	assets: [
+		{
+			id: '',
+			downloadUrl: 'downloadUrl',
+			path: ''
+		}
+	],
+	id: ''
+});
+
+const baseBuildContext = createBuildContext({
+	projectInfo: {
+		group: 'craigmiller160',
+		name: 'my-project',
+		isPreRelease: false,
+		version: ''
+	}
+});
+
 describe('downloadArtifactForDeployment', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
+		downloadArtifactMock.mockImplementation(() => TE.right(''));
 	});
 
 	it('skips for Docker project', async () => {
@@ -40,7 +75,29 @@ describe('downloadArtifactForDeployment', () => {
 	});
 
 	it('downloads maven release artifact', async () => {
-		throw new Error();
+		searchForMavenReleasesMock.mockImplementation(() =>
+			TE.right({ items: [createItem('1.0.0')] })
+		);
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenApplication,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				version: '1.0.0',
+				isPreRelease: false
+			}
+		};
+
+		const result = await downloadArtifactForDeployment.execute(
+			buildContext
+		)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(downloadArtifactMock).toHaveBeenCalledWith('downloadUrl')
+		expect(searchForMavenSnapshotsMock).not.toHaveBeenCalled();
+		expect(searchForMavenReleasesMock).toHaveBeenCalledWith('craigmiller160', 'my-project', '1.0.0');
+		expect(downloadArtifactMock).not.toHaveBeenCalled();
+		expect(searchForNpmReleasesMock).not.toHaveBeenCalled();
 	});
 
 	it('downloads maven pre-release artifact', async () => {
