e784d173d103707d30f870d7cc0a60003bfcdf6e
Craig <craigmiller160@gmail.com>
2021-12-25 14:57:21 -0500

    Implemented more tests

--- test/stages/gitTag.test.ts
+++ test/stages/gitTag.test.ts
@@ -3,6 +3,7 @@ import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import { gitTag } from '../../src/stages/gitTag';
 import '@relmify/jest-fp-ts';
+import * as TE from 'fp-ts/TaskEither';
 
 const baseBuildContext = createBuildContext();
 
@@ -23,6 +24,21 @@ describe('gitTag', () => {
 	});
 
 	it('creates and pushes tag for release project', async () => {
-		throw new Error();
+		runCommandMock.mockImplementation(() => TE.right(''));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: false,
+				version: '1.0.0'
+			}
+		};
+
+		const result = await gitTag.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledTimes(2);
+		expect(runCommandMock).toHaveBeenNthCalledWith(1, 'git tag v1.0.0');
+		expect(runCommandMock).toHaveBeenNthCalledWith(2, 'git push --tags');
 	});
 });
