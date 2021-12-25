6558b4e0c5eae6674dc6d8252cf217bcfbc1f31d
Craig Miller <craigmiller160@gmail.com>
2021-12-25 15:03:23 -0500

    Began implementing stage logic

--- src/stages/gitTag.ts
+++ src/stages/gitTag.ts
@@ -1,8 +1,27 @@
 import { Stage, StageFunction } from './Stage';
+import { BuildContext } from '../context/BuildContext';
+import * as TE from 'fp-ts/TaskEither';
+import { match, when } from 'ts-pattern';
+import { logger } from '../logger';
+import { isRelease } from '../context/projectInfoUtils';
 
-const execute: StageFunction = (context) => {
+const doGitTag = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> => {
 	throw new Error();
 };
+
+const handleGitTagByProject = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> =>
+	match(context)
+		.with({ projectInfo: when(isRelease) }, doGitTag)
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
+
+const execute: StageFunction = (context) => handleGitTagByProject(context);
 
 export const gitTag: Stage = {
 	name: 'Git Tag',
