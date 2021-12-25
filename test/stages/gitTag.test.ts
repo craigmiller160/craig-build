a414f6dac7c680e752e4b93ebf2392b99a2db13a
Craig <craigmiller160@gmail.com>
2021-12-25 14:55:28 -0500

    Implemented a test

--- test/stages/gitTag.test.ts
+++ test/stages/gitTag.test.ts
@@ -1,11 +1,28 @@
-export {};
+import { runCommandMock } from '../testutils/runCommandMock';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { gitTag } from '../../src/stages/gitTag';
+import '@relmify/jest-fp-ts';
+
+const baseBuildContext = createBuildContext();
 
 describe('gitTag', () => {
-	it('skips for pre-release project', () => {
-		throw new Error();
+	it('skips for pre-release project', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				isPreRelease: true
+			}
+		};
+
+		const result = gitTag.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
 	});
 
-	it('creates and pushes tag for release project', () => {
+	it('creates and pushes tag for release project', async () => {
 		throw new Error();
 	});
 });
