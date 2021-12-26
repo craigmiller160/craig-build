9790d55ccdb1dc8b5531374addb92fc13fe3d491
Craig Miller <craigmiller160@gmail.com>
2021-12-26 15:43:21 -0500

    Stubbed out tests

---
+++ test/stages/buildAndPushDocker.test.ts
@@ -0,0 +1,35 @@
+export {};
+
+describe('buildAndPushDocker', () => {
+	it('no docker username environment variable', () => {
+		throw new Error();
+	});
+
+	it('no docker password environment variable', () => {
+		throw new Error();
+	});
+
+	it('skips for npm library', () => {
+		throw new Error();
+	});
+
+	it('skips for maven library', () => {
+		throw new Error();
+	});
+
+	it('builds and pushes docker image for maven application', () => {
+		throw new Error();
+	});
+
+	it('builds and pushes docker image for npm application', () => {
+		throw new Error();
+	});
+
+	it('builds and pushes docker image for docker application', () => {
+		throw new Error();
+	});
+
+	it('builds and pushes docker image for docker image', () => {
+		throw new Error();
+	});
+});
