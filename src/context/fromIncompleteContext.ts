d223bee9ed296252bddfc81189a7042739b03c10
Craig Miller <craigmiller160@gmail.com>
2021-12-26 14:52:14 -0500

    Refactored fromIncompleteContext to use binding

--- src/context/fromIncompleteContext.ts
+++ src/context/fromIncompleteContext.ts
@@ -1,6 +1,6 @@
 import { IncompleteBuildContext } from './IncompleteBuildContext';
 import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { BuildContext } from './BuildContext';
 import { CommandInfo } from './CommandInfo';
@@ -10,7 +10,35 @@ import { ProjectType } from './ProjectType';
 type Container2 = [CommandInfo, BuildToolInfo];
 type Container3 = [CommandInfo, BuildToolInfo, ProjectType];
 
-export const fromIncompleteContext = (
+type FromIncompleteContextFn = (
+	context: IncompleteBuildContext
+) => E.Either<Error, BuildContext>;
+export const fromIncompleteContext: FromIncompleteContextFn = flow(
+	O.of,
+	O.bindTo('context'),
+	O.bind('commandInfo', ({ context }) => context.commandInfo),
+	O.bind('buildToolInfo', ({ context }) => context.buildToolInfo),
+	O.bind('projectType', ({ context }) => context.projectType),
+	O.bind('projectInfo', ({ context }) => context.projectInfo),
+	O.map(
+		({
+			commandInfo,
+			buildToolInfo,
+			projectType,
+			projectInfo
+		}): BuildContext => ({
+			commandInfo,
+			buildToolInfo,
+			projectType,
+			projectInfo
+		})
+	),
+	E.fromOption(
+		() => new Error('IncompleteBuildContext has not yet been completed')
+	)
+);
+
+export const fromIncompleteContext2 = (
 	context: IncompleteBuildContext
 ): E.Either<Error, BuildContext> =>
 	pipe(
