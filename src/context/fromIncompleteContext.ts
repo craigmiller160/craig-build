ccd859f44923527512e7a21112906f7755d4e0af
Craig Miller <craigmiller160@gmail.com>
2021-12-26 14:56:00 -0500

    Finished overhauling fromIncompleteContext

--- src/context/fromIncompleteContext.ts
+++ src/context/fromIncompleteContext.ts
@@ -1,18 +1,13 @@
 import { IncompleteBuildContext } from './IncompleteBuildContext';
 import * as E from 'fp-ts/Either';
-import { flow, pipe } from 'fp-ts/function';
+import { flow } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { BuildContext } from './BuildContext';
-import { CommandInfo } from './CommandInfo';
-import { BuildToolInfo } from './BuildToolInfo';
-import { ProjectType } from './ProjectType';
-
-type Container2 = [CommandInfo, BuildToolInfo];
-type Container3 = [CommandInfo, BuildToolInfo, ProjectType];
 
 type FromIncompleteContextFn = (
 	context: IncompleteBuildContext
 ) => E.Either<Error, BuildContext>;
+
 export const fromIncompleteContext: FromIncompleteContextFn = flow(
 	O.of,
 	O.bindTo('context'),
@@ -20,63 +15,8 @@ export const fromIncompleteContext: FromIncompleteContextFn = flow(
 	O.bind('buildToolInfo', ({ context }) => context.buildToolInfo),
 	O.bind('projectType', ({ context }) => context.projectType),
 	O.bind('projectInfo', ({ context }) => context.projectInfo),
-	O.map(
-		({
-			commandInfo,
-			buildToolInfo,
-			projectType,
-			projectInfo
-		}): BuildContext => ({
-			commandInfo,
-			buildToolInfo,
-			projectType,
-			projectInfo
-		})
-	),
+	O.map(({ context, ...rest }) => rest),
 	E.fromOption(
 		() => new Error('IncompleteBuildContext has not yet been completed')
 	)
 );
-
-export const fromIncompleteContext2 = (
-	context: IncompleteBuildContext
-): E.Either<Error, BuildContext> =>
-	pipe(
-		context.commandInfo,
-		O.chain((commandInfo) =>
-			pipe(
-				context.buildToolInfo,
-				O.map(
-					(buildToolInfo): Container2 => [commandInfo, buildToolInfo]
-				)
-			)
-		),
-		O.chain(([commandInfo, buildToolInfo]) =>
-			pipe(
-				context.projectType,
-				O.map(
-					(projectType): Container3 => [
-						commandInfo,
-						buildToolInfo,
-						projectType
-					]
-				)
-			)
-		),
-		O.chain(([commandInfo, buildToolInfo, projectType]) =>
-			pipe(
-				context.projectInfo,
-				O.map(
-					(projectInfo): BuildContext => ({
-						commandInfo,
-						buildToolInfo,
-						projectType,
-						projectInfo
-					})
-				)
-			)
-		),
-		E.fromOption(
-			() => new Error('IncompleteBuildContext has not yet been completed')
-		)
-	);
