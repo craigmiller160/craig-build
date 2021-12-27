fd8d8e7da30f72715e34a2813d91a88aaa2cc93d
Craig <craigmiller160@gmail.com>
2021-12-27 10:18:11 -0500

    Began implementing tests

--- test/stages/deployToKubernetes.test.ts
+++ test/stages/deployToKubernetes.test.ts
@@ -1,35 +1,70 @@
-export {};
+import { runCommandMock } from '../testutils/runCommandMock';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { ProjectType } from '../../src/context/ProjectType';
+import { deployToKubernetes } from '../../src/stages/deployToKubernetes';
+import '@relmify/jest-fp-ts';
+
+const baseBuildContext = createBuildContext();
 
 describe('deployToKubernetes', () => {
-	it('skips for NpmLibrary', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('skips for NpmLibrary', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmLibrary
+		};
+
+		const result = await deployToKubernetes.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
+	});
+
+	it('skips for MavenLibrary', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenLibrary
+		};
+
+		const result = await deployToKubernetes.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
+	});
+
+	it('skips for DockerImage', async () => {
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.DockerImage
+		};
+
+		const result = await deployToKubernetes.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).not.toHaveBeenCalled();
+	});
+
+	it('deploys for MavenApplication', async () => {
 		throw new Error();
 	});
 
-	it('skips for MavenLibrary', () => {
+	it('deploys for NpmApplication', async () => {
 		throw new Error();
 	});
 
-	it('skips for DockerImage', () => {
+	it('deploys for DockerApplication', async () => {
 		throw new Error();
 	});
 
-	it('deploys for MavenApplication', () => {
+	it('deploys for MavenApplication with configmap', async () => {
 		throw new Error();
 	});
 
-	it('deploys for NpmApplication', () => {
-		throw new Error();
-	});
-
-	it('deploys for DockerApplication', () => {
-		throw new Error();
-	});
-
-	it('deploys for MavenApplication with configmap', () => {
-		throw new Error();
-	});
-
-	it('deploys for MavenApplication with multiple configmaps', () => {
+	it('deploys for MavenApplication with multiple configmaps', async () => {
 		throw new Error();
 	});
 });
