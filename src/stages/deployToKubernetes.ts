b7d796f751c6596d1dc5dda54ca222a7abbceda9
Craig <craigmiller160@gmail.com>
2021-12-27 10:42:02 -0500

    Implemented configmap deployment logic

--- src/stages/deployToKubernetes.ts
+++ src/stages/deployToKubernetes.ts
@@ -4,12 +4,13 @@ import * as TE from 'fp-ts/TaskEither';
 import { match, when } from 'ts-pattern';
 import { logger } from '../logger';
 import { isApplication } from '../context/projectTypeUtils';
-import { pipe } from 'fp-ts/function';
+import { flow, pipe } from 'fp-ts/function';
 import { listFilesInDir } from '../functions/File';
 import path from 'path';
 import { getCwd } from '../command/getCwd';
 import * as A from 'fp-ts/Array';
 import * as E from 'fp-ts/Either';
+import { runCommand } from '../command/runCommand';
 
 const findConfigmaps = (deployDir: string): TE.TaskEither<Error, string[]> =>
 	pipe(
@@ -18,10 +19,34 @@ const findConfigmaps = (deployDir: string): TE.TaskEither<Error, string[]> =>
 		TE.fromEither
 	);
 
+const deployConfigmaps = (
+	deployDir: string,
+	configmapFiles: string[]
+): TE.TaskEither<Error, string> =>
+	pipe(
+		configmapFiles,
+		A.reduce(TE.right<Error, string>(''), (result, file) =>
+			pipe(
+				result,
+				TE.chain(() =>
+					runCommand(`kubectl apply -f ${file}`, {
+						printOutput: true,
+						cwd: deployDir
+					})
+				)
+			)
+		)
+	);
+
 const doDeploy = (
 	context: BuildContext
 ): TE.TaskEither<Error, BuildContext> => {
 	const deployDir = path.join(getCwd(), 'deploy');
+	pipe(
+		findConfigmaps(deployDir),
+		TE.chain((_) => deployConfigmaps(deployDir, _))
+	);
+
 	throw new Error();
 };
 
