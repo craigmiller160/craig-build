6e4ef36c9b7d146aec5ebc574f595e6dd106875a
Craig <craigmiller160@gmail.com>
2021-12-27 10:29:49 -0500

    Began implementing stage logic

--- src/stages/deployToKubernetes.ts
+++ src/stages/deployToKubernetes.ts
@@ -1,4 +1,25 @@
 import { Stage, StageFunction } from './Stage';
+import { BuildContext } from '../context/BuildContext';
+import * as TE from 'fp-ts/TaskEither';
+import { match, when } from 'ts-pattern';
+import { logger } from '../logger';
+import { isApplication } from '../context/projectTypeUtils';
+
+const doDeploy = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> => {
+	throw new Error();
+};
+
+const handleDeployByProject = (
+	context: BuildContext
+): TE.TaskEither<Error, BuildContext> =>
+	match(context)
+		.with({ projectType: when(isApplication) }, doDeploy)
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
 
 const execute: StageFunction = (context) => {
 	throw new Error();
