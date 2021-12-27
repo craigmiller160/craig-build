c51d06ca65b79daa063f77e57987e670646a2825
Craig <craigmiller160@gmail.com>
2021-12-27 10:22:32 -0500

    Implemented first main test

--- test/stages/deployToKubernetes.test.ts
+++ test/stages/deployToKubernetes.test.ts
@@ -1,15 +1,27 @@
 import { runCommandMock } from '../testutils/runCommandMock';
+import { getCwdMock } from '../testutils/getCwdMock';
 import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import { ProjectType } from '../../src/context/ProjectType';
 import { deployToKubernetes } from '../../src/stages/deployToKubernetes';
 import '@relmify/jest-fp-ts';
+import path from 'path';
+import { baseWorkingDir } from '../testutils/baseWorkingDir';
+import * as TE from 'fp-ts/TaskEither';
 
-const baseBuildContext = createBuildContext();
+const baseBuildContext = createBuildContext({
+	projectInfo: {
+		group: 'craigmiller160',
+		name: 'my-project',
+		version: '1.0.0',
+		isPreRelease: false
+	}
+});
 
 describe('deployToKubernetes', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
+		runCommandMock.mockImplementation(() => TE.right(''));
 	});
 
 	it('skips for NpmLibrary', async () => {
@@ -22,6 +34,7 @@ describe('deployToKubernetes', () => {
 		expect(result).toEqualRight(buildContext);
 
 		expect(runCommandMock).not.toHaveBeenCalled();
+		expect(getCwdMock).not.toHaveBeenCalled();
 	});
 
 	it('skips for MavenLibrary', async () => {
@@ -34,6 +47,7 @@ describe('deployToKubernetes', () => {
 		expect(result).toEqualRight(buildContext);
 
 		expect(runCommandMock).not.toHaveBeenCalled();
+		expect(getCwdMock).not.toHaveBeenCalled();
 	});
 
 	it('skips for DockerImage', async () => {
@@ -46,10 +60,29 @@ describe('deployToKubernetes', () => {
 		expect(result).toEqualRight(buildContext);
 
 		expect(runCommandMock).not.toHaveBeenCalled();
+		expect(getCwdMock).not.toHaveBeenCalled();
 	});
 
 	it('deploys for MavenApplication', async () => {
-		throw new Error();
+		const baseCwd = path.join(baseWorkingDir, 'mavenReleaseApplication');
+		getCwdMock.mockImplementation(() => baseCwd);
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenApplication
+		};
+
+		const result = await deployToKubernetes.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledTimes(1);
+		expect(runCommandMock).toHaveBeenNthCalledWith(
+			1,
+			'KUBE_IMG_VERSION=1.0.0 envsubst < deployment.yml | kubectl apply -f -',
+			{
+				cwd: path.join(baseWorkingDir, 'deploy'),
+				printOutput: true
+			}
+		);
 	});
 
 	it('deploys for NpmApplication', async () => {
