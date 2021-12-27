90edba958dcebfd0a89bbb9f6f902a58bc1cbd33
Craig <craigmiller160@gmail.com>
2021-12-27 13:02:32 -0500

    Stubbed out more tests

--- test/execute.test.ts
+++ test/execute.test.ts
@@ -1,32 +1,82 @@
-import { execute } from '../src/execute';
-import { BuildContext } from '../src/context/BuildContext';
 import {
 	createBuildContext,
 	createIncompleteBuildContext
 } from './testutils/createBuildContext';
-import { conditionalStages, setupStages } from '../src/stages';
-import * as TE from 'fp-ts/TaskEither';
 import '@relmify/jest-fp-ts';
-import { Context } from '../src/context/Context';
+import { ConditionalStage, SetupStage } from '../src/stages/Stage';
+import { IncompleteBuildContext } from '../src/context/IncompleteBuildContext';
+import * as O from 'fp-ts/Option';
+import { CommandType } from '../src/context/CommandType';
+
+const setupStageExecuteFn = jest.fn();
+const conditionalStageExecuteFn = jest.fn();
+
+const createSetupStageMock = (stage: SetupStage): SetupStage => ({
+	name: stage.name,
+	execute: setupStageExecuteFn
+});
+
+const createConditionalStageMock = (
+	stage: ConditionalStage
+): ConditionalStage => ({
+	name: stage.name,
+	execute: conditionalStageExecuteFn,
+	commandAllowsStage: stage.commandAllowsStage,
+	projectAllowsStage: stage.projectAllowsStage
+});
+
+const baseIncompleteContext = createIncompleteBuildContext();
+const baseContext = createBuildContext();
 
 describe('execute', () => {
-	it('executes full build for MavenApplication', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('executes full build for release MavenApplication', () => {
+		const incompleteContext: IncompleteBuildContext = {
+			...baseIncompleteContext,
+			commandInfo: O.some({
+				type: CommandType.FULL_BUILD
+			})
+		};
+
 		throw new Error();
 	});
 
-	it('executes full build for MavenLibrary', () => {
+	it('executes full build for pre-release MavenApplication', () => {
 		throw new Error();
 	});
 
-	it('executes full build for NpmApplication', () => {
+	it('executes full build for release MavenLibrary', () => {
 		throw new Error();
 	});
 
-	it('executes full build for NpmLibrary', () => {
+	it('executes full build for pre-release MavenLibrary', () => {
 		throw new Error();
 	});
 
-	it('executes full build for NpmApplication', () => {
+	it('executes full build for release NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('executes full build for pre-release NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('executes full build for release NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes full build for pre-release NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes full build for release NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('executes full build for pre-release NpmApplication', () => {
 		throw new Error();
 	});
 });
