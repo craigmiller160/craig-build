afdd1bfa9c16cc8189c8b0742f246e1b08b05f88
Craig <craigmiller160@gmail.com>
2021-12-27 12:39:18 -0500

    Major refactor of execute flow

--- src/execute.ts
+++ src/execute.ts
@@ -10,32 +10,61 @@ import { IncompleteBuildContext } from './context/IncompleteBuildContext';
 import { Context } from './context/Context';
 import { toLoggableContext } from './context/toLoggableContext';
 import { fromIncompleteContext } from './context/fromIncompleteContext';
-import { BaseStage } from './stages/Stage';
+import { BaseStage, ConditionalStage } from './stages/Stage';
+import * as P from 'fp-ts/Predicate';
+import * as O from 'fp-ts/Option';
+import { match, when } from 'ts-pattern';
+
+const isConditionalStage = (stage: BaseStage<any>): stage is ConditionalStage =>
+	(stage as any).commandAllowsStage !== undefined;
+
+const shouldStageRun = <Ctx extends Context>(
+	context: Ctx,
+	stage: BaseStage<Ctx>
+): boolean =>
+	match(stage)
+		.with(when(isConditionalStage), (_: ConditionalStage) =>
+			pipe(
+				_.commandAllowsStage,
+				P.and(_.projectAllowsStage)
+			)(context as unknown as BuildContext)
+		)
+		.otherwise(() => true);
 
 const executeStage = <Ctx extends Context>(
+	context: Ctx,
+	stage: BaseStage<Ctx>
+): TE.TaskEither<Error, Ctx> =>
+	match(shouldStageRun(context, stage))
+		.with(true, () => stage.execute(context))
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
+
+const setupAndExecuteStage = <Ctx extends Context>(
 	contextTE: TE.TaskEither<Error, Ctx>,
 	stage: BaseStage<Ctx>
 ) =>
 	pipe(
 		contextTE,
-		TE.chain((context) => {
+		TE.map((_) => {
 			logger.info(`Starting stage: ${stage.name}`);
-			return pipe(
-				stage.execute(context),
-				TE.map((_) => {
-					logger.info(
-						`Completed stage: ${stage.name} ${EU.getOrThrow(
-							stringifyJson(toLoggableContext(_), 2)
-						)}`
-					);
-					return _;
-				}),
-				TE.mapLeft((_) => {
-					logger.error(`Error in stage: ${stage.name}`);
-					logger.error(_);
-					return _;
-				})
+			return _;
+		}),
+		TE.chain((_) => executeStage(_, stage)),
+		TE.map((_) => {
+			logger.info(
+				`Completed stage: ${stage.name} ${EU.getOrThrow(
+					stringifyJson(toLoggableContext(_), 2)
+				)}`
 			);
+			return _;
+		}),
+		TE.mapLeft((_) => {
+			logger.error(`Error in stage: ${stage.name}`);
+			logger.error(_);
+			return _;
 		})
 	);
 
@@ -46,7 +75,7 @@ const executeAllStages = <Ctx extends Context>(
 	pipe(
 		stages,
 		A.reduce(TE.right<Error, Ctx>(context), (ctxTE, stage) =>
-			executeStage(ctxTE, stage)
+			setupAndExecuteStage(ctxTE, stage)
 		)
 	);
 
