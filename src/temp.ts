ababd3ab127624335f3610d2c4e33e463f9a89b6
Craig <craigmiller160@gmail.com>
2021-12-27 12:08:05 -0500

    More work on new prototype

--- src/temp.ts
+++ src/temp.ts
@@ -3,6 +3,7 @@ import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import * as P from 'fp-ts/Predicate';
 import { createBuildContext } from '../test/testutils/createBuildContext';
+import { match, when } from 'ts-pattern';
 
 type StageExecuteFn = (
 	context: BuildContext
@@ -23,13 +24,32 @@ const doRunStage = (stage: ConditionalStage): P.Predicate<BuildContext> => {
 };
 
 const buildContext: BuildContext = createBuildContext();
-
-const result: boolean = doRunStage({
+const theStage: ConditionalStage = {
 	name: '',
 	execute: () => TE.left(new Error()),
 	commandAllowsStage: () => true,
 	projectAllowsStage: () => true
-})(buildContext);
+};
+
+const runStage = (
+	context: BuildContext,
+	stage: ConditionalStage
+): TE.TaskEither<Error, BuildContext> => {
+	const shouldRunStage = pipe(
+		stage.commandAllowsStage,
+		P.and(stage.commandAllowsStage)
+	);
+	return match(context)
+		.with(when(shouldRunStage), stage.execute)
+		.otherwise(() => TE.right(context));
+};
+
+pipe(
+	TE.right(buildContext),
+	TE.chain((_) => runStage(_, theStage))
+);
+
+const result: boolean = doRunStage(theStage)(buildContext);
 
 /*
  * EarlyStages will all always run
