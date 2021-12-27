483b3279897cf81b4f72d529fab53ae2d81c9851
Craig <craigmiller160@gmail.com>
2021-12-27 12:16:43 -0500

    Completely refactored Stage types

--- src/stages/Stage.ts
+++ src/stages/Stage.ts
@@ -2,19 +2,20 @@ import { BuildContext } from '../context/BuildContext';
 import * as TE from 'fp-ts/TaskEither';
 import { IncompleteBuildContext } from '../context/IncompleteBuildContext';
 import { Context } from '../context/Context';
+import * as P from 'fp-ts/Predicate';
 
-export type BaseStageFunction<Ctx extends Context> = (
+export type StageExecuteFn<Ctx extends Context> = (
 	context: Ctx
 ) => TE.TaskEither<Error, Ctx>;
-export type EarlyStageFunction = BaseStageFunction<IncompleteBuildContext>;
-export type StageFunction = BaseStageFunction<BuildContext>;
 
-export interface BaseStage<
-	Ctx extends Context,
-	StageFn extends BaseStageFunction<Ctx>
-> {
+export interface BaseStage<Ctx extends Context> {
 	readonly name: string;
-	readonly execute: StageFn;
+	execute: StageExecuteFn<Ctx>;
 }
-export type EarlyStage = BaseStage<IncompleteBuildContext, EarlyStageFunction>;
-export type Stage = BaseStage<BuildContext, StageFunction>;
+
+export type EarlyStage = BaseStage<IncompleteBuildContext>;
+
+export interface ConditionalStage extends BaseStage<BuildContext> {
+	commandAllowsStage: P.Predicate<BuildContext>;
+	projectAllowsStage: P.Predicate<BuildContext>;
+}
