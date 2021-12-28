686710f042fa5a295f61209facda9febde6d5c6d
Craig <craigmiller160@gmail.com>
2021-12-28 09:43:46 -0500

    Fixed lint errors

--- src/execute/index.ts
+++ src/execute/index.ts
@@ -11,74 +11,74 @@ import { stages } from './stages';
 import { Stage } from './stages/Stage';
 
 const shouldStageRun = <Ctx extends Context>(
-    context: Ctx,
-    stage: BaseStage<Ctx>
+	context: Ctx,
+	stage: BaseStage<Ctx>
 ): boolean =>
-    match(stage)
-        .with(when(isConditionalStage), (_: ConditionalStage) =>
-            pipe(
-                _.commandAllowsStage,
-                P.and(_.projectAllowsStage)
-            )(context as unknown as BuildContext)
-        )
-        .otherwise(() => true);
+	match(stage)
+		.with(when(isConditionalStage), (_: ConditionalStage) =>
+			pipe(
+				_.commandAllowsStage,
+				P.and(_.projectAllowsStage)
+			)(context as unknown as BuildContext)
+		)
+		.otherwise(() => true);
 
 const executeStage = <Ctx extends Context>(
-    context: Ctx,
-    stage: BaseStage<Ctx>
+	context: Ctx,
+	stage: BaseStage<Ctx>
 ): TE.TaskEither<Error, Ctx> =>
-    match(shouldStageRun(context, stage))
-        .with(true, () => stage.execute(context))
-        .otherwise(() => {
-            logger.debug('Skipping stage');
-            return TE.right(context);
-        });
+	match(shouldStageRun(context, stage))
+		.with(true, () => stage.execute(context))
+		.otherwise(() => {
+			logger.debug('Skipping stage');
+			return TE.right(context);
+		});
 
 const verifyAndExecuteStage = (
-    contextTE: TE.TaskEither<Error, BuildContext>,
-    stage: Stage
+	contextTE: TE.TaskEither<Error, BuildContext>,
+	stage: Stage
 ): TE.TaskEither<Error, BuildContext> => {
-    pipe(
-        contextTE,
-        TE.map((_) => {
-            logger.info(`Starting stage: ${stage.name}`);
-            return _;
-        })
-    );
+	pipe(
+		contextTE,
+		TE.map((_) => {
+			logger.info(`Starting stage: ${stage.name}`);
+			return _;
+		})
+	);
 };
 
 const setupAndExecuteStage = <Ctx extends Context>(
-    contextTE: TE.TaskEither<Error, Ctx>,
-    stage: BaseStage<Ctx>
+	contextTE: TE.TaskEither<Error, Ctx>,
+	stage: BaseStage<Ctx>
 ) =>
-    pipe(
-        contextTE,
-        TE.map((_) => {
-            logger.info(`Starting stage: ${stage.name}`);
-            return _;
-        }),
-        TE.chain((_) => executeStage(_, stage)),
-        TE.map((_) => {
-            logger.info(
-                `Completed stage: ${stage.name} ${EU.getOrThrow(
-                    stringifyJson(_, 2)
-                )}`
-            );
-            return _;
-        }),
-        TE.mapLeft((_) => {
-            logger.error(`Error in stage: ${stage.name}`);
-            logger.error(_);
-            return _;
-        })
-    );
+	pipe(
+		contextTE,
+		TE.map((_) => {
+			logger.info(`Starting stage: ${stage.name}`);
+			return _;
+		}),
+		TE.chain((_) => executeStage(_, stage)),
+		TE.map((_) => {
+			logger.info(
+				`Completed stage: ${stage.name} ${EU.getOrThrow(
+					stringifyJson(_, 2)
+				)}`
+			);
+			return _;
+		}),
+		TE.mapLeft((_) => {
+			logger.error(`Error in stage: ${stage.name}`);
+			logger.error(_);
+			return _;
+		})
+	);
 
 export const execute = (
-    context: BuildContext
+	context: BuildContext
 ): TE.TaskEither<Error, BuildContext> =>
-    pipe(
-        stages,
-        A.reduce(TE.right<Error, BuildContext>(context), (ctxTE, stage) =>
-            verifyAndExecuteStage(ctxTE, stage)
-        )
-    );
+	pipe(
+		stages,
+		A.reduce(TE.right<Error, BuildContext>(context), (ctxTE, stage) =>
+			verifyAndExecuteStage(ctxTE, stage)
+		)
+	);
