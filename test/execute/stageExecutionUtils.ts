3f75327e493cca57b6fd14aa64c9122fa7ba5b5f
Craig <craigmiller160@gmail.com>
2021-12-28 09:52:42 -0500

    Setup all utility functions for stageExecutionUtils

--- test/execute/stageExecutionUtils.ts
+++ test/execute/stageExecutionUtils.ts
@@ -2,6 +2,9 @@ import { Stage } from '../../src/stages/Stage';
 import { BuildContext } from '../../src/context/BuildContext';
 import { StageExecution } from './StageExecution';
 import { StageExecutionStatus } from './StageExecutionStatus';
+import { match, when } from 'ts-pattern';
+
+// TODO write tests for these
 
 export const createStageExecution = (
 	stage: Stage,
@@ -11,3 +14,41 @@ export const createStageExecution = (
 	context,
 	status: StageExecutionStatus.Proceed
 });
+
+export const proceedIfCommandAllows = (
+	execution: StageExecution
+): StageExecution =>
+	match(execution)
+		.with(
+			{
+				status: StageExecutionStatus.Proceed,
+				stage: when((stage) =>
+					stage.commandAllowsStage(execution.context)
+				)
+			},
+			(_) => _
+		)
+		.with({ status: StageExecutionStatus.SkipForProject }, (_) => _)
+		.otherwise((_) => ({
+			..._,
+			status: StageExecutionStatus.SkipForCommand
+		}));
+
+export const proceedIfProjectAllows = (
+	execution: StageExecution
+): StageExecution =>
+	match(execution)
+		.with(
+			{
+				status: StageExecutionStatus.Proceed,
+				stage: when((stage) =>
+					stage.commandAllowsStage(execution.context)
+				)
+			},
+			(_) => _
+		)
+		.with({ status: StageExecutionStatus.SkipForCommand }, (_) => _)
+		.otherwise((_) => ({
+			..._,
+			status: StageExecutionStatus.SkipForCommand
+		}));
