d1c29b7fb912b715998c280dbfca4170b6d34b87
Craig <craigmiller160@gmail.com>
2021-12-28 09:46:11 -0500

    Setting up container types

---
+++ test/execute/StageExecution.ts
@@ -0,0 +1,9 @@
+import { Stage } from '../../src/stages/Stage';
+import { BuildContext } from '../../src/context/BuildContext';
+import { StageExecutionStatus } from './StageExecutionStatus';
+
+interface StageExecution {
+	readonly stage: Stage;
+	readonly context: BuildContext; // TODO see if there's some way to drop this
+	readonly status: StageExecutionStatus;
+}
