bae0f46d5d0cfe01780ee411aca58bae94443be7
Craig <craigmiller160@gmail.com>
2021-12-28 17:38:31 -0500

    Implemented more tests

---
+++ test/execute/execute.kubernetesOnly.test.ts
@@ -0,0 +1,53 @@
+import { createBuildContext } from '../testutils/createBuildContext';
+
+const baseContext = createBuildContext();
+
+describe('execute.kubernetesOnly', () => {
+	it('executes kubernetes only for release MavenApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release MavenApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for release MavenLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release MavenLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for release NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release NpmApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for release NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release NpmLibrary', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for release DockerApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release DockerApplication', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for release DockerImage', () => {
+		throw new Error();
+	});
+
+	it('executes kubernetes only for pre-release DockerImage', () => {
+		throw new Error();
+	});
+});
