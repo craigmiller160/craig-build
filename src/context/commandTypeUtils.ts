5fccebbf264f27b33c61c8cf25d0e9a1b4366fd4
Craig <craigmiller160@gmail.com>
2021-12-28 18:48:50 -0500

    Implemented new helper functions and tests

---
+++ src/context/commandTypeUtils.ts
@@ -0,0 +1,11 @@
+import * as P from 'fp-ts/Predicate';
+import { CommandType } from './CommandType';
+
+export const isFullBuild: P.Predicate<CommandType> = (commandType) =>
+	CommandType.FullBuild === commandType;
+
+export const isDockerOnly: P.Predicate<CommandType> = (commandType) =>
+	CommandType.DockerOnly === commandType;
+
+export const isKubernetesOnly: P.Predicate<CommandType> = (commandType) =>
+	CommandType.KubernetesOnly === commandType;
