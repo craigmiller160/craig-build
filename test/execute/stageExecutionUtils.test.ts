a64362ded74eb98739aa453f4f9a0a4ffdd58c9c
Craig <craigmiller160@gmail.com>
2021-12-28 11:07:08 -0500

    Stubbed out new tests

---
+++ test/execute/stageExecutionUtils.test.ts
@@ -0,0 +1,49 @@
+export {};
+
+describe('stageExecutionUtils', () => {
+	it('createStageExecution', () => {
+		throw new Error();
+	});
+
+	describe('proceedIfCommandAllowed', () => {
+		it('status is Proceed and commandAllowsStage returns true', () => {
+			throw new Error();
+		});
+
+		it('status is Proceed and commandAllowsStage returns false', () => {
+			throw new Error();
+		});
+
+		it('status is SkipForProject', () => {
+			throw new Error();
+		});
+	});
+
+	describe('proceedIfProjectAllowed', () => {
+		it('status is Proceed and projectAllowsStage returns true', () => {
+			throw new Error();
+		});
+
+		it('status is Proceed and projectAllowsStage returns false', () => {
+			throw new Error();
+		});
+
+		it('status is SkipForCommand', () => {
+			throw new Error();
+		});
+	});
+
+	describe('executeIfAllowed', () => {
+		it('status is Proceed', () => {
+			throw new Error();
+		});
+
+		it('status is SkipForCommand', () => {
+			throw new Error();
+		});
+
+		it('status is SkipForProject', () => {
+			throw new Error();
+		});
+	});
+});
