555b7963dceaa8694c9bd26f24b1cec6ee7eb37b
Craig <craigmiller160@gmail.com>
2021-12-29 14:40:10 -0500

    Tweaked publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 yarn publish --no-git-tag-version --new-version $1
-git add package.json
-git commit -m "Bumped version"
-git push
+if [ $? -eq 0 ]; then
+  git add package.json
+  git commit -m "Bumped version"
+  git push
+fi
