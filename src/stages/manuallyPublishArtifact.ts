1a81cba4d46f1104385a6a0259668909cd9b0c8a
Craig Miller <craigmiller160@gmail.com>
2022-01-17 11:34:49 -0500

    Feature/publish dir (#63)
    * Setting up test data
    
    * Implemented test
    
    * Setup logic to use a custom publish directory
    
    * Fixed lint
    
    * Updating tests
    
    * Adding lint-staged and husky
    
    * Fixed remaining tests
    
    * Fixed lint
    
    * Bumped version
    
    * Bumped version again
    
    * Bumped version
    
    * Added logging
    
    * Bumped version

--- src/stages/manuallyPublishArtifact.ts
+++ src/stages/manuallyPublishArtifact.ts
@@ -7,22 +7,48 @@ import { runCommand } from '../command/runCommand';
 import { Stage, StageExecuteFn } from './Stage';
 import * as P from 'fp-ts/Predicate';
 import { isFullBuild } from '../context/commandTypeUtils';
+import { readFile } from '../functions/File';
+import { getCwd } from '../command/getCwd';
+import path from 'path';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { parseJson } from '../functions/Json';
+import { PackageJson } from '../configFileTypes/PackageJson';
+import { NPM_PROJECT_FILE } from '../configFileTypes/constants';
+import { logger } from '../logger';
 
 export const NPM_PUBLISH_COMMAND =
 	'yarn publish --no-git-tag-version --new-version';
 
 export const CLEAR_FILES_COMMAND = 'git checkout .';
 
+const getPublishDir = (): string =>
+	pipe(
+		readFile(path.resolve(getCwd(), NPM_PROJECT_FILE)),
+		E.chain((_) => parseJson<PackageJson>(_)),
+		E.map((_) => O.fromNullable(_.publishDirectory)),
+		O.fromEither,
+		O.flatten,
+		O.fold(
+			() => getCwd(),
+			(_) => path.join(getCwd(), _)
+		)
+	);
+
 const publishNpmArtifact = (
 	context: BuildContext
-): TE.TaskEither<Error, BuildContext> =>
-	pipe(
+): TE.TaskEither<Error, BuildContext> => {
+	const publishDir = getPublishDir();
+	logger.debug(`NPM artifact publish directory: ${publishDir}`);
+	return pipe(
 		runCommand(`${NPM_PUBLISH_COMMAND} ${context.projectInfo.version}`, {
-			printOutput: true
+			printOutput: true,
+			cwd: publishDir
 		}),
 		TE.chain(() => runCommand(CLEAR_FILES_COMMAND)),
 		TE.map(() => context)
 	);
+};
 
 const handlePublishByProject = (
 	context: BuildContext
--- test/stages/manuallyPublishArtifact.test.ts
+++ test/stages/manuallyPublishArtifact.test.ts
@@ -3,12 +3,15 @@ import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import { ProjectType } from '../../src/context/ProjectType';
 import { runCommandMock } from '../testutils/runCommandMock';
+import { getCwdMock } from '../testutils/getCwdMock';
 import {
 	CLEAR_FILES_COMMAND,
 	manuallyPublishArtifact
 } from '../../src/stages/manuallyPublishArtifact';
 import * as TE from 'fp-ts/TaskEither';
 import { NPM_PUBLISH_COMMAND } from '../../src/stages/manuallyPublishArtifact';
+import path from 'path';
+import { baseWorkingDir } from '../testutils/baseWorkingDir';
 
 const baseBuildContext = createBuildContext();
 
@@ -18,6 +21,10 @@ describe('manuallyPublishArtifact', () => {
 	});
 
 	it('publishes NPM project', async () => {
+		const projectPath = path.join(baseWorkingDir, 'npmReleaseApplication');
+		getCwdMock.mockImplementation(() =>
+			path.join(baseWorkingDir, 'npmReleaseApplication')
+		);
 		runCommandMock.mockImplementation(() => TE.right(''));
 		const buildContext: BuildContext = {
 			...baseBuildContext,
@@ -35,7 +42,38 @@ describe('manuallyPublishArtifact', () => {
 		expect(runCommandMock).toHaveBeenNthCalledWith(
 			1,
 			`${NPM_PUBLISH_COMMAND} 1.0.0`,
-			{ printOutput: true }
+			{ printOutput: true, cwd: projectPath }
+		);
+		expect(runCommandMock).toHaveBeenNthCalledWith(2, CLEAR_FILES_COMMAND);
+	});
+
+	it('publishes NPM project with publishDirectory', async () => {
+		const projectPath = path.join(
+			baseWorkingDir,
+			'npmReleaseApplicationWithPublishDir'
+		);
+		getCwdMock.mockImplementation(() => projectPath);
+		runCommandMock.mockImplementation(() => TE.right(''));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmApplication,
+			projectInfo: {
+				...baseBuildContext.projectInfo,
+				version: '1.0.0'
+			}
+		};
+
+		const result = await manuallyPublishArtifact.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledTimes(2);
+		expect(runCommandMock).toHaveBeenNthCalledWith(
+			1,
+			`${NPM_PUBLISH_COMMAND} 1.0.0`,
+			{
+				printOutput: true,
+				cwd: path.join(projectPath, 'lib')
+			}
 		);
 		expect(runCommandMock).toHaveBeenNthCalledWith(2, CLEAR_FILES_COMMAND);
 	});
