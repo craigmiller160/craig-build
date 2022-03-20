0234308b85a612515293e62b2d2df4d2c9df1ce6
Craig Miller <craigmiller160@gmail.com>
2022-03-20 08:46:44 -0400

    More progress parsing gradle

--- src/ast/gradleKotlin/index.ts
+++ src/ast/gradleKotlin/index.ts
@@ -3,12 +3,34 @@ import * as Either from 'fp-ts/Either';
 import path from 'path';
 import { getCwd } from '../../command/getCwd';
 import { pipe } from 'fp-ts/function';
+import { match, when } from 'ts-pattern';
+import * as RArray from 'fp-ts/ReadonlyArray';
+import * as Option from 'fp-ts/Option';
+import * as Regex from '../../functions/RegExp';
+
+const COMMENT_REGEX = /^\/\/.*$/;
+const testCommentRegex = Regex.regexTest(COMMENT_REGEX);
+
+enum LineType {
+	COMMENT
+}
+
+type LineAndType = [line: string, type: LineType];
 
 interface Context {
 	readonly sectionName: string;
 }
 
-const parse = (context: Context, lines: ReadonlyArray<string>) => {};
+const getLineType = (line: string): Option.Option<LineAndType> =>
+	match(line.trim())
+		.with(when(testCommentRegex), () =>
+			Option.some<LineAndType>([line, LineType.COMMENT])
+		)
+		.otherwise(() => Option.none);
+
+const parse = (context: Context, lines: ReadonlyArray<string>) => {
+	pipe(RArray.head(lines), Option.chain(getLineType));
+};
 
 const parseSettingsGradle = () => {
 	pipe(
@@ -18,4 +40,6 @@ const parseSettingsGradle = () => {
 	);
 };
 
-export const parseGradleConfig = () => {};
+export const parseGradleConfig = () => {
+	parseSettingsGradle();
+};
