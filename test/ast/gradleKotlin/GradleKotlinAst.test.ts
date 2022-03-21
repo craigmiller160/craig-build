24b0e38547253d9676fe1867508ce17100287922
Craig Miller <craigmiller160@gmail.com>
2022-03-21 17:32:08 -0400

    Got tests working for nested children

--- test/ast/gradleKotlin/GradleKotlinAst.test.ts
+++ test/ast/gradleKotlin/GradleKotlinAst.test.ts
@@ -3,6 +3,12 @@ import path from 'path';
 import { parseGradleAst } from '../../../src/ast/gradleKotlin';
 import '@relmify/jest-fp-ts';
 
+const emptyChild = {
+	name: 'child',
+	properties: {},
+	children: []
+};
+
 describe('GradleKotlin AST parser', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
@@ -25,11 +31,26 @@ describe('GradleKotlin AST parser', () => {
 				'rootProject.name': 'spring-gradle-playground',
 				'rootProject.buildFileName': 'foobar',
 				group: 'io.craigmiller160',
-				version: '1.0.0',
-				sourceCompatibility: 'JavaVersion.VERSION_17',
-				targetCompatibility: 'JavaVersion.VERSION_17'
+				version: '1.0.0'
 			},
-			children: []
+			children: [
+				emptyChild,
+				{
+					name: 'child',
+					properties: {
+						sourceCompatibility: 'JavaVersion.VERSION_17',
+						targetCompatibility: 'JavaVersion.VERSION_17'
+					},
+					children: []
+				},
+				emptyChild,
+				emptyChild,
+				{
+					name: 'child',
+					properties: {},
+					children: [emptyChild]
+				}
+			]
 		});
 	});
 });
