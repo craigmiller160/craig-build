2615426ab9f6b13e0e7961dd5b58d7d34ccbe963
Craig Miller <craigmiller160@gmail.com>
2022-05-08 15:24:52 -0400

    Added some test files

---
+++ test/__working-dirs__/gradleKotlinReleaseApplicationBadDependency/deploy/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot").version("2.6.4")
+    id("io.spring.dependency-management").version("1.0.11.RELEASE")
+}
+
+group = "io.craigmiller160"
+version = "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val postgresVersion = "42.2.18-SNAPSHOT"
+    val webUtilsVersion = "1.1.0-SNAPSHOT"
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("io.craigmiller160:spring-web-utils:$webUtilsVersion")
+}
+
+tasks {
+    named("test") {
+//        useJUnitPlatform()
+    }
+}
