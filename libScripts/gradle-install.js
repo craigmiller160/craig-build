12c79517852ea992b66a83c2bfe45d1aece7b5aa
Craig Miller <craigmiller160@gmail.com>
2022-05-28 19:21:18 -0400

    Beginning work on gradle install script

--- libScripts/gradle-install.js
+++ libScripts/gradle-install.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const qs = require('qs');
+
+const getNexusSearchUri = () => {
+	const queryObj = {
+		'maven.groupId': 'io.craigmiller160',
+		'maven.artifactId': 'craig-build-gradle-tool',
+		repository: 'maven-releases',
+		sort: 'version',
+		direction: 'desc'
+	};
+	return `/search?${qs.stringify(queryObj)}`;
+};
