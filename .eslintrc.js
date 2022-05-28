8929db715f055ea50e9fab6ae3beecfebb32eb05
Craig Miller <craigmiller160@gmail.com>
2022-05-28 19:17:33 -0400

    Improved eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
 	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module'
+		ecmaVersion: '2020'
 	},
+	env: {
+		browser: true,
+		amd: true,
+		node: true
+	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	overrides: [
+		{
+			files: ['**/*.ts?(x)'],
+			parser: '@typescript-eslint/parser',
+			extends: ['plugin:@typescript-eslint/recommended']
+		}
+	],
 	rules: {
 		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 		'no-console': ['error']
