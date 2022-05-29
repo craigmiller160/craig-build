28efbf6db4448d86f7e01fa7b51b077df02ac7f3
Craig Miller <craigmiller160@gmail.com>
2022-05-29 09:12:00 -0400

    Added some filesystem prep stuff

--- libScripts/gradle-install.js
+++ libScripts/gradle-install.js
@@ -3,12 +3,30 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const qs = require('qs');
+const os = require('os');
 
 const LIB_VERSION = '1.0.0';
 
 const restApiInstance = axios.create({
 	baseURL: 'https://craigmiller160.ddns.net:30003/service/rest/v1'
 });
+const targetDirectory = path.join(os.homedir(), '.craig-build', 'gradle');
+const targetFile = path.join(
+	targetDirectory,
+	`craig-build-gradle-tool-${LIB_VERSION}.jar`
+);
+
+const prepareDirectory = () => {
+	if (fs.existsSync(targetDirectory)) {
+		fs.rmSync(targetDirectory, {
+			force: true,
+			recursive: true
+		});
+	}
+	fs.mkdirSync(targetDirectory, {
+		recursive: true
+	});
+};
 
 const searchForLib = () => {
 	const queryObj = {
@@ -32,4 +50,5 @@ const handleError = (error) => {
 	process.exit(1);
 };
 
+prepareDirectory();
 searchForLib().then(getDownloadUrl).catch(handleError);
