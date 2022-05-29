a4de63463847b5395b1ff1167df20faed82c61fc
Craig Miller <craigmiller160@gmail.com>
2022-05-29 09:15:55 -0400

    Fixed eslint bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
 	env: {
 		browser: true,
 		amd: true,
-		node: true
+		node: true,
+		es6: true
 	},
 	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 	overrides: [
