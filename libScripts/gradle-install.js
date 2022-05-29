b7f6688d76f0cb27febadfa6dd151be545707028
Craig Miller <craigmiller160@gmail.com>
2022-05-29 09:06:59 -0400

    Progress with install script

--- libScripts/gradle-install.js
+++ libScripts/gradle-install.js
@@ -1,15 +1,32 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const qs = require('qs');
 
-const getNexusSearchUri = () => {
+const LIB_VERSION = '1.0.0';
+
+const searchForLib = () => {
 	const queryObj = {
 		'maven.groupId': 'io.craigmiller160',
 		'maven.artifactId': 'craig-build-gradle-tool',
 		repository: 'maven-releases',
 		sort: 'version',
-		direction: 'desc'
+		direction: 'desc',
+		version: LIB_VERSION
 	};
-	return `/search?${qs.stringify(queryObj)}`;
+	return axios.get(`/search?${qs.stringify(queryObj)}`);
 };
+
+const getDownloadUrl = (result) =>
+	result.items[0].assets.find((asset) => asset.downloadUrl.endsWith('jar'))
+		.downloadUrl;
+
+const handleError = (error) => {
+	console.error('Error while installing gradle lib', error);
+	process.exit(1);
+};
+
+searchForLib()
+	.then(getDownloadUrl)
+	.catch(handleError);
