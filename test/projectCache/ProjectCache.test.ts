1e330b3eea9086e8f1ee999860d0dcddd4aa067e
Craig Miller <craigmiller160@gmail.com>
2022-05-30 08:52:03 -0400

    Working on caching tests

--- test/projectCache/ProjectCache.test.ts
+++ test/projectCache/ProjectCache.test.ts
@@ -1,4 +1,14 @@
-import { getRawProjectData } from '../../src/projectCache';
+import { getCwdMock } from '../testutils/getCwdMock';
+import path from 'path';
+import { baseWorkingDir } from '../testutils/baseWorkingDir';
+import { ProjectType } from '../../src/context/ProjectType';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { VersionType } from '../../src/context/VersionType';
+import { getProjectInfo } from '../../src/stages/getProjectInfo';
+import '@relmify/jest-fp-ts';
+
+const baseBuildContext = createBuildContext();
 
 describe('caching raw project data', () => {
 	beforeEach(() => {
@@ -10,6 +20,37 @@ describe('caching raw project data', () => {
 	});
 
 	it('loads the data once, uses cache after that', async () => {
-		throw new Error();
+		getCwdMock.mockImplementation(() =>
+			path.resolve(baseWorkingDir, 'npmReleaseLibrary')
+		);
+
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.NpmLibrary
+		};
+		const expectedContext: BuildContext = {
+			...buildContext,
+			projectInfo: {
+				group: 'craigmiller160',
+				name: 'craig-build',
+				version: '1.0.0',
+				versionType: VersionType.Release
+			}
+		};
+		// Reads the actual project file
+		const result = await getProjectInfo.execute(buildContext)();
+		expect(result).toEqualRight(expectedContext);
+
+		getCwdMock.mockImplementation(() =>
+			path.resolve(baseWorkingDir, 'MavenReleaseLibrary')
+		);
+
+		const differentBuildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenLibrary
+		};
+		// Re-uses the same project file from cache despite args being different
+		const result2 = await getProjectInfo.execute(differentBuildContext)();
+		expect(result2).toEqualRight(expectedContext);
 	});
 });
