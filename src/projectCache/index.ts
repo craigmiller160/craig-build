43fb0d4875b18742b7ddd6f63e658e60eada4880
Craig Miller <craigmiller160@gmail.com>
2022-05-30 08:36:34 -0400

    More progress with read/cache stuff

--- src/projectCache/index.ts
+++ src/projectCache/index.ts
@@ -6,26 +6,37 @@ import { pipe } from 'fp-ts/function';
 import path from 'path';
 import { readFile } from '../functions/File';
 import { getCwd } from '../command/getCwd';
-import { MAVEN_PROJECT_FILE } from '../configFileTypes/constants';
+import {
+	MAVEN_PROJECT_FILE,
+	NPM_PROJECT_FILE
+} from '../configFileTypes/constants';
 import { PomXml } from '../configFileTypes/PomXml';
 import { parseXml } from '../functions/Xml';
 import { match, when } from 'ts-pattern';
-import { isMaven } from '../context/projectTypeUtils';
+import { isMaven, isNpm } from '../context/projectTypeUtils';
+import { PackageJson } from '../configFileTypes/PackageJson';
+import { parseJson } from '../functions/Json';
 
 let project: O.Option<unknown> = O.none;
 
-const readMavenProject = (): TE.TaskEither<Error, PomXml> =>
+const readMavenProject = (): E.Either<Error, PomXml> =>
 	pipe(
 		readFile(path.join(getCwd(), MAVEN_PROJECT_FILE)),
-		E.chain((_) => parseXml<PomXml>(_)),
-		TE.fromEither
+		E.chain((_) => parseXml<PomXml>(_))
+	);
+
+const readNpmProject = (): E.Either<Error, PackageJson> =>
+	pipe(
+		readFile(path.join(getCwd(), NPM_PROJECT_FILE)),
+		E.chain((_) => parseJson<PackageJson>(_))
 	);
 
 const readAndCacheRawProjectData = <T>(
 	projectType: ProjectType
 ): TE.TaskEither<Error, T> => {
-	const rawProjectTE = match(projectType)
-		.with(when(isMaven), readMavenProject)
+	const rawProjectTE: TE.TaskEither<Error, unknown> = match(projectType)
+		.with(when(isMaven), () => TE.fromEither(readMavenProject()))
+		.with(when(isNpm), () => TE.fromEither(readNpmProject()))
 		.run();
 	return pipe(
 		rawProjectTE,
