520dbe9a0a09fa1e26ab27179f3ab13ea0b5d646
Craig Miller <craigmiller160@gmail.com>
2022-05-30 08:34:38 -0400

    Even more progress on project data caching

--- src/projectCache/index.ts
+++ src/projectCache/index.ts
@@ -1,14 +1,39 @@
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
 import { ProjectType } from '../context/ProjectType';
 import { pipe } from 'fp-ts/function';
+import path from 'path';
+import { readFile } from '../functions/File';
+import { getCwd } from '../command/getCwd';
+import { MAVEN_PROJECT_FILE } from '../configFileTypes/constants';
+import { PomXml } from '../configFileTypes/PomXml';
+import { parseXml } from '../functions/Xml';
+import { match, when } from 'ts-pattern';
+import { isMaven } from '../context/projectTypeUtils';
 
-const project: O.Option<unknown> = O.none;
+let project: O.Option<unknown> = O.none;
+
+const readMavenProject = (): TE.TaskEither<Error, PomXml> =>
+	pipe(
+		readFile(path.join(getCwd(), MAVEN_PROJECT_FILE)),
+		E.chain((_) => parseXml<PomXml>(_)),
+		TE.fromEither
+	);
 
 const readAndCacheRawProjectData = <T>(
 	projectType: ProjectType
 ): TE.TaskEither<Error, T> => {
-	throw new Error();
+	const rawProjectTE = match(projectType)
+		.with(when(isMaven), readMavenProject)
+		.run();
+	return pipe(
+		rawProjectTE,
+		TE.map((data) => {
+			project = O.some(data);
+			return data as unknown as T;
+		})
+	);
 };
 
 export const getRawProjectData = <T>(
