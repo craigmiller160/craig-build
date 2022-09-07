4fdd2f6e3e943b4e3c8655b584adc8625772d27c
Craig Miller <craigmiller160@gmail.com>
2022-09-07 15:55:55 -0400

    Cleanup

--- src/stages/validateKubernetesConfig.ts
+++ src/stages/validateKubernetesConfig.ts
@@ -20,8 +20,6 @@ import { Stage, StageExecuteFn } from './Stage';
 import { logger } from '../logger';
 import * as EU from '../functions/EitherUtils';
 
-// TODO not entirely sure I still need this
-
 const KUBE_IMAGE_REGEX =
 	/^(?<repoPrefix>.*)\/(?<imageName>.*):(?<imageVersion>.*)$/;
 
