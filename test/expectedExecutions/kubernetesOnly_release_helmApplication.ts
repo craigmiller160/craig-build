65b93c1b51e9c3fc1598c33eff9239166e091f11
Craig Miller <craigmiller160@gmail.com>
2022-12-27 17:23:27 -0500

    More test data

---
+++ test/expectedExecutions/kubernetesOnly_release_helmApplication.ts
@@ -0,0 +1,39 @@
+import { validateDependencyVersions } from '../../src/stages/validateDependencyVersions';
+import { validateProjectVersionAllowed } from '../../src/stages/validateProjectVersionAllowed';
+import { validateGitTag } from '../../src/stages/validateGitTag';
+import { buildArtifact } from '../../src/stages/buildArtifact';
+import { preparePreReleaseVersion } from '../../src/stages/preparePreReleaseVersion';
+import { manuallyPublishArtifact } from '../../src/stages/manuallyPublishArtifact';
+import { gitTag } from '../../src/stages/gitTag';
+import { downloadArtifactForDeployment } from '../../src/stages/downloadArtifactForDeployment';
+import { buildAndPushDocker } from '../../src/stages/buildAndPushDocker';
+import { deployToKubernetes } from '../../src/stages/deployToKubernetes';
+import { ExpectedExecution } from './ExpectedExecution';
+import { getCommandInfo } from '../../src/stages/getCommandInfo';
+import { getBuildToolInfo } from '../../src/stages/getBuildToolInfo';
+import { validateBuildToolVersion } from '../../src/stages/validateBuildToolVersion';
+import { checkForUncommittedChanges } from '../../src/stages/checkForUncommittedChanges';
+import { getProjectType } from '../../src/stages/getProjectType';
+import { getProjectInfo } from '../../src/stages/getProjectInfo';
+import { waitOnNexusUpdate } from '../../src/stages/waitOnNexusUpdate';
+
+export const kubernetesOnly_release_helmApplication: ExpectedExecution = {
+	[getCommandInfo.name]: true,
+	[getBuildToolInfo.name]: true,
+	[validateBuildToolVersion.name]: true,
+	[checkForUncommittedChanges.name]: true,
+	[getProjectType.name]: true,
+	[getProjectInfo.name]: true,
+	[validateDependencyVersions.name]: false,
+	[validateProjectVersionAllowed.name]: false,
+	// [validateKubernetesConfig.name]: false,
+	[validateGitTag.name]: false,
+	[buildArtifact.name]: false,
+	[preparePreReleaseVersion.name]: false,
+	[manuallyPublishArtifact.name]: false,
+	[gitTag.name]: false,
+	[waitOnNexusUpdate.name]: false,
+	[downloadArtifactForDeployment.name]: false,
+	[buildAndPushDocker.name]: false,
+	[deployToKubernetes.name]: false
+};
