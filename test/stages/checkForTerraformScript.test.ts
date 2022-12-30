2055e5d790f55c3f6c742d4df8df878a99c3e230
Craig Miller <craigmiller160@gmail.com>
2022-12-30 11:52:33 -0500

    Implementing a test and adding a flag saying that the project has terraform

--- test/stages/checkForTerraformScript.test.ts
+++ test/stages/checkForTerraformScript.test.ts
@@ -1,8 +1,30 @@
-export {};
+import { getCwdMock } from '../testutils/getCwdMock';
+import path from 'path';
+import { baseWorkingDir } from '../testutils/baseWorkingDir';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { ProjectType } from '../../src/context/ProjectType';
+import { checkForTerraformScript } from '../../src/stages/checkForTerraformScript';
+import '@relmify/jest-fp-ts';
+
+const baseBuildContext = createBuildContext();
 
 describe('checkForTerraformScript', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
 	it('is application with terraform script', async () => {
-		throw new Error();
+		getCwdMock.mockImplementation(() =>
+			path.join(baseWorkingDir, 'mavenReleaseApplicationWithTerraform')
+		);
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			projectType: ProjectType.MavenApplication
+		};
+
+		const result = await checkForTerraformScript.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
 	});
 
 	it('is application without terraform script', async () => {
