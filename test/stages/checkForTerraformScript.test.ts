9f148220f07a9aaf9908b92ff1840da76d6ba2be
Craig Miller <craigmiller160@gmail.com>
2022-12-30 11:26:59 -0500

    Stubbed out tests

---
+++ test/stages/checkForTerraformScript.test.ts
@@ -0,0 +1,11 @@
+export {};
+
+describe('checkForTerraformScript', () => {
+	it('is application with terraform script', async () => {
+		throw new Error();
+	});
+
+	it('is application without terraform script', async () => {
+		throw new Error();
+	});
+});
