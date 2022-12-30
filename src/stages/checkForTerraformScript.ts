33b242bdc7ad1a16422fb4e0a76ab699769ed607
Craig Miller <craigmiller160@gmail.com>
2022-12-30 11:23:05 -0500

    Fixed type error

--- src/stages/checkForTerraformScript.ts
+++ src/stages/checkForTerraformScript.ts
@@ -4,7 +4,8 @@ import { BuildContext } from '../context/BuildContext';
 import { isApplication } from '../context/projectTypeUtils';
 
 const execute: StageExecuteFn = (context) => TaskEither.right(context);
-const shouldStageExecute = (context: BuildContext) => isApplication(context);
+const shouldStageExecute = (context: BuildContext) =>
+	isApplication(context.projectType);
 
 export const checkForTerraformScript: Stage = {
 	name: 'Check For Terraform Script',
