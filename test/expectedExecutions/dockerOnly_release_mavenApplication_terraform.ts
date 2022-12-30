02926ae72674f6a6e4aa19daf6d64b2e5ab6967b
Craig Miller <craigmiller160@gmail.com>
2022-12-30 12:05:48 -0500

    Added more test data

---
+++ test/expectedExecutions/dockerOnly_release_mavenApplication_terraform.ts
@@ -0,0 +1,44 @@
+import { validateDependencyVersions } from '../../src/stages/validateDependencyVersions';
+import { validateProjectVersionAllowed } from '../../src/stages/validateProjectVersionAllowed';
+import { validateGitTag } from '../../src/stages/validateGitTag';
+import { buildArtifact } from '../../src/stages/buildArtifact';
+import { preparePreReleaseVersion } from '../../src/stages/preparePreReleaseVersion';
+import { manuallyPublishArtifact } from '../../src/stages/manuallyPublishArtifact';
+import { gitTag } from '../../src/stages/gitTag';
+import { downloadArtifactForDeployment } from '../../src/stages/downloadArtifactForDeployment';
+import { buildAndPushDocker } from '../../src/stages/buildAndPushDocker';
+import { deployToKubernetes } from '../../src/stages/deployToKubernetes';
+import { ExpectedExecution } from './ExpectedExecution';
+import { getCommandInfo } from '../../src/stages/getCommandInfo';
+import { getBuildToolInfo } from '../../src/stages/getBuildToolInfo';
+import { validateBuildToolVersion } from '../../src/stages/validateBuildToolVersion';
+import { checkForUncommittedChanges } from '../../src/stages/checkForUncommittedChanges';
+import { getProjectType } from '../../src/stages/getProjectType';
+import { getProjectInfo } from '../../src/stages/getProjectInfo';
+import { waitOnNexusUpdate } from '../../src/stages/waitOnNexusUpdate';
+import { runTerraformScript } from '../../src/stages/runTerraformScript';
+import { checkForTerraformScript } from '../../src/stages/checkForTerraformScript';
+
+export const dockerOnly_release_mavenApplication_terraform: ExpectedExecution =
+	{
+		[getCommandInfo.name]: true,
+		[getBuildToolInfo.name]: true,
+		[validateBuildToolVersion.name]: true,
+		[checkForUncommittedChanges.name]: true,
+		[getProjectType.name]: true,
+		[getProjectInfo.name]: true,
+		[checkForTerraformScript.name]: true,
+		[validateDependencyVersions.name]: false,
+		[validateProjectVersionAllowed.name]: false,
+		// [validateKubernetesConfig.name]: true,
+		[validateGitTag.name]: false,
+		[buildArtifact.name]: false,
+		[preparePreReleaseVersion.name]: false,
+		[manuallyPublishArtifact.name]: false,
+		[gitTag.name]: false,
+		[waitOnNexusUpdate.name]: true,
+		[downloadArtifactForDeployment.name]: true,
+		[buildAndPushDocker.name]: true,
+		[deployToKubernetes.name]: true,
+		[runTerraformScript.name]: true
+	};
