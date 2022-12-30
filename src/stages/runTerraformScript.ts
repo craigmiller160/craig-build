a0762b89fa03f1ea981e1e04977f68c6668eb9a0
Craig Miller <craigmiller160@gmail.com>
2022-12-30 12:09:45 -0500

    Implemented should execute logic

--- src/stages/runTerraformScript.ts
+++ src/stages/runTerraformScript.ts
@@ -1,8 +1,15 @@
 import { Stage, StageExecuteFn } from './Stage';
 import * as TaskEither from 'fp-ts/TaskEither';
+import * as Pred from 'fp-ts/Predicate';
+import { BuildContext } from '../context/BuildContext';
+import { pipe } from 'fp-ts/function';
+import { isApplication } from '../context/projectTypeUtils';
 
 const execute: StageExecuteFn = (context) => TaskEither.right(context);
-const shouldStageExecute = () => false;
+const shouldStageExecute: Pred.Predicate<BuildContext> = pipe(
+	(_: BuildContext) => isApplication(_.projectType),
+	Pred.and((_) => _.hasTerraform)
+);
 
 export const runTerraformScript: Stage = {
 	name: 'Run Terraform Script',
