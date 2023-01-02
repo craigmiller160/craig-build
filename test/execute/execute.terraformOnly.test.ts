7d4260e531383aaad956319f31779e611b17ca44
Craig Miller <craigmiller160@gmail.com>
2023-01-02 08:54:11 -0500

    Stubbed out tests

---
+++ test/execute/execute.terraformOnly.test.ts
@@ -0,0 +1,9 @@
+describe('execute.terraformOnly', () => {
+	it('executes terraform only for MavenApplication without terraform', async () => {
+		throw new Error();
+	});
+
+	it('executes terraform only for MavenApplication with terraform', async () => {
+		throw new Error();
+	});
+});
