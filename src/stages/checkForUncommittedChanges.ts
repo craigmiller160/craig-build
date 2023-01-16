4bd86cb4e59ea11a4d3022175d1510e926b83230
Craig Miller <craigmiller160@gmail.com>
2023-01-16 16:49:58 -0500

    Fully refactored stage

--- src/stages/checkForUncommittedChanges.ts
+++ src/stages/checkForUncommittedChanges.ts
@@ -1,13 +1,14 @@
 import { Stage, StageExecuteFn } from './Stage';
 import { runCommand } from '../command/runCommand';
 import { pipe } from 'fp-ts/function';
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { match, P } from 'ts-pattern';
 import * as Pred from 'fp-ts/Predicate';
 import { BuildContext } from '../context/BuildContext';
 import { CommandType } from '../context/CommandType';
+import { readUserInput } from '../utils/readUserInput';
 
+const ERROR_MESSAGE = 'Cannot run with uncommitted changes';
 export const GIT_COMMAND = 'git status --porcelain';
 
 const isUserPromptCommand: Pred.Predicate<CommandType> = (type) =>
@@ -15,31 +16,47 @@ const isUserPromptCommand: Pred.Predicate<CommandType> = (type) =>
 const hasUncommittedChanges: Pred.Predicate<string> = (message) =>
 	message.length > 0;
 
+const handlePromptResult = (result: string): TE.TaskEither<Error, string> => {
+	if (result.trim().toLowerCase() === 'y') {
+		return TE.right('');
+	}
+	return TE.left(new Error(ERROR_MESSAGE));
+};
+
+const promptToProceed = (): TE.TaskEither<Error, string> =>
+	pipe(
+		readUserInput(
+			'Uncommitted changes found, do you want to proceed? (y/n): '
+		),
+		TE.fromTask,
+		TE.chain(handlePromptResult)
+	);
+
 const handleCommandResult = (
 	context: BuildContext,
 	message: string
-): E.Either<Error, string> =>
+): TE.TaskEither<Error, string> =>
 	match({ commandType: context.commandInfo.type, message })
 		.with(
 			{
 				commandType: P.when(isUserPromptCommand),
 				message: P.when(hasUncommittedChanges)
 			},
-			() => E.right('')
+			promptToProceed
 		)
 		.with(
 			{
 				commandType: P.when(Pred.not(isUserPromptCommand)),
 				message: P.when(hasUncommittedChanges)
 			},
-			() => E.left(new Error('Cannot run with uncommitted changes'))
+			() => TE.left(new Error(ERROR_MESSAGE))
 		)
-		.otherwise(() => E.right(''));
+		.otherwise(() => TE.right(''));
 
 const execute: StageExecuteFn = (context) =>
 	pipe(
 		runCommand(GIT_COMMAND),
-		TE.chain((_) => pipe(handleCommandResult(context, _), TE.fromEither)),
+		TE.chain((_) => handleCommandResult(context, _)),
 		TE.map(() => context)
 	);
 const shouldStageExecute: Pred.Predicate<BuildContext> = () => true;
