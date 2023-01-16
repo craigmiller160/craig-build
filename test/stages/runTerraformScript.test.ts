9a1cdc918da90c73fbdd380200fde49ab0d9b0cf
Craig Miller <craigmiller160@gmail.com>
2023-01-16 08:07:28 -0500

    Updated tests

--- test/stages/runTerraformScript.test.ts
+++ test/stages/runTerraformScript.test.ts
@@ -12,6 +12,20 @@ import * as Task from 'fp-ts/Task';
 import * as TaskEither from 'fp-ts/TaskEither';
 import shellEnv from 'shell-env';
 
+const NO_CHANGES_OUTPUT = `
+data.keycloak_realm.apps_prod: Reading...
+data.keycloak_realm.apps_dev: Reading...
+data.keycloak_realm.apps_dev: Read complete after 0s [id=apps-dev]
+keycloak_openid_client.expense_tracker_ui_dev: Refreshing state... [id=7ba71e16-28cf-4568-9ea1-819781bdf85e]
+data.keycloak_realm.apps_prod: Read complete after 0s [id=apps-prod]
+keycloak_openid_client.expense_tracker_ui_prod: Refreshing state... [id=302c0308-9523-43cf-aa23-d5dbb055bd17]
+
+No changes. Your infrastructure matches the configuration.
+
+Terraform has compared your real infrastructure against your configuration
+and found no differences, so no changes are needed.
+`;
+
 jest.mock('../../src/utils/readUserInput', () => ({
 	readUserInput: jest.fn()
 }));
@@ -143,5 +157,39 @@ describe('runTerraformScript', () => {
 				env: expect.any(Object)
 			}
 		);
+	});
+
+	it('executes terraform script with no changes to apply', async () => {
+		prepareEnvMock();
+		const workingDir = path.join(
+			baseWorkingDir,
+			'mavenReleaseApplicationWithTerraformAndSecrets'
+		);
+		getCwdMock.mockImplementation(() => workingDir);
+		readUserInputMock.mockImplementation(() => Task.of('y'));
+		runCommandMock.mockImplementation(() =>
+			TaskEither.right(NO_CHANGES_OUTPUT)
+		);
+		const buildContext: BuildContext = {
+			...createBuildContext(),
+			projectType: ProjectType.MavenApplication,
+			hasTerraform: true
+		};
+
+		const result = await runTerraformScript.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(readUserInputMock).toHaveBeenCalledTimes(1);
+		expect(readUserInputMock).toHaveBeenNthCalledWith(
+			1,
+			'Do you want to execute the terraform script? (y/n): '
+		);
+
+		expect(runCommandMock).toHaveBeenCalledTimes(1);
+		expect(runCommandMock).toHaveBeenNthCalledWith(1, 'terraform plan', {
+			printOutput: true,
+			cwd: path.join(workingDir, 'deploy', 'terraform'),
+			env: expect.any(Object)
+		});
 	});
 });
