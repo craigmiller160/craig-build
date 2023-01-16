71518c5ffc636574cc2f7e8d91da1bbcd85059dc
Craig Miller <craigmiller160@gmail.com>
2023-01-16 16:58:24 -0500

    Feature/cb 82 (#143)
    * Stubbed out new tests
    
    * Updating test logic
    
    * More test work
    
    * More work on test setup
    
    * More test setup work
    
    * Implemented all tests
    
    * Enhancing logic to factor in the different command types
    
    * Fully refactored stage
    
    * Fixing tests
    
    * Bumped version

--- src/stages/checkForUncommittedChanges.ts
+++ src/stages/checkForUncommittedChanges.ts
@@ -1,28 +1,62 @@
 import { Stage, StageExecuteFn } from './Stage';
 import { runCommand } from '../command/runCommand';
 import { pipe } from 'fp-ts/function';
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { match } from 'ts-pattern';
+import { match, P } from 'ts-pattern';
 import * as Pred from 'fp-ts/Predicate';
 import { BuildContext } from '../context/BuildContext';
+import { CommandType } from '../context/CommandType';
+import { readUserInput } from '../utils/readUserInput';
 
+const ERROR_MESSAGE = 'Cannot run with uncommitted changes';
 export const GIT_COMMAND = 'git status --porcelain';
 
-const handleCommandResult = (message: string): E.Either<Error, string> =>
-	match(message)
-		.when(
-			(_) => _.length === 0,
-			() => E.right('')
+const isUserPromptCommand: Pred.Predicate<CommandType> = (type) =>
+	[CommandType.TerraformOnly, CommandType.KubernetesOnly].includes(type);
+const hasUncommittedChanges: Pred.Predicate<string> = (message) =>
+	message.length > 0;
+
+const handlePromptResult = (result: string): TE.TaskEither<Error, string> => {
+	if (result.trim().toLowerCase() === 'y') {
+		return TE.right('');
+	}
+	return TE.left(new Error(ERROR_MESSAGE));
+};
+
+const promptToProceed = (): TE.TaskEither<Error, string> =>
+	pipe(
+		readUserInput(
+			'Uncommitted changes found, do you want to proceed? (y/n): '
+		),
+		TE.fromTask,
+		TE.chain(handlePromptResult)
+	);
+
+const handleCommandResult = (
+	context: BuildContext,
+	message: string
+): TE.TaskEither<Error, string> =>
+	match({ commandType: context.commandInfo.type, message })
+		.with(
+			{
+				commandType: P.when(isUserPromptCommand),
+				message: P.when(hasUncommittedChanges)
+			},
+			promptToProceed
 		)
-		.otherwise(() =>
-			E.left(new Error('Cannot run with uncommitted changes'))
-		);
+		.with(
+			{
+				commandType: P.when(Pred.not(isUserPromptCommand)),
+				message: P.when(hasUncommittedChanges)
+			},
+			() => TE.left(new Error(ERROR_MESSAGE))
+		)
+		.otherwise(() => TE.right(''));
 
 const execute: StageExecuteFn = (context) =>
 	pipe(
 		runCommand(GIT_COMMAND),
-		TE.chain((_) => pipe(handleCommandResult(_), TE.fromEither)),
+		TE.chain((_) => handleCommandResult(context, _)),
 		TE.map(() => context)
 	);
 const shouldStageExecute: Pred.Predicate<BuildContext> = () => true;
--- test/stages/checkForUncommittedChanges.test.ts
+++ test/stages/checkForUncommittedChanges.test.ts
@@ -1,30 +1,36 @@
 import { runCommandMock } from '../testutils/runCommandMock';
-import * as TE from 'fp-ts/TaskEither';
+import * as TaskEither from 'fp-ts/TaskEither';
+import * as Task from 'fp-ts/Task';
 import '@relmify/jest-fp-ts';
 import {
 	checkForUncommittedChanges,
 	GIT_COMMAND
 } from '../../src/stages/checkForUncommittedChanges';
 import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { CommandType } from '../../src/context/CommandType';
+import { readUserInput } from '../../src/utils/readUserInput';
 
-const buildContext = createBuildContext();
+const baseBuildContext = createBuildContext();
+jest.mock('../../src/utils/readUserInput', () => ({
+	readUserInput: jest.fn()
+}));
+const readUserInputMock = readUserInput as jest.Mock;
 
 describe('checkForUncommittedChanges', () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
 	});
 
-	it('uncommitted changes found', async () => {
-		runCommandMock.mockImplementation(() => TE.right(''));
-
-		const result = await checkForUncommittedChanges.execute(buildContext)();
-		expect(result).toEqualRight(buildContext);
-
-		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
-	});
-
-	it('uncommitted changes not found', async () => {
-		runCommandMock.mockImplementation(() => TE.right('abc'));
+	it('uncommitted changes found for FullBuild build', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right('abc'));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.FullBuild
+			}
+		};
 
 		const result = await checkForUncommittedChanges.execute(buildContext)();
 		expect(result).toEqualLeft(
@@ -32,5 +38,104 @@ describe('checkForUncommittedChanges', () => {
 		);
 
 		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).not.toHaveBeenCalled();
+	});
+
+	it('uncommitted changes found for DockerOnly build', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right('abc'));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.DockerOnly
+			}
+		};
+
+		const result = await checkForUncommittedChanges.execute(buildContext)();
+		expect(result).toEqualLeft(
+			new Error('Cannot run with uncommitted changes')
+		);
+
+		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).not.toHaveBeenCalled();
+	});
+
+	it('uncommitted changes not found', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right(''));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.DockerOnly
+			}
+		};
+
+		const result = await checkForUncommittedChanges.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).not.toHaveBeenCalled();
+	});
+
+	it('uncommitted changes found for KubernetesOnly build, approve to proceed', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right('abc'));
+		readUserInputMock.mockImplementation(() => Task.of('y'));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.KubernetesOnly
+			}
+		};
+
+		const result = await checkForUncommittedChanges.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).toHaveBeenCalledWith(
+			'Uncommitted changes found, do you want to proceed? (y/n): '
+		);
+	});
+
+	it('uncommitted changes found for TerraformOnly build, approve to proceed', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right('abc'));
+		readUserInputMock.mockImplementation(() => Task.of('y'));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.TerraformOnly
+			}
+		};
+
+		const result = await checkForUncommittedChanges.execute(buildContext)();
+		expect(result).toEqualRight(buildContext);
+
+		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).toHaveBeenCalledWith(
+			'Uncommitted changes found, do you want to proceed? (y/n): '
+		);
+	});
+
+	it('uncommitted changes found for TerraformOnly build, deny to proceed', async () => {
+		runCommandMock.mockImplementation(() => TaskEither.right('abc'));
+		readUserInputMock.mockImplementation(() => Task.of('n'));
+		const buildContext: BuildContext = {
+			...baseBuildContext,
+			commandInfo: {
+				...baseBuildContext.commandInfo,
+				type: CommandType.TerraformOnly
+			}
+		};
+
+		const result = await checkForUncommittedChanges.execute(buildContext)();
+		expect(result).toEqualLeft(
+			new Error('Cannot run with uncommitted changes')
+		);
+
+		expect(runCommandMock).toHaveBeenCalledWith(GIT_COMMAND);
+		expect(readUserInputMock).toHaveBeenCalledWith(
+			'Uncommitted changes found, do you want to proceed? (y/n): '
+		);
 	});
 });
