532f2b611c757ef87cb3d2754d77592fe2f75f49
Craig Miller <craigmiller160@gmail.com>
2023-01-16 16:46:42 -0500

    Enhancing logic to factor in the different command types

--- src/stages/checkForUncommittedChanges.ts
+++ src/stages/checkForUncommittedChanges.ts
@@ -3,26 +3,43 @@ import { runCommand } from '../command/runCommand';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
-import { match } from 'ts-pattern';
+import { match, P } from 'ts-pattern';
 import * as Pred from 'fp-ts/Predicate';
 import { BuildContext } from '../context/BuildContext';
+import { CommandType } from '../context/CommandType';
 
 export const GIT_COMMAND = 'git status --porcelain';
 
-const handleCommandResult = (message: string): E.Either<Error, string> =>
-	match(message)
-		.when(
-			(_) => _.length === 0,
+const isUserPromptCommand: Pred.Predicate<CommandType> = (type) =>
+	[CommandType.TerraformOnly, CommandType.KubernetesOnly].includes(type);
+const hasUncommittedChanges: Pred.Predicate<string> = (message) =>
+	message.length > 0;
+
+const handleCommandResult = (
+	context: BuildContext,
+	message: string
+): E.Either<Error, string> =>
+	match({ commandType: context.commandInfo.type, message })
+		.with(
+			{
+				commandType: P.when(isUserPromptCommand),
+				message: P.when(hasUncommittedChanges)
+			},
 			() => E.right('')
 		)
-		.otherwise(() =>
-			E.left(new Error('Cannot run with uncommitted changes'))
-		);
+		.with(
+			{
+				commandType: P.when(Pred.not(isUserPromptCommand)),
+				message: P.when(hasUncommittedChanges)
+			},
+			() => E.left(new Error('Cannot run with uncommitted changes'))
+		)
+		.otherwise(() => E.right(''));
 
 const execute: StageExecuteFn = (context) =>
 	pipe(
 		runCommand(GIT_COMMAND),
-		TE.chain((_) => pipe(handleCommandResult(_), TE.fromEither)),
+		TE.chain((_) => pipe(handleCommandResult(context, _), TE.fromEither)),
 		TE.map(() => context)
 	);
 const shouldStageExecute: Pred.Predicate<BuildContext> = () => true;
