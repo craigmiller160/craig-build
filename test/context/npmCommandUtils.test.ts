807e7c2e68340adcf06ef568741c65cbc45740c8
Craig Miller <craigmiller160@gmail.com>
2023-07-23 11:00:42 -0400

    Working on test logic

--- test/context/npmCommandUtils.test.ts
+++ test/context/npmCommandUtils.test.ts
@@ -1,18 +1,41 @@
+import { getCwdMock } from '../testutils/getCwdMock';
+import path from 'path';
+
+const getNpmCommandBaseDir = path.join(
+	process.cwd(),
+	'test',
+	'__getNpmCommand__'
+);
+
 describe('npmCommandUtils', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
 	describe('getNpmCommand', () => {
 		it('is npm', () => {
+			getCwdMock.mockImplementation(() =>
+				path.join(getNpmCommandBaseDir, 'npm')
+			);
 			throw new Error();
 		});
 
 		it('is yarn', () => {
+			getCwdMock.mockImplementation(() =>
+				path.join(getNpmCommandBaseDir, 'yarn')
+			);
 			throw new Error();
 		});
 
 		it('is pnpm', () => {
+			getCwdMock.mockImplementation(() =>
+				path.join(getNpmCommandBaseDir, 'yarn')
+			);
 			throw new Error();
 		});
 
 		it('is unknown', () => {
+			getCwdMock.mockImplementation(() => getNpmCommandBaseDir);
 			throw new Error();
 		});
 	});
