7ca8abb0aa7f302871c01d3e04221f22d439b821
Craig Miller <craigmiller160@gmail.com>
2023-07-23 10:39:58 -0400

    Stubbing out new tests

---
+++ test/context/npmCommandUtils.test.ts
@@ -0,0 +1,19 @@
+describe('npmCommandUtils', () => {
+	describe('getNpmCommand', () => {
+		it('is npm', () => {
+			throw new Error();
+		});
+
+		it('is yarn', () => {
+			throw new Error();
+		});
+
+		it('is pnpm', () => {
+			throw new Error();
+		});
+
+		it('is unknown', () => {
+			throw new Error();
+		});
+	});
+});
