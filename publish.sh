1fe5bea20baff62629f4a5d94148bc1037c973ef
Craig Miller <craigmiller160@gmail.com>
2023-07-23 14:36:51 -0400

    Fixed publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm version --no-git-tag-version $1 && npm publish
+npm version --allow-same-version --no-git-tag-version $1 && npm publish
 if [ $? -eq 0 ]; then
   git add package.json
   git commit -m "Bumped version"
