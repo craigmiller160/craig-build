a4c25df423fc169f593d9e505fcb6595e4992f15
Craig Miller <craigmiller160@gmail.com>
2023-07-29 15:06:54 -0400

    More work on tests

--- test/utils/isNpmVersionInRange.test.ts
+++ test/utils/isNpmVersionInRange.test.ts
@@ -1,22 +1,16 @@
 import { isNpmVersionInRange } from '../../src/utils/isNpmVersionInRange';
 
 describe('isNpmVersionInRange', () => {
-	describe('absolute versions', () => {
-		it('greater than', () => {
-			expect(isNpmVersionInRange('2.0.0', '1.0.0')).toEqual(false);
-			expect(isNpmVersionInRange('1.0.1', '1.0.0')).toEqual(false);
-			expect(isNpmVersionInRange('1.1.0', '1.0.0')).toEqual(false);
-		});
+	it('absolute versions', () => {
+		expect(isNpmVersionInRange('2.0.0', '1.0.0')).toEqual(false);
+		expect(isNpmVersionInRange('1.0.1', '1.0.0')).toEqual(false);
+		expect(isNpmVersionInRange('1.1.0', '1.0.0')).toEqual(false);
 
-		it('less than', () => {
-			expect(isNpmVersionInRange('1.0.0', '2.0.0')).toEqual(false);
-			expect(isNpmVersionInRange('1.0.0', '1.1.0')).toEqual(false);
-			expect(isNpmVersionInRange('1.0.0', '1.0.1')).toEqual(false);
-		});
+		expect(isNpmVersionInRange('1.0.0', '2.0.0')).toEqual(false);
+		expect(isNpmVersionInRange('1.0.0', '1.1.0')).toEqual(false);
+		expect(isNpmVersionInRange('1.0.0', '1.0.1')).toEqual(false);
 
-		it('equal to', () => {
-			throw new Error();
-		});
+		expect(isNpmVersionInRange('1.0.0', '1.0.0')).toEqual(true);
 	});
 
 	describe('X.x versions', () => {
