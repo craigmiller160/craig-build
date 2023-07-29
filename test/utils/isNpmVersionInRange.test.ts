dc620987c310a18baba65a8869acd7a2748f22c0
Craig Miller <craigmiller160@gmail.com>
2023-07-29 15:02:11 -0400

    Stubbed out more tests

--- test/utils/isNpmVersionInRange.test.ts
+++ test/utils/isNpmVersionInRange.test.ts
@@ -2,4 +2,46 @@ describe('isNpmVersionInRange', () => {
 	it('tests', () => {
 		throw new Error();
 	});
+
+	describe('absolute versions', () => {
+		it('greater than', () => {
+			throw new Error();
+		});
+
+		it('less than', () => {
+			throw new Error();
+		});
+
+		it('equal to', () => {
+			throw new Error();
+		});
+	});
+
+	describe('^/~ versions', () => {
+		it('greater than', () => {
+			throw new Error();
+		});
+
+		it('less than', () => {
+			throw new Error();
+		});
+
+		it('equal to', () => {
+			throw new Error();
+		});
+	});
+
+	describe('range versions', () => {
+		it('greater than', () => {
+			throw new Error();
+		});
+
+		it('less than', () => {
+			throw new Error();
+		});
+
+		it('equal to', () => {
+			throw new Error();
+		});
+	});
 });
