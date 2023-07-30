1fe147e2a65b473dea4fab543aca267aa1493590
Craig Miller <craigmiller160@gmail.com>
2023-07-30 11:03:03 -0400

    Implemented function

--- src/utils/isNpmVersionInRange.ts
+++ src/utils/isNpmVersionInRange.ts
@@ -1,14 +1,7 @@
-import {string} from 'fp-ts';
+import semver from 'semver';
 
-const cleanVersionForPeerValidation = (version: string): string =>
-	version.replace(/-beta/g, '').replace(/^[\^~]/, '');
+// const cleanVersionForPeerValidation = (version: string): string =>
+// 	version.replace(/-beta/g, '').replace(/^[\^~]/, '');
 
-export const isNpmVersionInRange = (
-	version: string,
-	range: string
-): boolean => {
-
-
-	// TODO finish this
-	return false;
-};
+export const isNpmVersionInRange = (version: string, range: string): boolean =>
+	semver.satisfies(version, range);
