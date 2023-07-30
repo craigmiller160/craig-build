0ebd055959ca4f076be7d263ae10b3f50dd8a00b
Craig Miller <craigmiller160@gmail.com>
2023-07-30 18:39:10 -0400

    Validated regex

--- src/utils/semverUtils.ts
+++ src/utils/semverUtils.ts
@@ -1,13 +1,19 @@
 import semver from 'semver';
 
-export const VERSION_REGEX = /(?<major>)\d+\.(?<minor>\d+)\.(?<patch>\d)(-beta(\.(?<beta>\d+)?))?/;
+export type VersionRegexGroups = Readonly<{
+	major: string;
+	minor: string;
+	patch: string;
+	beta?: string;
+}>;
+
+export const VERSION_REGEX =
+	/(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d)(-beta(\.(?<beta>\d+)?))?/;
 
 export const semverTrimVersion = (version: string): string =>
 	version.split('-')[0];
 
 export const semverMaxVersion = (version: string): string => {
-
-
 	if (version.charAt(0) === '~') {
 		const parts = version.slice(1).split('.');
 		return `${parts[0]}.${parts[1]}.999`;
--- test/utils/semverUtils.test.ts
+++ test/utils/semverUtils.test.ts
@@ -1,10 +1,36 @@
 import {
 	semverMaxVersion,
 	semverSatisifies,
-	semverTrimVersion
+	semverTrimVersion,
+	VERSION_REGEX,
+	VersionRegexGroups
 } from '../../src/utils/semverUtils';
 
 describe('semverUtils', () => {
+	it('VERSION_REGEX', () => {
+		expect(VERSION_REGEX.exec('foo')).toBeNull();
+
+		const nonBetaResult = VERSION_REGEX.exec('1.2.3');
+		expect(nonBetaResult).not.toBeNull();
+		const nonBetaGroups = nonBetaResult!.groups as unknown as VersionRegexGroups;
+		expect(nonBetaGroups).toEqual({
+			major: '1',
+			minor: '2',
+			patch: '3',
+			beta: undefined
+		});
+
+		const betaResult = VERSION_REGEX.exec('1.2.3-beta.4');
+		expect(betaResult).not.toBeNull();
+		const betaGroups = betaResult!.groups as unknown as VersionRegexGroups;
+		expect(betaGroups).toEqual({
+			major: '1',
+			minor: '2',
+			patch: '3',
+			beta: '4'
+		});
+	});
+
 	it('semverTrimVersion', () => {
 		expect(semverTrimVersion('1.0.0')).toEqual('1.0.0');
 		expect(semverTrimVersion('1.0.0-beta')).toEqual('1.0.0');
