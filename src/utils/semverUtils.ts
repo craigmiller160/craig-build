cc92ce8da18f54d30358cdac795c6ed1053b0666
Craig Miller <craigmiller160@gmail.com>
2023-07-30 18:25:33 -0400

    Implemented new semver function

--- src/utils/semverUtils.ts
+++ src/utils/semverUtils.ts
@@ -1,2 +1,16 @@
 export const semverTrimVersion = (version: string): string =>
 	version.split('-')[0];
+
+export const semverMaxVersion = (version: string): string => {
+	if (version.charAt(0) === '~') {
+		const parts = version.slice(1).split('.');
+		return `${parts[0]}.${parts[1]}.999`;
+	}
+
+	if (version.charAt(0) == '^') {
+		const parts = version.slice(1).split('.');
+		return `${parts[0]}.999.999`;
+	}
+
+	return version;
+};
---
+++ test/utils/semverUtils.test.ts
@@ -0,0 +1,17 @@
+import {
+	semverMaxVersion,
+	semverTrimVersion
+} from '../../src/utils/semverUtils';
+
+describe('semverUtils', () => {
+	it('semverTrimVersion', () => {
+		expect(semverTrimVersion('1.0.0')).toEqual('1.0.0');
+		expect(semverTrimVersion('1.0.0-beta')).toEqual('1.0.0');
+	});
+
+	it('semverMaxVersion', () => {
+		expect(semverMaxVersion('1.0.0')).toEqual('1.0.0');
+		expect(semverMaxVersion('~1.0.0')).toEqual('1.0.999');
+		expect(semverMaxVersion('^1.0.0')).toEqual('1.999.999');
+	});
+});
