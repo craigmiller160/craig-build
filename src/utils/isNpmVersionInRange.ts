13d1776c9df3f2c4d5e1c19ca4be06a750ebd5b7
Craig Miller <craigmiller160@gmail.com>
2023-07-30 15:03:32 -0400

    Deleted unnecessary utility function

--- src/utils/isNpmVersionInRange.ts
+++
@@ -1,7 +0,0 @@
-import semver from 'semver';
-
-// const cleanVersionForPeerValidation = (version: string): string =>
-// 	version.replace(/-beta/g, '').replace(/^[\^~]/, '');
-
-export const isNpmVersionInRange = (version: string, range: string): boolean =>
-	semver.satisfies(version, range);
