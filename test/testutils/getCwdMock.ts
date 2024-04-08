a8eecb5e085cd644a111d4fbb82e5a243d03ad47
Craig Miller <craigmiller160@gmail.com>
2024-04-08 18:41:40 -0400

    More fixes

--- test/testutils/getCwdMock.ts
+++ test/testutils/getCwdMock.ts
@@ -1,7 +1,8 @@
+import { vi, MockedFunction } from 'vitest';
 import { getCwd } from '../../src/command/getCwd';
 
 vi.mock('../../src/command/getCwd', () => ({
 	getCwd: vi.fn()
 }));
 
-export const getCwdMock = getCwd as vi.Mock;
+export const getCwdMock = getCwd as MockedFunction<typeof getCwd>;
