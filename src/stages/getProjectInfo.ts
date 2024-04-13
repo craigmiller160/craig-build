37449fa19a700b8bae07ee0d49e0cad8382bc857
Craig Miller <craigmiller160@gmail.com>
2024-04-13 09:32:02 -0400

    Implemented new process for reading maven monorepo project info

--- src/stages/getProjectInfo.ts
+++ src/stages/getProjectInfo.ts
@@ -2,9 +2,10 @@ import { Stage, StageExecuteFn } from './Stage';
 import { ProjectType } from '../context/ProjectType';
 import { ProjectInfo } from '../context/ProjectInfo';
 import { match } from 'ts-pattern';
-import { taskEither } from 'fp-ts';
+import { option, readonlyArray, taskEither } from 'fp-ts';
 import { either, function as func } from 'fp-ts';
 import { predicate } from 'fp-ts';
+import path from 'path';
 import {
 	isDocker,
 	isGradle,
@@ -15,7 +16,7 @@ import {
 import { npmSeparateGroupAndName } from '../utils/npmSeparateGroupAndName';
 import { PackageJson } from '../configFileTypes/PackageJson';
 import { DockerJson } from '../configFileTypes/DockerJson';
-import { PomXml } from '../configFileTypes/PomXml';
+import { MavenModules, PomXml } from '../configFileTypes/PomXml';
 import { BuildContext } from '../context/BuildContext';
 import { VersionType } from '../context/VersionType';
 import { regexTest } from '../functions/RegExp';
@@ -23,6 +24,7 @@ import { GradleProject } from '../special/gradle';
 import { getRawProjectData } from '../projectReading';
 import { HelmJson } from '../configFileTypes/HelmJson';
 import { getNpmBuildTool } from '../context/npmCommandUtils';
+import { getCwd } from '../command/getCwd';
 
 const BETA_VERSION_REGEX = /^.*-beta/;
 const SNAPSHOT_VERSION_REGEX = /^.*-SNAPSHOT/;
@@ -41,19 +43,54 @@ const getVersionType = (version: string): VersionType =>
 		.otherwise(() => VersionType.Release);
 
 const readMavenProjectInfo = (
-	projectType: ProjectType
+	projectType: ProjectType,
+	cwd: string = getCwd(),
+	rootProjectInfo: ProjectInfo | undefined = undefined
 ): taskEither.TaskEither<Error, ProjectInfo> =>
 	func.pipe(
-		getRawProjectData<PomXml>(projectType),
-		taskEither.map((pomXml): ProjectInfo => {
-			const version = pomXml.project.version[0];
-			return {
-				group: pomXml.project.groupId[0],
+		getRawProjectData<PomXml>(projectType, cwd),
+		taskEither.flatMap((pomXml) => {
+			const version =
+				pomXml.project.version?.[0] ?? rootProjectInfo?.version;
+			const group = pomXml.project.groupId?.[0] ?? rootProjectInfo?.group;
+			if (!version || !group) {
+				return taskEither.left(
+					new Error('Cannot find required fields from pom.xml')
+				);
+			}
+
+			const projectInfo: ProjectInfo = {
+				group,
 				name: pomXml.project.artifactId[0],
 				version,
 				versionType: getVersionType(version),
 				repoType: 'polyrepo'
 			};
+
+			if (pomXml.project.modules) {
+				return func.pipe(
+					pomXml.project.modules,
+					readonlyArray.head,
+					option.getOrElse((): MavenModules => ({ module: [] })),
+					(m) => m.module,
+					readonlyArray.map((module) => path.join(cwd, module)),
+					readonlyArray.map((modulePath) =>
+						readMavenProjectInfo(
+							projectType,
+							modulePath,
+							projectInfo
+						)
+					),
+					taskEither.sequenceArray,
+					taskEither.map(
+						(monorepoChildren): ProjectInfo => ({
+							...projectInfo,
+							monorepoChildren
+						})
+					)
+				);
+			}
+			return taskEither.right(projectInfo);
 		})
 	);
 
