aa5c71d3c9f5936dc89f41faf1463c20d1971a3f
Craig Miller <craigmiller160@gmail.com>
2024-04-13 18:14:16 -0400

    Beginning work on new phase

---
+++ src/stages/validateMonorepoVersions.ts
@@ -0,0 +1,7 @@
+import { Stage } from './Stage';
+
+export const validateMonorepoVersions: Stage = {
+	name: 'Validate Monorepo Versions',
+	execute: () => null,
+	shouldStageExecute: () => false
+};
