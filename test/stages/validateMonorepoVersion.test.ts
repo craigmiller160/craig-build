3122e9d01a113f5b09c4ad8343fe71f4cd668d1b
Craig Miller <craigmiller160@gmail.com>
2024-04-14 09:51:51 -0400

    Implemented method

--- test/stages/validateMonorepoVersion.test.ts
+++ test/stages/validateMonorepoVersion.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { RepoType } from '../../src/context/ProjectInfo';
+import { ProjectInfo, RepoType } from '../../src/context/ProjectInfo';
 import { createBuildContext } from '../testutils/createBuildContext';
 import { BuildContext } from '../../src/context/BuildContext';
 import { validateMonorepoVersions } from '../../src/stages/validateMonorepoVersions';
@@ -27,22 +27,53 @@ type ExecuteArgs = Readonly<{
 	label: string;
 	parentVersionType: VersionType;
 	childVersionTypes: ReadonlyArray<VersionType>;
+	valid: boolean;
 }>;
 
 test.each<ExecuteArgs>([
 	{
 		label: 'All version types are release',
 		parentVersionType: VersionType.Release,
-		childVersionTypes: [VersionType.Release, VersionType.Release]
+		childVersionTypes: [VersionType.Release, VersionType.Release],
+		valid: true
 	},
 	{
 		label: 'Parent version type is pre-release, children are mixed',
 		parentVersionType: VersionType.PreRelease,
-		childVersionTypes: [VersionType.Release, VersionType.PreRelease]
+		childVersionTypes: [VersionType.Release, VersionType.PreRelease],
+		valid: true
 	},
 	{
 		label: 'Parent version type is release, children are mixed',
 		parentVersionType: VersionType.Release,
-		childVersionTypes: [VersionType.Release, VersionType.PreRelease]
+		childVersionTypes: [VersionType.Release, VersionType.PreRelease],
+		valid: false
 	}
-])('validateMonorepoVersion executes for $label', () => {});
+])(
+	'validateMonorepoVersion executes for $label',
+	async ({ parentVersionType, childVersionTypes, valid }) => {
+		const projectInfo = baseContext.projectInfo;
+		const context: BuildContext = {
+			...baseContext,
+			projectInfo: {
+				...projectInfo,
+				repoType: 'monorepo',
+				versionType: parentVersionType,
+				monorepoChildren: childVersionTypes.map(
+					(versionType): ProjectInfo => ({
+						...projectInfo,
+						versionType
+					})
+				)
+			}
+		};
+		const result = await validateMonorepoVersions.execute(context)();
+		if (valid) {
+			expect(result).toEqualRight(context);
+		} else {
+			expect(result).toEqualLeft(
+				new Error('Invalid version types in monorepo child projects')
+			);
+		}
+	}
+);
