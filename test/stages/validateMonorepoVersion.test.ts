ffa91277f025b7b333aeb074455e0d7a38292bdc
Craig Miller <craigmiller160@gmail.com>
2024-04-14 09:45:04 -0400

    Implemented some tests

--- test/stages/validateMonorepoVersion.test.ts
+++ test/stages/validateMonorepoVersion.test.ts
@@ -1,10 +1,24 @@
-import { test } from 'vitest';
+import { test, expect } from 'vitest';
 import { RepoType } from '../../src/context/ProjectInfo';
+import { createBuildContext } from '../testutils/createBuildContext';
+import { BuildContext } from '../../src/context/BuildContext';
+import { validateMonorepoVersions } from '../../src/stages/validateMonorepoVersions';
+
+const baseContext = createBuildContext();
 
 test.each<RepoType>(['polyrepo', 'monorepo'])(
 	'validateMonorepoVersion should stage execute for %s',
 	(repoType) => {
-		throw new Error();
+		const context: BuildContext = {
+			...baseContext,
+			projectInfo: {
+				...baseContext.projectInfo,
+				repoType
+			}
+		};
+
+		const result = validateMonorepoVersions.shouldStageExecute(context);
+		expect(result).toEqual(repoType === 'monorepo');
 	}
 );
 
