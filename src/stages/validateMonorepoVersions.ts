5585ab49cf5a0af926c05688a67073c584b778b3
Craig Miller <craigmiller160@gmail.com>
2024-04-14 09:40:15 -0400

    Refactored should stage execute

--- src/stages/validateMonorepoVersions.ts
+++ src/stages/validateMonorepoVersions.ts
@@ -1,8 +1,13 @@
 import { taskEither } from 'fp-ts';
 import { Stage } from './Stage';
+import { predicate } from 'fp-ts';
+import { BuildContext } from '../context/BuildContext';
+
+const shouldStageExecute: predicate.Predicate<BuildContext> = (context) =>
+	context.projectInfo.repoType === 'monorepo';
 
 export const validateMonorepoVersions: Stage = {
 	name: 'Validate Monorepo Versions',
 	execute: (c) => taskEither.right(c),
-	shouldStageExecute: () => true
+	shouldStageExecute
 };
