09178352b843fbdb2f0f1521a740167f9e8abe75
Craig Miller <craigmiller160@gmail.com>
2024-04-14 09:54:14 -0400

    Implemented stage

--- src/stages/validateMonorepoVersions.ts
+++ src/stages/validateMonorepoVersions.ts
@@ -1,13 +1,30 @@
-import { taskEither } from 'fp-ts';
-import { Stage } from './Stage';
-import { predicate } from 'fp-ts';
+import { Stage, StageExecuteFn } from './Stage';
+import { predicate, taskEither } from 'fp-ts';
 import { BuildContext } from '../context/BuildContext';
+import { VersionType } from '../context/VersionType';
+
+const execute: StageExecuteFn = (context) => {
+	if (context.projectInfo.versionType === VersionType.PreRelease) {
+		return taskEither.right(context);
+	}
+
+	const hasPreReleaseVersion = !!context.projectInfo.monorepoChildren
+		?.map((childInfo) => childInfo.versionType)
+		?.find((versionType) => VersionType.PreRelease === versionType);
+
+	if (!hasPreReleaseVersion) {
+		return taskEither.right(context);
+	}
+	return taskEither.left(
+		new Error('Invalid version types in monorepo child projects')
+	);
+};
 
 const shouldStageExecute: predicate.Predicate<BuildContext> = (context) =>
 	context.projectInfo.repoType === 'monorepo';
 
 export const validateMonorepoVersions: Stage = {
 	name: 'Validate Monorepo Versions',
-	execute: (c) => taskEither.right(c),
+	execute,
 	shouldStageExecute
 };
